e4a557cb86159e70ecaf131e1d680e691422bec4
ming zhang <kevinzhang@bbtechgroup.com>
2014-08-20 18:38:58 +0800

    [server]Add:add conf and modify common

--- source/server/app.py
+++ source/server/app.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+before = datetime.utcnow()
 
 from libfelix.f_common import f_app, make_error_codes_docs
 import common
 
 # Import modules you need here
+
+import libfelix.f_user
+import libfelix.f_log
+import libfelix.f_message
+import libfelix.f_mongo
+
+
+
+libfelix.f_message.f_message_push_plugin()
+
+def error_codes():
+    # 2013110102 this timestamp is for ci to generate correct general resources docs
+    pass
+
+make_error_codes_docs(error_codes)
+
+libfelix.f_log.info("Application successfully loaded in", str((datetime.utcnow() - before).total_seconds()), "seconds")
--- source/server/common.py
+++ source/server/common.py
@@ -7,5 +7,32 @@ from libfelix.f_common import f_app, f_plugin, f_common
 class common(f_common):
   
     name = "currant"
+    debug = True
+
+    newrelic = False
+    newrelic_config = "conf/currant_newrelic.ini"
+
+    static_file_debug_enabled = True
+    static_file_debug = lambda self, filepath, root: (filepath, "views/src/.tmp/static/")
+
+    tpl_debug_enabled = True
+    tpl_debug = lambda self, tplname: "src/.tmp/" + tplname
+
+    log_file = "currant.log"
+
+    cookie_name = "currant_auth"
+
+    mongo_dbname = "currant"
+    mongo_server = "172.20.1.54"
+    mongo_auth = False
+
+    memcache_server = ["172.20.1.54:11211"]
+    memcache_lib = "memcache"
+
+   
+    version_more_dimension = ["channel", "platform"]
+
+    message_self_hosted_push_port = 8286
+    parse_delay = 5
 
 common()
