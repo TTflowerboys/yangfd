3c3c227c2ad1a4c8231649812e38f0c7cec22fb3
Luigi Fan <fdb713@gmail.com>
2014-08-25 06:20:48 +0000

    [server] Mod: change zh_Hans_HK to zh_Hant_HK

--- source/server/news_api_interface.py
+++ source/server/news_api_interface.py
@@ -34,9 +34,9 @@ def news_list(params):
 
 
 @f_api('/news/add', params=dict(
-    title=('i18n', ['zh_Hans_CN', 'zh_Hans_HK'], True),
+    title=('i18n', ['zh_Hans_CN', 'zh_Hant_HK'], True),
     link=(str, True),
-    content=('i18n', ['zh_Hans_CN', 'zh_Hans_HK'], True),
+    content=('i18n', ['zh_Hans_CN', 'zh_Hant_HK'], True),
     category=(str, "news"),
 ))
 @f_app.user.login.check(force=30)
