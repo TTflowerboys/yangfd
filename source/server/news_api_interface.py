a07b1da57ffedbf4171d2ef08d352d0d8f126fd5
Luigi Fan <fdb713@gmail.com>
2014-08-25 08:07:37 +0000

    [server] Rem: /news/related interface

--- source/server/news_api_interface.py
+++ source/server/news_api_interface.py
@@ -19,14 +19,18 @@ def blog_add(user):
     blogs = f_app.blog.get_all()
     if len(blogs) > 0:
         abort(40000, logger.warning("Blog already exists"))
-    return f_app.blog.add({"name": "currant"})
+    return f_app.blog.add({"name": f_app.common.blog_name})
 
 
-@f_api('/news', params=dict(
+@f_api('/news/search', params=dict(
     per_page=int,
     time=datetime,
+    category=(list, None, str),
 ))
 def news_list(params):
+    if "category" in params:
+        if not set(params["category"]) <= set(["news", "announcement"]):
+            abort(40000, logger.warning("Invalid params: category", params["category"], exc_info=False))
     per_page = params.pop("per_page", 0)
     params["blog_id"] = ObjectId(_blog_id)
     post_list = f_app.blog.post.search(params, per_page=per_page)
@@ -35,7 +39,6 @@ def news_list(params):
 
 @f_api('/news/add', params=dict(
     title=('i18n', ['zh_Hans_CN', 'zh_Hant_HK'], True),
-    link=(str, True),
     content=('i18n', ['zh_Hans_CN', 'zh_Hant_HK'], True),
     category=(str, "news"),
 ))
@@ -58,14 +61,13 @@ def news_get(news_id):
 
 @f_api('/news/<news_id>/edit', params=dict(
     title=('i18n', None, str),
-    link=(str, None),
     content=('i18n', None, str),
     category=(str, None),
 ))
 @f_app.user.login.check(force=30)
 def news_edit(user, news_id, params):
     if "category" in params:
-        if params.get["category"] not in ("announcement", "news"):
+        if params.get("category") not in ("announcement", "news"):
             abort(40000, logger.warning("Invalid params: category not okay"))
     return f_app.blog.post.update_set(news_id, params)
 
@@ -74,12 +76,3 @@ def news_edit(user, news_id, params):
 @f_app.user.login.check(force=30)
 def news_remove(user, news_id):
     return f_app.blog.post.remove(news_id)
-
-
-@f_api('/news/<news_id>/related', params=dict(
-    per_page=(int, 3),
-))
-def news_related(news_id, params):
-    per_page = params.pop("per_page")
-    params["_id"] = {"$ne": ObjectId(news_id)}
-    return f_app.blog.post.output(f_app.blog.post.search(params, per_page=per_page))
