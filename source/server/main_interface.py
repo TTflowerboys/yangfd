46e4d1903984725d2a06d4400faa186abc0efbf8
Luigi Fan <fdb713@gmail.com>
2014-08-27 03:07:54 +0000

    [server] Add: static path in main_interface

--- source/server/main_interface.py
+++ source/server/main_interface.py
@@ -7,3 +7,8 @@ from libfelix.f_interface import f_get, static_file, template
 @f_get('/')
 def default():
     return "Hello, world!"
+
+
+@f_get("/static/<filepath:path>")
+def static_route(filepath):
+    return static_file(filepath, root="views/static")
