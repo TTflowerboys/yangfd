fef763f66768c29e97d0c83711a72902234a795c
Luigi Fan <fdb713@gmail.com>
2014-08-27 09:18:00 +0000

    [server] Add: ticket output plugin

--- source/server/f_currant.py
+++ source/server/f_currant.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals, absolute_import
 from libfelix.f_common import f_app
 from libfelix.f_user import f_user
+from libfelix.f_ticket import f_ticket
 from libfelix.f_interface import abort
 from pymongo import ASCENDING, DESCENDING
 
@@ -10,6 +11,11 @@ logger = logging.getLogger(__name__)
 
 
 class f_currant_user(f_user):
+    """
+        ==================================================================
+        User
+        ==================================================================
+    """
     def custom_search(self, params, count=False, notime=False, per_page=10, sort=['register_time', 'desc']):
         params.setdefault("status", {"$ne": "deleted"})
         if sort is not None:
@@ -61,3 +67,51 @@ class f_currant_user(f_user):
             return False
 
 f_currant_user()
+
+
+class f_currant_ticket(f_ticket):
+    """
+        ==================================================================
+        Ticket
+        ==================================================================
+    """
+    def output(self, ticket_id_list):
+        ticket_list = f_app.ticket.get(ticket_id_list)
+        user_id_set = set()
+        for t in ticket_list:
+            user_id_set.add(t.get("creator_user_id"))
+            user_id_set |= set(t.get("assignee", []))
+
+        user_list = f_app.user.output(user_id_set, custom_fields=f_app.common.user_custom_fields)
+        user_dict = {}
+
+        for u in user_list:
+            user_dict[u["id"]] = u
+
+        for t in ticket_list:
+            t["creator_user"] = user_dict.get(t.pop("creator_user_id"))
+            if "shop_id" in t:
+                t["lab_id"] = str(t.pop("shop_id", None))
+            if isinstance(t.get("assignee"), list):
+                t["assignee"] = map(lambda x: user_dict.get(x), t["assignee"])
+
+        return ticket_list
+
+f_currant_ticket()
+
+
+class f_currant_plugins(f_app.plugin_base):
+    """
+        ==================================================================
+        Plugins
+        ==================================================================
+    """
+    def ticket_get(self, ticket):
+        if "creator_user_id" in ticket:
+            ticket["creator_user_id"] = str(ticket.pop("creator_user_id", None))
+        if "assignee" in ticket:
+            ticket["assignee"] = map(str, ticket.pop("assignee", []))
+
+        return ticket
+
+f_currant_plugins()
