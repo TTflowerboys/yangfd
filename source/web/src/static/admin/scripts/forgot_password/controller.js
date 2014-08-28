f35513ff618fbcc529f54c05ee397769cd7421d0
Michael Chou <113563@qq.com>
2014-08-28 18:10:17 +0800

    [web] Add: add countdown button of verification.

--- source/web/src/static/admin/scripts/forgot_password/controller.js
+++ source/web/src/static/admin/scripts/forgot_password/controller.js
@@ -3,22 +3,45 @@
 
 (function () {
 
-    function ctrlForgotPassword($scope, $state, $http, $rootScope, userApi, $stateParams) {
-        $scope.countries=[{name:'中国',value:'CN'},{name:'英国',value:'UK'},{name:'香港',value:'HK'},{name:'美国',value:'US'}]
-        $scope.user={}
-        $scope.user.country=$rootScope.defaultCountry
-
+    function ctrlForgotPassword($scope, $timeout, $state, $http, $rootScope, userApi, $stateParams, countries, defaultCountry) {
+        $scope.countries = countries
+        $scope.user = {}
+        $scope.user.country = defaultCountry
+        sendText = '发送'
+        $scope.send = sendText
+        $scope.sendable = false
         $scope.submit = function ($event, form) {
             $event.preventDefault()
             if (form.$invalid) {
                 return
             }
 
-            userApi.smsVerificationSend($scope.user.country,$scope.user.phone, $scope.user.password)
+            userApi.smsResetPassword($scope.user.id, $scope.user.code, $scope.user.password)
                 .success(function (data, status, headers, config) {
                     $state.go($stateParams.from || 'dashboard')
                 })
 
+        }
+
+        $scope.sendVerification = function () {
+            userApi.smsVerificationSend($scope.user.country, $scope.user.phone)
+                .success(function (data, status, headers, config) {
+                    $scope.user.id = $data.val
+                })
+            $scope.onTimeout = function () {
+                $scope.send--;
+                if ($scope.send <= 0) {
+                    $scope.send = sendText
+                    $scope.sendable = false;
+                    return
+                }
+                $timeout($scope.onTimeout, 1000);
+            }
+            if ($scope.send == sendText) {
+                $scope.send = 60;
+                $scope.sendable = true;
+                $timeout($scope.onTimeout, 1000);
+            }
         }
     }
 
