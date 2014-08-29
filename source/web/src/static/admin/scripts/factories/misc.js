127c351ebea72c1a7b7a54e569416d267ccdfa77
Frank Fang <frankfang1990@gmail.com>
2014-08-29 16:23:42 +0800

    [web] Mod: Add delayer

--- source/web/src/static/admin/scripts/factories/misc.js
+++ source/web/src/static/admin/scripts/factories/misc.js
@@ -2,7 +2,45 @@
 
 angular.module('app')
     .factory('misc', function () {
+        /**
+         * Delayer constructor, do a task in the future, and you can update the task
+         * @param options {data:{},task:function(){},delay:200}
+         * @constructor
+         */
+        function Delayer(options) {
+            options = options || {}
+            this.data = options.data
+            this.task = options.task
+            this.delay = options.delay || 200
+            this.timer = setTimeout(function () {
+                if (this.task) {
+                    this.task(this.data)
+                }
+            }.bind(this), this.delay)
+        }
+
+        Delayer.prototype.update = function (options) {
+            options = options || {}
+            if (options.task !== undefined) {
+                this.task = options.task
+            }
+            if (options.data !== undefined) {
+                this.data = options.data
+            }
+            if (options.delay !== undefined) {
+                this.delay = options.delay
+            }
+
+            if (this.timer) {
+                window.clearTimeout(this.timer)
+            }
+            this.timer = setTimeout(function () {
+                this.task(this.data)
+            }.bind(this), this.delay)
+        }
+
         return{
+
             findBy: function (array, by, value) {
                 var found
                 angular.forEach(array, function (item, key) {
@@ -13,6 +51,7 @@ angular.module('app')
                 })
                 return found
             },
+
             findById: function (array, id) {
                 var found
                 angular.forEach(array, function (value, key) {
@@ -23,6 +62,7 @@ angular.module('app')
                 })
                 return found
             },
+
             getChangedAttributes: function (newJson, oldJson) {
                 var result = null
                 for (var key in newJson) {
@@ -36,9 +76,12 @@ angular.module('app')
                 }
                 return result
             },
+
             resetArray: function (array, items) {
                 if (array === undefined) { array = [] }
                 array.splice.apply(array, [0, array.length].concat(items))
-            }
+            },
+
+            Delayer: Delayer
         }
     })
