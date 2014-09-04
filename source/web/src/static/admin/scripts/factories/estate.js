91ee9c3253c62a4ab5d68fd0c3dfb77c7c5bc731
Michael Chou <113563@qq.com>
2014-09-04 16:53:06 +0800

    [web] Mod: issue #4949 修改房产数据读取。

---
+++ source/web/src/static/admin/scripts/factories/estate.js
@@ -0,0 +1,27 @@
+/* Created by frank on 14-8-23. */
+(function () {
+
+    function estateApi($http) {
+
+        return {
+            getAll: function (config) {
+                return $http.get('/api/1/property/search', config)
+            },
+            getOne: function (id, config) {
+                return $http.get('/api/1/property/' + id, config)
+            },
+            update: function (data, config) {
+                return $http.post('/api/1/property/' + data.id + '/edit', data, config)
+            },
+            remove: function (id, config) {
+                return $http.post('/api/1/property/' + id + '/remove', null, config)
+            },
+            create: function (data, config) {
+                return $http.post('/api/1/property/add', data, config)
+            }
+        }
+
+    }
+
+    angular.module('app').factory('estateApi', estateApi)
+})()
