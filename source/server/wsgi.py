e96c6ec48c53178777ad2a14b6b2f3224b053757
ming zhang <kevinzhang@bbtechgroup.com>
2014-09-04 17:56:52 +0800

    [server]Add:add analytics_api_interface and modify user register api doc

--- source/server/wsgi.py
+++ source/server/wsgi.py
@@ -14,6 +14,8 @@ import property_api_interface
 import report_api_interface
 import subscription_api_interface
 import ticket_api_interface
+import analytics_api_interface
 import user_api_interface
+
 
 application = f_app(__name__)
