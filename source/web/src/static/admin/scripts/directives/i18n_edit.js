d83da047b66db8952eb37e10dc283166d33102cc
Frank Fang <frankfang1990@gmail.com>
2014-09-11 16:18:46 +0800

    add i18n

---
+++ source/web/src/static/admin/scripts/directives/i18n_edit.js
@@ -0,0 +1,10 @@
+/* Created by frank on 14-9-11. */
+angular.module('app')
+    .directive('i18nEdit', function () {
+        return {
+            restrict: 'AE',
+            link: function (scope, elm, attrs) {
+            }
+        }
+    })
+
