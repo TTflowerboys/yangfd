afb80844d9d77d742d070aa407c4c5e4e3d9f870
Frank Fang <frankfang1990@gmail.com>
2014-09-15 15:31:18 +0800

    [web]  Fix: Issue #4884 (finished)

--- source/web/src/static/admin/scripts/directives/change_estate_status.js
+++ source/web/src/static/admin/scripts/directives/change_estate_status.js
@@ -8,25 +8,23 @@ angular.module('app')
             scope: {
                 item: '=ngModel'
             },
-            link: function ($scope, elm, attrs) {
+            link: function (scope, elm, attrs) {
                 var user = userApi.getCurrentUser()
                 if (!user) { return }
                 var roles = user.role
                 if (_.contains(roles, 'admin') || _.contains(roles, 'jr_admin')) {
-                    $scope.estateStatus = estateStatus
-                } else if (_.contains(roles, 'developer') || _.contains(roles, 'agency')) {
-                    $scope.estateStatus = estateStatus.filter(function (one, index, array) {
-                        return one.value === 'selling' ||
-                            one.value === 'hidden' ||
-                            one.value === 'sold out' ||
-                            one.value === $scope.item.status
+                    scope.estateStatus = estateStatus
+                } else if ((_.contains(roles, 'developer') || _.contains(roles, 'agency')) &&
+                    _.contains(['selling', 'hidden', 'sold out'], scope.item.status)) {
+                    scope.estateStatus = estateStatus.filter(function (one, index, array) {
+                        return _.contains(['selling', 'hidden', 'sold out'], scope.item.status)
                     })
                 } else {
-                    $scope.estateStatus = estateStatus.filter(function (one, index, array) {
-                        return one.value === $scope.item.status
+                    scope.estateStatus = estateStatus.filter(function (one, index, array) {
+                        return one.value === scope.item.status
                     })
                 }
-                $scope.onUpdateStatus = function (item, newStatus) {
+                scope.onUpdateStatus = function (item, newStatus) {
                     estateApi.update({id: item.id, status: newStatus}, {successMessage: '增加权限操作成功', errorMessage: true})
                         .success(function () {
                             item.status = newStatus
