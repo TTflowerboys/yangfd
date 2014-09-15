c0586afc512506e4db7f93e79d55a43d3a0fef74
Frank Fang <frankfang1990@gmail.com>
2014-09-15 15:14:43 +0800

    [web]  Mod: 不同角色看到不同的状态。 Issue #4884

--- source/web/src/static/admin/scripts/config/errors.js
+++ source/web/src/static/admin/scripts/config/errors.js
@@ -3,6 +3,7 @@ angular.module('app')
     .constant('errors', {
         40103: 'Incorrect phone number or password',
         40100: '请先<a href="/admin#/signIn">登录</a>',
+        40300: '没有权限',
         404: 'API not found',
         unknown: 'Something is not right, please try later'
     })
