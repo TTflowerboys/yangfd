15597ebf33d8089bc3626dbf15480e572986804d
Frank Fang <frankfang1990@gmail.com>
2014-09-15 16:31:14 +0800

    [web] Add: support page Issue #4955

---
+++ source/web/src/static/admin/scripts/values/support_ticket_status.js
@@ -0,0 +1,9 @@
+/* Created by frank on 14-9-15. */
+angular.module('app')
+    .constant('supportTicketStatus', [
+        { name: '新提交', value: 'new' },
+        { name: '已指派', value: 'assigned' },
+        { name: '进行中', value: 'in_progress' },
+        { name: '已解决', value: 'solved' },
+        { name: '未解决', value: 'unsolved' }
+    ])
