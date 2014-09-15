2a6e2558300ad790280185feb948145fe5f8f12f
Frank Fang <frankfang1990@gmail.com>
2014-09-15 10:25:02 +0800

    [web]  add: type-i18n-unit

---
+++ source/web/src/static/admin/scripts/directives/type_i18n_unit.js
@@ -0,0 +1,18 @@
+/* Created by frank on 14-9-15. */
+angular.module('app')
+    .directive('typeI18nUnit', function ($parse, i18n_languages, $rootScope) {
+        return {
+            restrict: 'AE',
+            scope: {
+                model: '=typeI18nUnit'
+            },
+            link: function (scope, elm, attrs, ctrl) {
+                if (!scope.model) {scope.model = {}}
+                for (var i = 0, length = i18n_languages.length; i < length; i += 1) {
+                    scope.model.unit = scope.model.unit || ''
+                    scope.model.value = scope.model.value || ''
+                }
+            }
+        }
+    })
+
