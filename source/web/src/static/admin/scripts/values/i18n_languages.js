3d4aaf65373eddba715d9330cd4db1b55841026f
Frank Fang <frankfang1990@gmail.com>
2014-09-15 10:32:25 +0800

    [web]  add: hant

--- source/web/src/static/admin/scripts/values/i18n_languages.js
+++ source/web/src/static/admin/scripts/values/i18n_languages.js
@@ -3,8 +3,9 @@
  */
 angular.module('app')
     .constant('i18n_languages', [
-        { name: '中文（中国）', value: 'zh_Hans_CN' },
-        { name: 'English, UK', value: 'en_GB' },
+        { name: '简体中文(中国)', value: 'zh_Hans_CN' },
+        { name: '繁體中文(香港)', value: 'zh_Hant_HK' },
+        { name: 'English (UK)', value: 'en_GB' }
     ])
     .run(function ($rootScope, i18n_languages) {
         $rootScope.i18n_languages = i18n_languages
