df9b53e0745d29f6d8f4910b94ca718f3c94a966
Frank Fang <frankfang1990@gmail.com>
2014-09-17 16:22:23 +0800

    [web] Extend controller

---
+++ source/web/src/static/admin/scripts/controllers/property/list.js
@@ -0,0 +1,15 @@
+/* Created by frank on 14-9-17. */
+
+
+(function () {
+
+    function ctrlPropertyList($scope, $state) {
+
+        console.log($scope.api)
+    }
+
+    angular.module('app').controller('ctrlPropertyList', ctrlPropertyList)
+
+})()
+
+
