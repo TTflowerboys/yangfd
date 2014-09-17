07f32d76a0d5e071374c0cff302ca5f76784870b
Foster Yin <fyin@bbtechgroup.com>
2014-09-17 21:33:05 +0800

    [web] Add: click close announcement

---
+++ source/web/src/static/scripts/index.js
@@ -0,0 +1,8 @@
+$('#announcement').on('click', 'ul>li>.close', null, function (event) {
+    var $item = $(event.target.parentNode)
+    $item.remove()
+    var $container =$(event.delegateTarget)
+    if ($container.find('ul>li').length === 0) {
+        $container.hide()
+    }
+})
