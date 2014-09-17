84485ffbfc3bf3bccaf47719e10df1918057fd8d
Michael Chou <113563@qq.com>
2014-09-17 11:25:28 +0800

    [web] Mod: 添加平台咨询类别

--- source/web/src/static/admin/scripts/values/enum_types.js
+++ source/web/src/static/admin/scripts/values/enum_types.js
@@ -12,6 +12,7 @@ angular.module('app')
         { name: '学校年级', value: 'school_grade' },
         { name: '生活设施', value: 'facilities' },
         { name: '房产价格类型', value: 'property_price_type' },
+        { name: '平台咨询类别', value: 'news_category' },
     ]).run(function ($rootScope, enum_types) {
         $rootScope.enum_types = enum_types
     })
