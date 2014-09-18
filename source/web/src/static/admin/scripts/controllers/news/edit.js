f524c2c9b445cde72e422b8cde2e198e54248000
Frank Fang <frankfang1990@gmail.com>
2014-09-18 18:36:09 +0800

    [web] Update news edit page

--- source/web/src/static/admin/scripts/controllers/news/edit.js
+++
@@ -1,9 +0,0 @@
-/* Created by frank on 14-9-18. */
-(function () {
-
-    function ctrlNewsEdit($scope, $state, enumApi) {
-    }
-
-    angular.module('app').controller('ctrlNewsEdit', ctrlNewsEdit)
-
-})()
