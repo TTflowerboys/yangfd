c10f69f798bf3896b9c5ef2ca87b7a1107dd68fe
Michael Chou <113563@qq.com>
2014-09-19 15:27:20 +0800

    [web] Mod: 修改 房产详情。

--- source/web/src/static/admin/scripts/directives/type_i18n_unit.js
+++ source/web/src/static/admin/scripts/directives/type_i18n_unit.js
@@ -1,6 +1,6 @@
 /* Created by frank on 14-9-15. */
 angular.module('app')
-    .directive('typeI18nUnit', function ($parse, i18nLanguages, $rootScope) {
+    .directive('typeI18nUnit', function ($parse, $rootScope) {
         return {
             restrict: 'AE',
             templateUrl: '/static/admin/templates/type_i18n_unit.tpl.html',
@@ -13,9 +13,7 @@ angular.module('app')
             },
             link: function (scope, elm, attrs, ctrl) {
                 if (!scope.i18nUnit) {scope.i18nUnit = {}}
-                for (var i = 0, length = i18nLanguages.length; i < length; i += 1) {
-                    scope.i18nUnit.value = scope.i18nUnit.value || ''
-                }
+                scope.i18nUnit.value = scope.i18nUnit.value || ''
                 scope.isDefaultValue = true
                 scope.$watch('unit', function (value) {
                     if (scope.isDefaultValue===true) {
