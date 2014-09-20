8fb831f417a9a292992f34dea8a3cffd726fa532
Michael Chou <113563@qq.com>
2014-09-20 18:21:03 +0800

    [web] Mod: code review , update same code into directive

---
+++ source/web/src/static/admin/scripts/directives/add_property_items.js
@@ -0,0 +1,63 @@
+/**
+ * Created by Michael on 14/9/20.
+ */
+angular.module('app')
+    .directive('addPropertyItems', function ($rootScope) {
+        return {
+            restrict: 'AE',
+            link: function (scope) {
+
+                scope.addHighlight = function () {
+                    if (_.isEmpty(scope.item.highlight)) {
+                        scope.item.highlight = {}
+                    }
+                    if (!scope.item.highlight[$rootScope.userLanguage.value]) {
+                        scope.item.highlight[$rootScope.userLanguage.value] = []
+                    }
+                    var temp = scope.item.tempPoint
+                    scope.item.highlight[$rootScope.userLanguage.value].push(angular.copy(temp))
+                    scope.item.tempPoint = undefined
+                }
+
+                scope.addHistoricalPrice = function () {
+                    if (!scope.item.historical_price) {
+                        scope.item.historical_price = []
+                    }
+                    var temp = {time: scope.item.tempHistoryTime, price: scope.item.tempHistoryPrice}
+                    scope.item.historical_price.push(angular.copy(temp))
+                    scope.item.tempHistoryPrice.value = undefined
+                }
+
+                scope.addCost = function () {
+                    if (!scope.item.estimated_monthly_cost) {
+                        scope.item.estimated_monthly_cost = []
+                    }
+                    var temp = {item: scope.item.tempCostItem, price: scope.item.tempCostPrice}
+                    scope.item.estimated_monthly_cost.push(angular.copy(temp))
+                    scope.item.tempCostItem = {}
+                    scope.item.tempCostPrice.value = undefined
+                }
+
+                scope.addHouse = function () {
+                    if (!scope.item.main_house_types) {
+                        scope.item.main_house_types = []
+                    }
+                    var temp = {name: scope.item.tempHouseName,
+                        bedroom_count: scope.item.tempBedroomCount,
+                        living_room_count: scope.item.tempLivingRoomCount,
+                        bathroom_count: scope.item.tempBathroomCount,
+                        kitchen_count: scope.item.tempKitchenCount,
+                        total_price: angular.copy(scope.item.tempTotalPrice),
+                        floor_plan: scope.item.tempFloorPlan}
+                    scope.item.main_house_types.push(angular.copy(temp))
+                    scope.item.tempHouseName = {}
+                    scope.item.tempBedroomCount = undefined
+                    scope.item.tempLivingRoomCount = undefined
+                    scope.item.tempBathroomCount = undefined
+                    scope.item.tempKitchenCount = undefined
+                    scope.item.tempTotalPrice.value = undefined
+                    scope.item.tempFloorPlan = {}
+                }
+            }
+        }
+    })
--- source/web/src/static/admin/scripts/property/create.js
+++ source/web/src/static/admin/scripts/property/create.js
@@ -5,38 +5,6 @@
 
     function ctrlPropertyCreate($scope, $state, api, enumApi, $rootScope, i18nLanguages, misc) {
 
-        enumApi.getEnumsByType('property_type')
-            .success(function (data) {
-                $scope.propertyTypeList = data.val
-            })
-        enumApi.getEnumsByType('intention')
-            .success(function (data) {
-                $scope.intentionList = data.val
-            })
-        enumApi.getEnumsByType('equity_type')
-            .success(function (data) {
-                $scope.equityTypeList = data.val
-            })
-        enumApi.getEnumsByType('decorative_style')
-            .success(function (data) {
-                $scope.decorativeStyleList = data.val
-            })
-        enumApi.getEnumsByType('facing_direction')
-            .success(function (data) {
-                $scope.facingDirectionList = data.val
-            })
-        enumApi.getEnumsByType('property_price_type')
-            .success(function (data) {
-                $scope.propertyPriceTypeList = data.val
-            })
-        enumApi.getEnumsByType('country')
-            .success(function (data) {
-                $scope.systemCountries = data.val
-            })
-        enumApi.getEnumsByType('city')
-            .success(function (data) {
-                $scope.systemCities = data.val
-            })
         $scope.item = {}
 
         $scope.submit = function ($event, form) {
@@ -57,70 +25,6 @@
                 $scope.loading = false
             })
         }
-
-        $scope.addHighlight = function () {
-            if (_.isEmpty($scope.item.highlight)) {
-                $scope.item.highlight = {}
-            }
-            if (!$scope.item.highlight[$rootScope.userLanguage.value]) {
-                $scope.item.highlight[$rootScope.userLanguage.value] = []
-            }
-            var temp = $scope.item.tempPoint
-            $scope.item.highlight[$rootScope.userLanguage.value].push(angular.copy(temp))
-            $scope.item.tempPoint = undefined
-        }
-
-        $scope.addHistoricalPrice = function () {
-            if (!$scope.item.historical_price) {
-                $scope.item.historical_price = []
-            }
-            var temp = {time: $scope.item.tempHistoryTime, price: $scope.item.tempHistoryPrice}
-            $scope.item.historical_price.push(angular.copy(temp))
-            $scope.item.tempHistoryPrice.value = undefined
-        }
-
-        $scope.addCost = function () {
-            if (!$scope.item.estimated_monthly_cost) {
-                $scope.item.estimated_monthly_cost = []
-            }
-            var temp = {item: $scope.item.tempCostItem, price: $scope.item.tempCostPrice}
-            $scope.item.estimated_monthly_cost.push(angular.copy(temp))
-            $scope.item.tempCostItem = {}
-            $scope.item.tempCostPrice.value = undefined
-        }
-
-        $scope.addHouse = function () {
-            if (!$scope.item.main_house_types) {
-                $scope.item.main_house_types = []
-            }
-            var temp = {name: $scope.item.tempHouseName,
-                bedroom_count: $scope.item.tempBedroomCount,
-                living_room_count: $scope.item.tempLivingRoomCount,
-                bathroom_count: $scope.item.tempBathroomCount,
-                kitchen_count: $scope.item.tempKitchenCount,
-                total_price: angular.copy($scope.item.tempTotalPrice),
-                floor_plan: $scope.item.tempFloorPlan}
-            $scope.item.main_house_types.push(angular.copy(temp))
-            $scope.item.tempHouseName = {}
-            $scope.item.tempBedroomCount = undefined
-            $scope.item.tempLivingRoomCount = undefined
-            $scope.item.tempBathroomCount = undefined
-            $scope.item.tempKitchenCount = undefined
-            $scope.item.tempTotalPrice.value = undefined
-            $scope.item.tempFloorPlan = {}
-        }
-
-//        $scope.$watch('item.country', function (newValue) {
-//            console.log(newValue)
-//            if (newValue === undefined) {
-//                return
-//            }
-//            geoApi.getCitiesByCountry({params: {_i18n: 'disabled', country: newValue}})
-//                .success(function (data) {
-//                    console.log(data.val)
-//                    $scope.systemCities = data.val
-//                })
-//        })
     }
 
     angular.module('app').controller('ctrlPropertyCreate', ctrlPropertyCreate)
