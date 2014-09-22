1050ffc07b8e5ce42a3438b676bd9217047b9566
Felix Yan <felixonmars@gmail.com>
2014-09-22 19:02:04 +0800

    [server] Fix: wrong enum match

--- source/server/ticket_api_interface.py
+++ source/server/ticket_api_interface.py
@@ -121,10 +121,10 @@ def intention_ticket_add(params):
     ticket_id = f_app.ticket.add(params)
 
     if "budget" in params:
-        params["budget"] = f_app.util.match_enum({"id": params["budget"]["_id"]}).get("value")
+        params["budget"] = f_app.util.match_enum({"id": params["budget"]["_id"], "_enum": params["budget"]["_enum"]}).get("value")
     if "equity_type" in params:
-        params["equity_type"] = f_app.util.match_enum({"id": params["equity_type"]["_id"]}).get("value")
-    params["intention"] = [f_app.util.match_enum({"id": i["_id"]}).get("value") for i in params.get("intention", [])]
+        params["equity_type"] = f_app.util.match_enum({"id": params["equity_type"]["_id"], "_enum": params["equity_type"]["_enum"]}).get("value")
+    params["intention"] = [f_app.util.match_enum({"id": i["_id"], "_enum": i["_enum"]}).get("value") for i in params.get("intention", [])]
     if shadow_user_id is not None:
         f_app.user.counter_update(shadow_user_id)
 
