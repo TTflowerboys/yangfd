65296ad0f12b838db64643c1ddfc5d5cb03912cf
Michael Chou <113563@qq.com>
2014-09-22 16:42:13 +0800

    [web] Mod: 添加ticket列表。

---
+++ source/web/src/static/admin/scripts/filters/support_ticket_status_name.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Michael on 14/9/20.
+ */
+angular.module('app')
+    .filter('intentionTicketStatusName', function (misc, supportTicketStatus) {
+        return function (status) {
+            if (!status) { return }
+
+            var found = misc.findBy(supportTicketStatus, 'value', status)
+
+            return found ? found.name : ''
+        }
+    })
