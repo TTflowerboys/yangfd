0cb1c4bd94287ca9a96b39cd66f5b2e896ba8f70
Felix Yan <felixonmars@gmail.com>
2014-09-23 14:01:20 +0800

    [server] Mod: debug search params

--- source/server/f_currant.py
+++ source/server/f_currant.py
@@ -506,6 +506,7 @@ class f_property(f_app.module_base):
 
     def search(self, params, sort=["time", "desc"], notime=False, per_page=10, count=False):
         f_app.util.process_search_params(params)
+        self.logger.debug("search params: ", params)
         params.setdefault("status", "new")
         if sort is not None:
             try:
