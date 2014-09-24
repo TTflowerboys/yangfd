e8a6964c92afd8e8a1765e24be99252685c9d955
Luigi Fan <fdb713@gmail.com>
2014-09-24 06:32:11 +0000

    [server] Add: Task #5049

--- source/server/docs/index.rst
+++ source/server/docs/index.rst
@@ -30,6 +30,7 @@ API Reference
    ad_api
    analytics_api
    enum_api
+   message_api
    news_api
    property_api
    subscription_api
--- source/server/f_currant.py
+++ source/server/f_currant.py
@@ -387,7 +387,7 @@ class f_currant_plugins(f_app.plugin_base):
                     f_app.mongo_index.update(f_app.user.get_database, user_id, index_params.values())
 
     def post_add(self, params, post_id):
-        if {'_id': ObjectId('5417eca66b80992d07638186'), 'type': 'news_category', '_enum': 'news_category'} in params["category"]:
+        if {'_id': ObjectId(f_app.enum.get_by_slug('announcement')['id']), 'type': 'news_category', '_enum': 'news_category'} in params["category"]:
             # System
             message = {
                 "type": "system",
@@ -396,9 +396,9 @@ class f_currant_plugins(f_app.plugin_base):
             }
             user_list = f_app.user.search({"register_time": {"$ne": None}, "system_message_type": "system"})
             f_app.message.add(message, user_list)
-        if "zipcode_index" in params:
+        if "category" in params:
             # Favorite
-            related_property_list = f_app.property.search({"zipcode_index": params["zipcode_index"], "status": {"$in": ["selling", "sold out"]}})
+            related_property_list = f_app.property.search({"news_category": params["category"], "status": {"$in": ["selling", "sold out"]}})
             related_property_list = [ObjectId(property) for property in related_property_list]
             favorite_user_list = f_app.user.favorite.search({"property_id": related_property_list}, per_page=0)
             favorite_user_list = [_id for _id in favorite_user_list if "favorite" in f_app.user.get(_id).get("system_message_type", [])]
