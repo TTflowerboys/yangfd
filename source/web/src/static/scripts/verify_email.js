c015473bcbda1d03554650efb5851196c8fc25e5
Foster Yin <fyin@bbtechgroup.com>
2014-09-24 12:26:27 +0800

    [web] Add: Refine verify email request call, when user has email or not

--- source/web/src/static/scripts/verify_email.js
+++ source/web/src/static/scripts/verify_email.js
@@ -11,53 +11,38 @@ $('form[name=verifyEmail]').submit(function (e) {
     }})
 
     if (!valid) {return}
-    var params = $(this).serializeObject()    
+    var params = $(this).serializeObject()
 
-    if (window.user.email && window.user.email === params.email) {
-        $.post('/api/1/user/edit' , params)
-        .done(function (data) {
-            if (data.ret !== 0) {
+    function sendVerificationEmail() {
+        return team.wrapErrors($.post('/api/1/user/' + window.user.id + '/email_verification/send', {}))
+            .done(function (data) {
+                resultArea.text(window.i18n('邮件已发送，如果没有收到，请在60秒后重试'))
+                resultArea.show()                
+            })
+            .fail(function (data) {
+                resultArea.text(window.i18n('发送验证邮件失败'))
+                resultArea.show()
+            })
+    }
+
+    function updateUserEmail() {
+        return team.wrapErrors($.post('/api/1/user/edit', params))
+            .done (function (data) {
+                window.user = data
+            })
+            .fail (function (data) {
                 console.log(data)
                 resultArea.text(window.i18n('修改邮箱失败'))
-                resultArea.show()                
-            }
-            else {
-               $.post('/api/1/user/' + window.user.id + 'email_verification/send', {})
-                    .done(function (data) {
-                        if (data.ret !== 0) {
-                            resultArea.text(window.i18n('发送验证邮件失败'))
-                            resultArea.show()
-                        }
-                        else {
-                            resultArea.text(window.i18n('邮件已发送，如果没有收到，请在60秒后重试'))
-                            resultArea.show()
-                        }
-                    })
-                    .always(function () {
-                        
-                    })
-            }
-        })
-        .always(function () {
+                resultArea.show()
+            })
+    }
 
-        })
+    if (window.user.email && window.user.email === params.email) {
+        sendVerificationEmail()
     }
     else {
-         $.post('/api/1/user/' + window.user.id + 'email_verification/send', {})
-                    .done(function (data) {
-                        if (data.ret !== 0) {
-                            resultArea.text(window.i18n('发送验证邮件失败'))
-                            resultArea.show()
-                        }
-                        else {
-                            resultArea.text(window.i18n('邮件已发送，如果没有收到，请在60秒后重试'))
-                            resultArea.show()
-                        }
-                    })
-                    .always(function () {
-                        
-                    })
+        updateUserEmail().done(function(data){
+            sendVerificationEmail()
+        })
     }
-    
-    
 })
