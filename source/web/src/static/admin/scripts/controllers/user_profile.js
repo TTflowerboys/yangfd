1ff4d040365eac0e727f0025d4534baa6e640e2c
Arnold Wang <wangchao0721@gmail.com>
2014-09-25 13:40:40 +0800

    [web]Mod: issue #4967 change tab implemention to state

--- source/web/src/static/admin/scripts/controllers/user_profile.js
+++ source/web/src/static/admin/scripts/controllers/user_profile.js
@@ -6,15 +6,11 @@
     function ctrlUserProfile($scope, $state , api, misc) {
         $scope.api = api
 
-
         if($state.current.url === '/:id'){
             // Refer to favs if someone enter url to access
             $state.go('.favs')
-            $scope.tabName = '/favs'
-        }else{
-            // Update tab active based on url
-            $scope.tabName = $state.current.url
         }
+
 
         //Get User Profile Information
         var itemFromParent = misc.findById($scope.$parent.list, $state.params.id)
@@ -27,9 +23,6 @@
                 })
         }
 
-        $scope.changeTab = function (tabName) {
-            $scope.tabName = tabName
-        }
     }
 
     angular.module('app').controller('ctrlUserProfile', ctrlUserProfile)
