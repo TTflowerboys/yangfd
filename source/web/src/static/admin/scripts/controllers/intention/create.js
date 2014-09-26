00cc05d765f7afa4ff83b7e6799f71a01256c818
Michael Chou <113563@qq.com>
2014-09-26 18:20:51 +0800

    [web] Mod: 添加 添加电话号码 directive

--- source/web/src/static/admin/scripts/controllers/intention/create.js
+++ source/web/src/static/admin/scripts/controllers/intention/create.js
@@ -25,44 +25,7 @@
             })
         }
 
-        $scope.checkPhone = function () {
-            checkPhone($scope.item.phoneCountry, $scope.item.phoneNumber).success(function (data) {
-                $scope.item.phone = data.val
-                getUserByPhone($scope.item.phoneCountry, $scope.item.phoneNumber)
-                    .success(function (data) {
-                        var res = data.val
-                        if (_.isArray(res) && res.length > 0) {
-                            $scope.item.userId = data.val[0].id
-                            console.log($scope.item.userId)
-                        } else {
-                            $scope.item.userId = undefined
-                        }
-                    }).error(function (data) {
-                        $scope.item.userId = undefined
-                    })
-            }).error(function () {
-                $scope.item.userId = undefined
-            })
 
-        }
-
-        function checkPhone(country, phone) {
-            return $http.get('/api/1/user/phone_test', {
-                params: {
-                    country: country,
-                    phone: phone
-                }
-            })
-        }
-
-        function getUserByPhone(country, phone) {
-            return $http.get('/api/1/user/admin/search', {
-                params: {
-                    country: country,
-                    phone: phone
-                }
-            })
-        }
 
     }
 
---
+++ source/web/src/static/admin/scripts/directives/add_ticket_phone.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Michael on 14/9/26.
+ */
+angular.module('app')
+    .directive('addTicketPhone', function ($http, $filter) {
+        return {
+            restrict: 'AE',
+            templateUrl: '/static/admin/templates/add_ticket_phone.tpl.html',
+            scope: {
+                phone: '=ngModel',
+                userId: '=userId'
+            },
+            link: function (scope) {
+                scope.checkPhone = function (country,phone) {
+                    console.log(country)
+                    console.log(phone)
+                    checkPhone(country, phone).success(function (data) {
+                        scope.phone = data.val
+                        getUserByPhone(country, phone)
+                            .success(function (data) {
+                                var res = data.val
+                                if (_.isArray(res) && res.length > 0) {
+                                    scope.userId = data.val[0].id
+                                } else {
+                                    scope.userId = undefined
+                                }
+                            }).error(function (data) {
+                                scope.userId = undefined
+                            })
+                    }).error(function () {
+                        scope.userId = undefined
+                    })
+                }
+
+                scope.cleanPhone = function () {
+                    scope.phone =undefined
+                    scope.userId = undefined
+
+                }
+
+                function checkPhone(country, phone) {
+                    return $http.get('/api/1/user/phone_test', {
+                        params: {
+                            country: country,
+                            phone: phone
+                        }
+                    })
+                }
+
+                function getUserByPhone(country, phone) {
+                    return $http.get('/api/1/user/admin/search', {
+                        params: {
+                            country: country,
+                            phone: phone
+                        }
+                    })
+                }
+            }
+        }
+    })
