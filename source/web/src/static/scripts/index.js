139003b91f5c3f7a2bdaa3d04b2ea4f486ce3ce6
Foster Yin <fyin@bbtechgroup.com>
2014-10-02 11:12:37 +0800

    [web] Add: Make intention tabs can collapse

--- source/web/src/static/scripts/index.js
+++ source/web/src/static/scripts/index.js
@@ -146,11 +146,11 @@
         })
 
         $intentionTag.find('#add').click(function () {
-            //$('.intentionTabs_wrapper').trigger('open')
+            $('.intentionTabs_wrapper').animate({height:'530px'}, 400, 'swing')
         })
 
         $('.intentionTabs_wrapper').find('#collapseButton').click(function () {
-            //$('.intentionTabs_wrapper').trigger('close')
+            $('.intentionTabs_wrapper').animate({height:'0'}, 400, 'swing')
         })
         loadPropertyList()
     }
