dcc6f779114a5e1096cc81feb087cb64ee87fe0e
Arnold Wang <wangchao0721@gmail.com>
2014-10-02 16:49:48 +0800

    [web]Add: #4973 add news category selector

---
+++ source/web/src/static/admin/scripts/controllers/operation_news.js
@@ -0,0 +1,37 @@
+/**
+ * Created by chaowang on 10/02/14.
+ */
+(function () {
+
+    function ctrlOperationNews($scope) {
+        newsApi = $scope.$parent.api
+        //Work around angular can not watch primitive type
+        $scope.selected = {}
+        $scope.selected.category = {}
+
+        var params = {
+            per_page: $scope.perPage
+        }
+
+        /*        $scope.$watch('newsCategoryList', function (value) {
+         console.log(value)
+         if(!_.isUndefined(value)&&value.length>0){
+         $scope.selected.category = value[0]
+         }
+         })*/
+
+        $scope.$watch('selected.category',function(newValue, oldValue){
+            // Ignore initial setup.
+            if ( newValue === oldValue ) {
+                return
+            }
+
+            params.category = $scope.selected.category.id
+            newsApi.getAll({ params: params }).success($scope.onGetList)
+        },true)
+    }
+
+    angular.module('app').controller('ctrlOperationNews', ctrlOperationNews)
+
+})()
+
