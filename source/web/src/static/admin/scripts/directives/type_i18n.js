9c04472063a582a361c5452ac99823b0d279686d
Frank Fang <frankfang1990@gmail.com>
2014-10-03 21:59:53 +0800

    [web]  update

--- source/web/src/static/admin/scripts/directives/type_i18n.js
+++ source/web/src/static/admin/scripts/directives/type_i18n.js
@@ -1,12 +1,12 @@
 /* Created by frank on 14-9-12. */
 angular.module('app')
-    .directive('typeI18n', function ($parse, i18nLanguages, $rootScope) {
+    .directive('typeI18n', function (i18nLanguages, $compile) {
         return {
             restrict: 'AE',
             scope: {
                 model: '=typeI18n'
             },
-            link: function (scope, elm, attrs, ctrl) {
+            link: function (scope, element, attrs, ctrl) {
                 if (!scope.model) {scope.model = {}}
                 for (var i = 0, length = i18nLanguages.length; i < length; i += 1) {
                     scope.model[i18nLanguages[i].value] = scope.model[i18nLanguages[i].value] || ''
