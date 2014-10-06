fbef37d0667c1b1fbc8fa5106bf77cae811f8094
Frank Fang <frankfang1990@gmail.com>
2014-10-06 16:12:24 +0800

    [web] add data type currency

---
+++ source/web/src/static/scripts/data_type.js
@@ -0,0 +1,23 @@
+/* Created by frank on 14-10-6. */
+(function ($) {
+    $('[data-type]').on('keyup', function (e) {
+        if (e.keyCode >= '48' && e.keyCode <= 57) {
+            checkType(this)
+        }
+    }).each(function () {
+        checkType(this)
+    })
+
+    function checkType(dom) {
+        var $dom = $(dom)
+        var type = $dom.attr('data-type')
+        if (type === 'currency') {
+            $dom.val(
+                team.encodeCurrency(
+                    $dom.val()
+                )
+            )
+        }
+    }
+
+})(jQuery)
