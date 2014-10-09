b2d607f9ae327c64dcc6c4ede2ccef8c34be6501
Frank Fang <frankfang1990@gmail.com>
2014-10-09 17:20:56 +0800

    [web] update user bought api

--- source/web/src/static/admin/scripts/factories/user_bought_api.js
+++ source/web/src/static/admin/scripts/factories/user_bought_api.js
@@ -1,15 +1,14 @@
 /* Created by frank on 14-8-23. */
 (function () {
 
-    function userBoughtApi($http,$stateParams) {
+    function userBoughtApi($http, $stateParams) {
         return {
             getAll: function (id, config) {
                 config = config || {}
-                config.params = config.params || {}
-                config.params = {
+                config.params = angular.extend({}, config.params, {
                     user_id: $stateParams.id,
                     status: 'bought'
-                }
+                })
                 return $http.get('/api/1/intention_ticket/search', config)
             }
         }
