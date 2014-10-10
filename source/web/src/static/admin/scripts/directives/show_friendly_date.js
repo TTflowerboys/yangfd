426b48d2539f93e8cbf46d37b7a6a5f90a724e66
Frank Fang <frankfang1990@gmail.com>
2014-10-10 11:35:49 +0800

    [web] Rename

--- source/web/src/static/admin/scripts/directives/show_friendly_date.js
+++ source/web/src/static/admin/scripts/directives/show_friendly_date.js
@@ -1,7 +1,7 @@
 /* Created by frank on 14-8-18. */
 
 angular.module('app')
-    .directive('friendlyDate', function () {
+    .directive('showFriendlyDate', function () {
 
         return {
             require: 'ngModel',
