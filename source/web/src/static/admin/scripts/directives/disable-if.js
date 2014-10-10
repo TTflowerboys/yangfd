76c9d6945ad142218ce3ae49b33c278b210e725d
Frank Fang <frankfang1990@gmail.com>
2014-10-10 15:12:35 +0800

    [web] Rename disabler

--- source/web/src/static/admin/scripts/directives/disable-if.js
+++ source/web/src/static/admin/scripts/directives/disable-if.js
@@ -1,10 +1,10 @@
 /* Created by frank on 14-8-18. */
 angular.module('app')
-    .directive('bbgDisabler', function () {
+    .directive('disableIf', function () {
         return {
             link: function (scope, elm, attrs) {
                 var initialValue = elm.prop('value')
-                scope.$watch(attrs.bbgDisabler, function (value) {
+                scope.$watch(attrs.disableIf, function (value) {
                     if (value) {
                         elm.val('Loading...')
                         elm.prop('disabled', true)
