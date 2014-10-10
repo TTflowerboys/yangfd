38d4d11b854032950134e00f3d9ea5ce13c13be4
Frank Fang <frankfang1990@gmail.com>
2014-10-10 11:38:54 +0800

    [web] Rename get_new_category

--- source/web/src/static/admin/scripts/directives/get_news_category_list.js
+++ source/web/src/static/admin/scripts/directives/get_news_category_list.js
@@ -1,7 +1,7 @@
 /* Created by frank on 14-9-17. */
 
 angular.module('app')
-    .directive('getNewsCategory', function (enumApi) {
+    .directive('getNewsCategoryList', function (enumApi) {
         return {
             link: function (scope, element, attrs) {
                 enumApi.getOriginEnumsByType('news_category', {params: {_i18n: 'disabled'}}).success(function (data) {
