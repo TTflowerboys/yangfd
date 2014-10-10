3200950675dce75f735fa1d9dcb5234ea20d2c21
Frank Fang <frankfang1990@gmail.com>
2014-10-10 14:55:30 +0800

    [web] Refactor

--- source/web/src/static/admin/scripts/directives/one_screen.js
+++ source/web/src/static/admin/scripts/directives/one_screen.js
@@ -3,10 +3,20 @@ angular.module('app')
     .directive('oneScreen', function () {
         return {
             link: function (scope, elm, attrs) {
-                $(elm).css('maxHeight', $(window).height() - $('#header').height())
-                $(window).on('resize', function () {
-                    $(elm).css('maxHeight', $(window).height() - $('#header').height())
-                })
+
+                recalculateMinHeight()
+
+                $(window).on('resize', recalculateMinHeight)
+
+                function recalculateMinHeight() {
+                    var $elm = $(elm)
+                    var siblingsHeight = 0
+                    $elm.siblings().each(function (index, dom) {
+                        siblingsHeight += $(dom).height()
+                    })
+                    $elm.css({minHeight: $(window).height() - siblingsHeight})
+
+                }
             }
         }
     })
