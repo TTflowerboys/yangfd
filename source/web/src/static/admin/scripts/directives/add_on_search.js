d3553631485039636cbc0b5cbfe0ba287dbea58d
Frank Fang <frankfang1990@gmail.com>
2014-10-10 15:02:59 +0800

    [web] Rename search query

--- source/web/src/static/admin/scripts/directives/add_on_search.js
+++ source/web/src/static/admin/scripts/directives/add_on_search.js
@@ -1,9 +1,10 @@
 /* Created by chaowang on 14-9-23. */
 angular.module('app')
-    .directive('searchQuery', function () {
+    .directive('addOnSearch', function () {
         return {
             restrict: 'AE',
             templateUrl: '/static/admin/templates/search_user_query.tpl.html',
+            replace: true,
             link: function ($scope, elm, attrs) {
                 $scope.onSearch = function (searchText) {
                     var param = {}
