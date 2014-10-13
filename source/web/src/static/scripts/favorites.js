a74dbda0d8b56052258e7f6900c2ea0ee4fb4f5a
Foster Yin <fyin@bbtechgroup.com>
2014-10-13 15:52:39 +0800

    [web] Add: Bug #5151 User Favorite change to ajax

--- source/web/src/static/scripts/favorites.js
+++ source/web/src/static/scripts/favorites.js
@@ -1,4 +1,15 @@
 
+$(function () {
+
+    var favoriteList = JSON.parse($('#favoriteData').text())
+
+    _.each(favoriteList, function (fav) {
+        var houseResult = _.template($('#houseCard_template').html())({fav: fav})
+        $('#list').append(houseResult)
+    })
+    
+})
+
 $('.list').on('click', '.houseCard #cancelFavorite', function (event) {
     var favoriteId = $(event.currentTarget).attr('data-id')
     $.betterPost('/api/1/user/favorite/' + favoriteId + '/remove')
@@ -17,8 +28,16 @@ $('.list').on('click', '.houseCard #undoFavorite', function (event) {
     var propertyId = $(event.currentTarget).attr('data-property-id')
     $.betterPost('/api/1/user/favorite/add', {'property_id':propertyId})
         .done(function (data) {
-            $(event.currentTarget).parent().hide()
-            location.reload()
+            var favId = data
+            $.betterPost('/api/1/user/favorite/' + favId)
+                .done(function (data) {
+                    var fav = data
+                    var houseResult = _.template($('#houseCard_template').html())({fav: fav})
+                    $('.houseCard[data-property-id=' + propertyId + ']').replaceWith(houseResult)
+                })
+		.fail(function (ret) {
+                    window.alert(window.i18n('撤销失败'))
+		})
         })
         .fail (function (ret) {
             window.alert(window.i18n('撤销失败'))
