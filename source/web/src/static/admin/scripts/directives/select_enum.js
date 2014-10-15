d710436d14e6af70cacb7a3fbd35883e9ed92277
Michael Chou <113563@qq.com>
2014-10-15 16:50:02 +0800

    [web] Mod: 修改 select enum， 将 enum ID 的判断改为 slug

--- source/web/src/static/admin/scripts/directives/select_enum.js
+++ source/web/src/static/admin/scripts/directives/select_enum.js
@@ -10,7 +10,8 @@ angular.module('app')
             scope: {
                 enumId: '=ngModel',
                 enumType: '@name',
-                enumOption: '@text'
+                enumOption: '@text',
+                enumSlug: '=?slug'
             },
             link: function (scope) {
                 scope.userLanguage = $rootScope.userLanguage
@@ -18,6 +19,20 @@ angular.module('app')
                     .success(function (data) {
                         scope.enumList = data.val
                     })
+                scope.changeSlug = function () {
+
+                    if(_.isEmpty(scope.enumId)){
+                        scope.enumSlug = undefined
+                        return
+                    }
+                    for(var p in scope.enumList){
+                        if(scope.enumList[p].id===scope.enumId){
+                            scope.enumSlug = scope.enumList[p].slug
+                            return
+                        }
+                    }
+
+                }
             }
         }
     })
