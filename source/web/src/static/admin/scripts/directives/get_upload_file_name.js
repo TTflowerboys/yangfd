8c710c9eedf94125de3f1d7080c7ebfa9f5f488d
Michael Chou <113563@qq.com>
2014-10-16 16:14:57 +0800

    [web] Mod: news 上传 图片

---
+++ source/web/src/static/admin/scripts/directives/get_upload_file_name.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Michael on 14/10/16.
+ */
+angular.module('app')
+    .directive('getUploadFileName', function ($upload) {
+        return {
+            restrict: 'AE',
+            templateUrl: '/static/admin/templates/get_upload_file_name.tpl.html',
+            replace: true,
+            scope: {
+                widthLimit: '@widthLimit',
+                ratio: '@ratio',
+                thumbnailSize: '@thumbnailSize',
+                text: '@text'
+            },
+            link: function (scope, elm, attrs) {
+                scope.onFileSelected = function ($files) {
+                    var file = $files[0]
+                    if (file) {
+                        $upload.upload({
+                            url: '/api/1/upload_image',
+                            file: file,
+                            fileFormDataName: 'data',
+                            data: {
+                                width_limit: scope.widthLimit || 1920,
+                                ratio: scope.ratio || 1,
+                                thumbnail_size: scope.thumbnailSize || '400,400',
+                                filename: file.name
+                            }
+                        })
+                            .success(function (data, status, headers, config) {
+                                scope.image = data.val.url
+                            })
+                    }
+                }
+            }
+        }
+    })
