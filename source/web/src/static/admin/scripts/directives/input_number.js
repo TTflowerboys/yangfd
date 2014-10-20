7c99d9b1726620f010eb81ddde32b47f5e875861
Michael Chou <113563@qq.com>
2014-10-20 18:13:56 +0800

    [web] Add: 添加数字录入 directive. 使其能够使用逗号分隔，并四舍五入。

---
+++ source/web/src/static/admin/scripts/directives/input_number.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Michael on 14/10/20.
+ */
+angular.module('app')
+    .directive('inputNumber', function ($filter) {
+        return {
+            restrict: 'AE',
+            template: '<input type="text" ng-model="tempValue" placeholder="{%placeholder%}" class="form-control ">',
+            replace: true,
+            scope: {
+                value: '=value',
+                placeholder: '@placeholder'
+            },
+            link: function (scope) {
+                var oldValue
+                scope.$watch('tempValue', function (newValue) {
+                    if (_.isEmpty(newValue)) {
+                        scope.value = undefined
+                        return
+                    }
+                    if (oldValue === newValue || oldValue + '.' === newValue) {
+                        return
+                    }
+                    var array = newValue.split('.')
+                    var res
+                    if (array) {
+                        var n1 = array[0].replace(/[^0-9]/g, '')
+                        res = $filter('number')(n1)
+                        if (array.length > 1) {
+                            if (array[1]) {
+                                var n2 = array[1].replace(/[^0-9]/g, '')
+                                var n2Length = n2.length
+                                if (n2Length > 2) {
+                                    n2Length = 2;
+                                }
+                                var n3 = n1 + '.' + n2;
+                                res = $filter('number')(n3, n2Length)
+                                scope.value = res.replace(/,/g, '')
+                            } else {
+                                res += '.'
+                                scope.value = n1
+                            }
+                        } else {
+                            scope.value = n1
+                        }
+                        oldValue = res
+                        scope.tempValue = res
+                    }
+                })
+            }
+        }
+    })
+
