407ebaa8aa82a0afc0e67f42618c5ddf82589eb2
Frank Fang <frankfang1990@gmail.com>
2014-10-20 18:49:17 +0800

    [web] Alert error code

--- source/web/src/static/admin/scripts/config/http_interceptor.js
+++ source/web/src/static/admin/scripts/config/http_interceptor.js
@@ -10,11 +10,11 @@ angular.module('app')
                 errorCode = response.data.ret
             }
             if (errorCode === 0) { return }
-            return response.data.debug_msg//TODO
-//            if (!response.config.errorMessage) {return}
-//
-//            return response.config.errorMessage[errorCode] || errors[errorCode] ||
-//                errors.unknown + ' Error code: ' + errorCode
+
+            if (!response.config.errorMessage) {return}
+
+            return response.data.debug_msg || response.config.errorMessage[errorCode] || errors[errorCode] ||
+                errors.unknown + ' Error code: ' + errorCode
         }
 
         $provide.factory('myHttpInterceptor', function ($q, growl, $rootScope, $sce) {
