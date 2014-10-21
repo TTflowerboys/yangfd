1e649c948d23d98855d244f8ea027a2ef977fab9
Foster Yin <fyin@bbtechgroup.com>
2014-10-21 18:08:56 +0800

    [web] Add: Task #5240 Update Sign up js call

---
+++ source/web/src/static/scripts/signup.js
@@ -0,0 +1,56 @@
+$(function () {
+
+    function showRecaptcha(containerId) {
+
+        $.betterPost('/api/1/captcha/generate', {})
+            .done(function (data) {
+
+            })
+            .fail(function (ret) {
+            })
+            .always(function () {
+
+            })
+
+    }
+
+    function getRecaptchaChallenge() {
+
+        return  ''
+    }
+
+    function getRecaptchaResponse() {
+        return  ''
+    }
+
+
+    showRecaptcha('captcha_div')
+    var errorArea = $('form[name=register]').find('.errorMessage')
+    $('form[name=register]').submit(function (e) {
+        e.preventDefault()
+        errorArea.hide()
+
+        var valid = $.validate(this, {onError: function (dom, validator, index) {
+            errorArea.text(window.getErrorMessage(dom.name, validator))
+            errorArea.show()
+        }})
+
+        if (!valid) {
+            return
+        }
+
+        var params = $(this).serializeObject()
+        params.password = Base64.encode(params.password)
+        params.challenge = getRecaptchaChallenge()
+        params.solution = getRecaptchaResponse()
+        $.betterPost('/api/1/user/register', params)
+            .done(function () {
+                window.project.goToIntention()
+            }).fail(function (ret) {
+                errorArea.append(window.getErrorMessageFromErrorCode(ret))
+            }).always(function () {
+                //refresh it for may user submit fail, or submit again with another account
+                showRecaptcha()
+            })
+    })
+})
