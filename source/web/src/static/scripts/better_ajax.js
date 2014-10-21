127ed26b3d39358b61b138f5806005967c4e18f3
Frank Fang <frankfang1990@gmail.com>
2014-10-21 14:50:32 +0800

    [web] extract better_ajax.js

---
+++ source/web/src/static/scripts/better_ajax.js
@@ -0,0 +1,30 @@
+/* Created by frank on 14/10/21. */
+$.each(['Post', 'Get'], function (index, key) {
+    var newName = 'better' + key
+    var oldName = key.toLowerCase()
+    var hiddenName = '_' + oldName
+    $[hiddenName] = $[oldName]
+    $[oldName] = function () {
+        if (window.console) {
+            window.console.log(['Please use', '$.' + newName + '()', 'instead of', '$.' + oldName + '()'].join(' '))
+        }
+        return $[hiddenName].apply($, arguments)
+    }
+    $['better' + key] = function () {
+        var deferred = $.Deferred()
+        $[hiddenName].apply($, arguments).done(function (data, textStatus, jqXHR) {
+            if (data.ret !== undefined) {
+                if (data.ret === 0) {
+                    deferred.resolve(data.val)
+                } else {
+                    deferred.reject(data.ret)
+                }
+            } else {
+                deferred.resolve(data, textStatus, jqXHR)
+            }
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            deferred.reject(jqXHR.status)
+        })
+        return deferred.promise()
+    }
+})
