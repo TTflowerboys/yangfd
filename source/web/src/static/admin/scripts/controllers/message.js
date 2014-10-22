c5c9c5a801f6b21c189c3d43c3511e1ad9140e00
Michael Chou <113563@qq.com>
2014-10-22 15:24:02 +0800

    [web] Mod: issue #5067 修改 消息列表，按分类展示。。

--- source/web/src/static/admin/scripts/controllers/message.js
+++ source/web/src/static/admin/scripts/controllers/message.js
@@ -4,22 +4,107 @@
 
 
 (function () {
+    function ctrlMessageList($scope, $state, $http, $rootScope, $stateParams, fctModal, $timeout, api) {
+        $scope.list = []
+        $scope.perPage = 12
+        $scope.currentPageNumber = 1
+        $scope.pages = []
+        $scope.api = api
+        $scope.fetched = false
+        $scope.currentType = 'system'
 
-    function ctrlMessageList($scope, $rootScope, $state, $stateParams, api) {
-        $scope.messageList = []
-        $scope.item = {}
+        var params = {
+            per_page: $scope.perPage,
+            type: $scope.currentType,
+            status: 'new',
+            _i18n: 'disabled'
+        }
 
-        $scope.getMessages = function () {
-            for (var i = 0; i < $rootScope.messageTypes.length; i += 1) {
-                getMessageByIndex(i)
+        api.search({ params: params }).success(onGetList)
+
+        $scope.refreshList = function () {
+            api.search({ params: params}).success(onGetList)
+        }
+
+        $scope.onRemove = function (item) {
+            fctModal.show('Do you want to remove it?', undefined, function () {
+                api.remove(item.id).success(function () {
+                    $scope.list.splice($scope.list.indexOf(item), 1)
+                })
+            })
+        }
+
+        $scope.nextPage = function () {
+            var lastItem = $scope.list[$scope.list.length - 1]
+            if (lastItem.time) {
+                params.time = lastItem.time
+            }
+            if (lastItem.register_time) {
+                params.register_time = lastItem.register_time
+            }
+            if (lastItem.insert_time) {
+                params.insert_time = lastItem.insert_time
+            }
+
+            api.search({params: params})
+                .success(function () {
+                    $scope.currentPageNumber += 1
+                })
+                .success(onGetList)
+
+        }
+        $scope.prevPage = function () {
+
+            var prevPrevPageNumber = $scope.currentPageNumber - 2
+            var prevPrevPageData
+            var lastItem
+            if (prevPrevPageNumber >= 1) {
+                prevPrevPageData = $scope.pages[prevPrevPageNumber]
+                lastItem = prevPrevPageData[prevPrevPageData.length - 1]
+            }
+
+            if (lastItem) {
+                if (lastItem.time) {
+                    params.time = lastItem.time
+                }
+                if (lastItem.register_time) {
+                    params.register_time = lastItem.register_time
+                }
+                if (lastItem.insert_time) {
+                    params.insert_time = lastItem.insert_time
+                }
+            } else {
+                delete params.time
+                delete params.register_time
+                delete params.insert_time
+            }
+
+            api.search({params: params})
+                .success(function () {
+                    $scope.currentPageNumber -= 1
+                })
+                .success(onGetList)
+        }
+
+        $scope.onGetList = onGetList
+
+        function onGetList(data) {
+            $scope.fetched = true
+            $scope.list = data.val
+            $scope.pages[$scope.currentPageNumber] = $scope.list
+
+            if (!$scope.list || $scope.list.length < $scope.perPage) {
+                $scope.noNext = true
+            } else {
+                $scope.noNext = false
+            }
+            if ($scope.currentPageNumber <= 1) {
+                $scope.noPrev = true
+            } else {
+                $scope.noPrev = false
             }
         }
-        function getMessageByIndex(index) {
-            api.getAll({params: {type: $rootScope.messageTypes[index].value, status: 'new', _i18n: 'disabled'}})
-                .success(function (data) {
-                    $scope.messageList[index] = data.val || {}
-                })
-        }
+
     }
 
     angular.module('app').controller('ctrlMessageList', ctrlMessageList)
