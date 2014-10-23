a5dd9e6918eac32eb715f43542177c2075bb1374
ChaoWang <wangchao0721@gmail.com>
2014-10-23 18:44:10 +0800

    [web]Mod: update permisson for jr_operation

--- source/web/src/static/admin/scripts/directives/edit_property_status.js
+++ source/web/src/static/admin/scripts/directives/edit_property_status.js
@@ -14,12 +14,11 @@ angular.module('app')
                     return
                 }
                 var roles = user.role
-                if (_.contains(roles, 'admin') || _.contains(roles, 'jr_admin')) {
+                if (_.contains(roles, 'admin') || _.contains(roles, 'jr_admin') || _.contains(roles, 'operation')) {
                     scope.propertyStatus = propertyStatus
-                } else if ((_.contains(roles, 'developer') || _.contains(roles, 'agency')) &&
-                    _.contains(['selling', 'hidden', 'sold out'], scope.item.status)) {
+                } else if (_.contains(roles, 'jr_operation')) {
                     scope.propertyStatus = propertyStatus.filter(function (one, index, array) {
-                        return _.contains(['selling', 'hidden', 'sold out'], scope.item.status)
+                        return _.contains(['draft', 'not translated', 'translating', 'not reviewed'], one.value)||one.value === scope.item.status
                     })
                 } else {
                     scope.propertyStatus = propertyStatus.filter(function (one, index, array) {
@@ -27,7 +26,7 @@ angular.module('app')
                     })
                 }
                 scope.onUpdateStatus = function (item, newStatus) {
-                    propertyApi.update({id: item.id, status: newStatus}, {successMessage: '增加权限操作成功', errorMessage: true})
+                    propertyApi.update({id: item.id, status: newStatus}, {successMessage: '操作成功', errorMessage: true})
                         .success(function () {
                             item.status = newStatus
                         })
