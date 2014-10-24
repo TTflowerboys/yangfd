dee58c729ee906fee696f928912296a4169cb2a2
Michael Chou <113563@qq.com>
2014-10-24 15:53:22 +0800

    [web] Mod: issue #5174 修改，销售状态查看，按照楼盘ID 查询。

--- source/web/src/static/admin/scripts/controllers/plot.js
+++ source/web/src/static/admin/scripts/controllers/plot.js
@@ -3,27 +3,35 @@
  */
 (function () {
 
-    function ctrlPlotList($scope, $state, $http, $rootScope, $stateParams, fctModal, propertyApi, api) {
-
-
-
+    function ctrlPlotList($scope, $state, $http, $rootScope, $stateParams, fctModal, propertyApi, enumApi, api) {
+        $scope.item = {}
 
         $scope.list = []
-        $scope.perPage = 12
         $scope.currentPageNumber = 1
         $scope.pages = []
         $scope.api = api
         $scope.fetched = false
+        $scope.propertyList
 
-        var params = {
-            per_page: $scope.perPage
-        }
-//        propertyApi.search().success()
+        enumApi.getEnumsByType('property_type').success(function (data) {
+            var list = data.val
+            var res
+            for (var item in list) {
+                if (list[item].slug === 'new_property' || list[item].slug === 'student_housing') {
+                    if (res) {
+                        res += ',' + list[item].id
+                    } else {
+                        res = list[item].id
+                    }
+                }
+            }
+            propertyApi.getAll({params: {property_type: res, status: 'selling'}}).success(function (data) {
+                $scope.propertyList = data.val.content
+            })
+        })
 
-        api.search({ params: params }).success(onGetList)
-
-        $scope.refreshList = function () {
-            api.search({ params: params}).success(onGetList)
+        $scope.onPropertyChange = function () {
+            api.search({ params: {property_id: $scope.item.propertyId}}).success(onGetList)
         }
 
         $scope.onRemove = function (item) {
