da64ac71259da8853bab666a3996471813c5e010
Foster Yin <fyin@bbtechgroup.com>
2014-10-24 18:37:12 +0800

    [web] Add: Uniform all house card related template to js template

---
+++ source/web/src/static/scripts/user_intentions.js
@@ -0,0 +1,19 @@
+$(function () {
+    //reload data or setup empty place holder
+    var ticketArray = JSON.parse($('#dataTicketList').text())
+    if (_.isEmpty(ticketArray)) {
+        $('#emptyPlaceHolder').show()
+    }
+    else {
+        _.each(ticketArray, function (ticket) {
+            if (ticket.property) {
+                var houseResult = _.template($('#houseCard_template').html())({ticket: ticket})
+                $('#list').append(houseResult)
+            }
+            else {
+                var intentionResult = _.template($('#intentionCard_template').html())({ticket: ticket})
+                $('#list').append(intentionResult)
+            }
+        })
+    }
+})
