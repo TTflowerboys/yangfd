7f3fbb3d1b61b880357bd4bd52be1ce46a706f08
Foster Yin <fyin@bbtechgroup.com>
2014-10-29 19:15:51 +0800

    [web] Add: Task #5286  Mobile Nav Bar

---
+++ source/web/src/static/scripts/responsivemobilemenu.js
@@ -0,0 +1,98 @@
+/*
+
+ Responsive Mobile Menu v1.0
+ Plugin URI: responsivemobilemenu.com
+
+ Author: Sergio Vitov
+ Author URI: http://xmacros.com
+
+ License: CC BY 3.0 http://creativecommons.org/licenses/by/3.0/
+
+ */
+
+function responsiveMobileMenu() {	
+    $('.rmm').each(function() {
+	
+	$(this).children('ul').addClass('rmm-main-list');	// mark main menu list
+	var $style = $(this).attr('data-menu-style');	// get menu style
+	if ( typeof $style === 'undefined' ||  $style === false )
+	{
+	    $(this).addClass('graphite'); // set graphite style if style is not defined
+	}
+	else {
+	    $(this).addClass($style);
+	}
+
+	var $width = 0;
+
+        $width = $(window).width()
+	// if modern browser
+	if ($.support.leadingWhitespace) {
+	    $(this).css('max-width' , $width*1.05+'px');
+	}
+	// 
+	else {
+	    $(this).css('width' , $width*1.05+'px');
+	}
+    });
+}
+function getMobileMenu() {
+
+    /* 	build toggled dropdown menu list */
+    $('.rmm').each(function() {	
+	var menutitle = $(this).attr('data-menu-title')
+	if ( menutitle === '' ) {
+	    menutitle = window.i18n('注册');
+	}
+	else if ( menutitle === undefined ) {
+	    menutitle = window.i18n('注册');
+	}
+	var $menulist = $(this).children('.rmm-main-list').html();
+        var $menuTextButton = '<div class="rmm-toggled-title">' + menutitle + '</div>'
+        var $menuButton = '<div class="rmm-button"><span>&nbsp;</span><span>&nbsp;</span><span>&nbsp;</span></div>'
+	var $menucontrols ='<div class="rmm-toggled-controls">' + $menuButton + $menuTextButton + '</div>'
+	$(this).prepend('<div class="rmm-toggled rmm-closed">'+$menucontrols+'<ul>'+$menulist+'</ul></div>')
+    });
+}
+
+function adaptMenu() {
+    /* 	toggle menu on resize */
+    $('.rmm').each(function() {
+
+	if ($(window).width() < 768 ) {
+	    $(this).children('.rmm-main-list').hide(0);
+	    $(this).children('.rmm-toggled').show(0);
+	}
+	else {
+	    $(this).children('.rmm-main-list').show(0);
+	    $(this).children('.rmm-toggled').hide(0);
+	}
+    });
+}
+
+$(function() {
+    responsiveMobileMenu();
+    getMobileMenu();
+    adaptMenu();
+
+    /* slide down mobile menu on click */
+    $('.rmm-toggled .rmm-toggled-controls .rmm-button').on('click', function(){
+        var $rmm = $('.rmm-toggled')
+	if ( $rmm.is('.rmm-closed')) {
+	    $rmm.find('ul').stop().show(300);
+	    $rmm.removeClass('rmm-closed');
+	}
+	else {
+	    $rmm.find('ul').stop().hide(300);
+	    $rmm.addClass('rmm-closed');
+	}
+    });
+
+     $('.rmm-toggled .rmm-toggled-controls .rmm-toggled-title').on('click', function(){
+       window.project.goToSignUp()
+    });
+});
+/* 	hide mobile menu on resize */
+$(window).resize(function() {
+    adaptMenu();
+});
