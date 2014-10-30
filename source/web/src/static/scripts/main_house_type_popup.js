68e79cf79189045bea0fe58b0349b13e9b451d21
ChaoWang <wangchao0721@gmail.com>
2014-10-30 18:19:03 +0800

    [web]Mod: reformat property less

--- source/web/src/static/scripts/main_house_type_popup.js
+++ source/web/src/static/scripts/main_house_type_popup.js
@@ -2,22 +2,24 @@
 
     $( document ).ready(function(){
         var popup = $('#main_house_type_popup')
+
         window.closeMainHouseType = function(){
-            //var popup = $('#main_house_type_popup')
             popup.hide()
         }
 
         window.openMainHouseType = function(event, floorplan, index) {
-            //var popup = $('#main_house_type_popup')
             //Set up floorplan
             if (floorplan) {
                 popup.find('.main_house_type_floorplan').attr('src', floorplan)
             }
 
             //Clone living room part from property detail
+            console.log(popup.find('.main_house_type_info .table_wrapper .text'))
             popup.find('.main_house_type_info .table_wrapper .text').replaceWith($('#propertyDetails_houseTypes').find('.item'+index).find('.text')[0].cloneNode(true))
 
             //Clone area and value from property detail
+            console.log(popup.find('.main_house_type_info .text2:nth-child(2) .value'))
+            console.log($('#propertyDetails_houseTypes').find('.item'+index).find('.text2 tr td:nth-child(1) .value'))
             //popup.find('.main_house_type_info .text2:nth-child(2) .value').replaceWith($('#propertyDetails_houseTypes').find('.item'+index).find('.text2 tr td:nth-child(1) .value').cloneNode(true))
             //popup.find('.main_house_type_info .text2:nth-child(3) .value').replaceWith($('#propertyDetails_houseTypes').find('.item'+index).find('.text2 tr td:nth-child(2) .value').cloneNode(true))
             popup.show()
