a3905a32a50c045ab90cac7bd9addc445af5657f
ChaoWang <wangchao0721@gmail.com>
2014-11-06 12:13:55 +0800

    [web]Mod: issue #5114 only load ga on production server

--- source/web/src/static/scripts/zoho.js
+++ source/web/src/static/scripts/zoho.js
@@ -1,4 +1,7 @@
 /**
  * Created by chaowang on 11/6/14.
  */
+/* jshint ignore:start */
 var $zoho= $zoho || {salesiq:{values:{},ready:function(){}}};var d=document;s=d.createElement("script");s.type="text/javascript";s.defer=true;s.src="https://salesiq.zoho.com/youngfunding/float.ls?embedname=youngfunding";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);
+
+/* jshint ignore:end */
