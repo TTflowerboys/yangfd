14350dfef0135e27f90098e9f188551b94b8f0a8
ChaoWang <wangchao0721@gmail.com>
2014-11-06 11:05:35 +0800

    [web]Add: issue #5345 add zoho

---
+++ source/web/src/static/scripts/zoho.js
@@ -0,0 +1,4 @@
+/**
+ * Created by chaowang on 11/6/14.
+ */
+var $zoho= $zoho || {salesiq:{values:{},ready:function(){}}};var d=document;s=d.createElement("script");s.type="text/javascript";s.defer=true;s.src="https://salesiq.zoho.com/youngfunding/float.ls?embedname=youngfunding";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);
