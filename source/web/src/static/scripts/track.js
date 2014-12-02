2681e9b43c277171d8aa6119483b12676de5e714
ChaoWang <wangchao0721@gmail.com>
2014-12-02 17:19:34 +0800

    [web]Mod: issue #5490 add three mouseflow account and fix ga link error(wait for test)

--- source/web/src/static/scripts/track.js
+++ source/web/src/static/scripts/track.js
@@ -19,10 +19,11 @@ ga('create', 'UA-55542465-1', 'auto', {'allowLinker': true});
 // Load the plugin.
 ga('require', 'linker');
 // Define which domains to autoLink. Exclude current domain when add link.
-var domains = ['yangfd.com', 'yangfd.co.uk','yangfd.cn'];
+var domains = ['yangfd.com', 'youngfunding.co.uk','yangfd.cn'];
 for(var i = domains.length - 1; i >= 0; i--) {
     if(domains[i] === window.location.host) {
-        ga('linker:autoLink', domains.splice(i, 1));
+        domains.splice(i, 1);
+        ga('linker:autoLink', domains);
     }
 }
 
@@ -33,13 +34,21 @@ if (window.user) {
 
 ga('send', 'pageview');
 
-// Mouse Flow
+// Set up Mouse Flow
 var _mfq = _mfq || [];
 (function () {
     var mf = document.createElement("script");
     mf.type = "text/javascript";
     mf.async = true;
-    mf.src = "//cdn.mouseflow.com/projects/10e0e312-30d7-487c-813a-1d95a574d0aa.js";
+
+    if(window.location.host === 'yangfd.com'){
+        mf.src = "//cdn.mouseflow.com/projects/719ffad3-9377-4ee5-88b7-5ec0900f18bb.js";
+    }else if(window.location.host === 'youngfunding.co.uk'){
+        mf.src = "//cdn.mouseflow.com/projects/cf781e50-74b4-43f3-9e9a-0a46076e63c5.js";
+    }else if(window.location.host === 'yangfd.cn'){
+        mf.src = "//cdn.mouseflow.com/projects/10e0e312-30d7-487c-813a-1d95a574d0aa.js";
+    }
+
     document.getElementsByTagName("head")[0].appendChild(mf);
 })();
 
