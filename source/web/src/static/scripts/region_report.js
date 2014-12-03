4b3bc1018ef585e419054cebc3d165dd09d71a2c
Foster Yin <fyin@bbtechgroup.com>
2014-12-03 19:18:14 +0800

    [web] Add: Task #4914 related properties

--- source/web/src/static/scripts/region_report.js
+++ source/web/src/static/scripts/region_report.js
@@ -1,3 +1,28 @@
 (function () {
+    $('#loadIndicator').show()
+    $.betterPost('/api/1/property/search', {zipcode_index:'E14'})
+        .done(function (val) {
+            var array = val.content
+
+            if (!_.isEmpty(array)) {
+
+                _.each(array, function (house) {
+                    var houseResult = _.template($('#houseCard_template').html())({house: house})
+                    $('.relatedProperties .jcarousel ul').append('<li>' +houseResult + '</li>')
+                })
+                $('.relatedProperties .jcarousel-wrapper').show()
+                if (array.length === 1)
+                {
+                    $('.relatedProperties .jcarousel-wrapper').width('600px')
+                    $('.relatedProperties .jcarousel-control-next').hide()
+                }
+            }
+        })
+        .fail (function () {
+
+        })
+        .always(function () {
+            $('#loadIndicator').hide()
+        })
 
 })();
