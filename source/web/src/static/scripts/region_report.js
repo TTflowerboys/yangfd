26244c8960bb633eeb6ffe6f6921eeb73f8de149
Foster Yin <fyin@bbtechgroup.com>
2014-12-04 11:45:08 +0800

    [web] Add: Task #4914 Update related house card switch

--- source/web/src/static/scripts/region_report.js
+++ source/web/src/static/scripts/region_report.js
@@ -1,21 +1,30 @@
 (function () {
     $('#loadIndicator').show()
-    $.betterPost('/api/1/property/search', {zipcode_index:'E14'})
+    $.betterPost('/api/1/property/search', {zipcode_index:'L3'})
         .done(function (val) {
             var array = val.content
 
             if (!_.isEmpty(array)) {
 
+                var index = 0
                 _.each(array, function (house) {
+                    index = index + 1
                     var houseResult = _.template($('#houseCard_template').html())({house: house})
-                    $('.relatedProperties .jcarousel ul').append('<li>' +houseResult + '</li>')
+                    $('.relatedProperties .jcarousel ul').append('<li class=item' + index + ' >' +houseResult + '</li>')
                 })
                 $('.relatedProperties .jcarousel-wrapper').show()
                 if (array.length === 1)
                 {
                     $('.relatedProperties .jcarousel-wrapper').width('600px')
+                    $('.relatedProperties .jcarousel-control-prev').hide()
                     $('.relatedProperties .jcarousel-control-next').hide()
                 }
+                else if (array.length === 2)
+                {
+                    $('.relatedProperties .jcarousel-control-prev').hide()
+                    $('.relatedProperties .jcarousel-control-next').hide()
+                }
+
             }
         })
         .fail (function () {
