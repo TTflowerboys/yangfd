2454e8cb15c2d6dc88de6b37c35e92e53f927349
Foster Yin <fyin@bbtechgroup.com>
2014-12-09 10:37:52 +0800

    [web] Add: Task #4914 uniform related properties with rslide

--- source/web/src/static/scripts/region_report.js
+++ source/web/src/static/scripts/region_report.js
@@ -27,38 +27,17 @@
                 _.each(array, function (house) {
                     index = index + 1
                     var houseResult = _.template($('#houseCard_template').html())({house: house})
-                    $('.relatedProperties .jcarousel ul').append('<li class=item' + index + ' >' +houseResult + '</li>')
-
                     $('.relatedProperties .rslides').append('<li class=item' + index + ' >' +houseResult + '</li>')
                 })
 
-                if (window.team.isPhone())
-                {
-                    $('.relatedProperties .rslides_wrapper').show()
-                    $('#propertySlider').responsiveSlides({
-                        pager: true,
-                        auto: false,
-                        nav: true,
-                        prevText: '<',
-                        nextText: '>'
-                    })
-
-                }
-                else {
-                    $('.relatedProperties .jcarousel-wrapper').show()
-
-                }
-                if (array.length === 1)
-                {
-                    $('.relatedProperties .jcarousel-wrapper').width('600px')
-                    $('.relatedProperties .jcarousel-control-prev').hide()
-                    $('.relatedProperties .jcarousel-control-next').hide()
-                }
-                else if (array.length === 2)
-                {
-                    $('.relatedProperties .jcarousel-control-prev').hide()
-                    $('.relatedProperties .jcarousel-control-next').hide()
-                }
+                $('.relatedProperties .rslides_wrapper').show()
+                $('#propertySlider').responsiveSlides({
+                    pager: true,
+                    auto: false,
+                    nav: true,
+                    prevText: '<',
+                    nextText: '>'
+                })
 
             }
         })
