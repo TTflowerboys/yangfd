4197d18384c7e5a621971b32ef20e5b03012fc1a
Felix Yan <felixonmars@gmail.com>
2014-12-17 15:52:50 +0800

    [server] Mod: add s3 reverse proxy

--- source/server/main_interface.py
+++ source/server/main_interface.py
@@ -841,9 +841,9 @@ def wechat_endpoint():
                 picurl = property["reality_images"][0]
                 if "bbt-currant.s3.amazonaws.com" in picurl:
                     picurl += "_thumbnail"
-                from urllib import quote
-                # etree.SubElement(item, "PicUrl").text = picurl.replace("bbt-currant.s3.amazonaws.com/", "yangfd.cn/s3/")
-                etree.SubElement(item, "PicUrl").text = schema + request.urlparts[1] + "/reverse_proxy?link=" + quote(picurl.replace("bbt-currant.s3.amazonaws.com/", "yangfd.cn/s3/"))
+                # from urllib import quote
+                etree.SubElement(item, "PicUrl").text = picurl.replace("bbt-currant.s3.amazonaws.com/", "s3.yangfd.cn/raw/")
+                # etree.SubElement(item, "PicUrl").text = schema + request.urlparts[1] + "/reverse_proxy?link=" + quote(picurl)
 
             etree.SubElement(item, "Url").text = schema + request.urlparts[1] + "/property/" + property["id"]
 
