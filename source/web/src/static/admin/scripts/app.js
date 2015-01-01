0cf95287a75b54d0f44aac76bdfb6d61034320e0
zhou <mzhou@bbtechgroup.com>
2015-01-01 17:17:17 +0800

    [web] Mod: issue #5617 将wysiwyg改为textAngular，并给textAngular 增加最大高度。
             新闻和咨询的 标签 加上 watermark 默认值

--- source/web/src/static/admin/scripts/app.js
+++ source/web/src/static/admin/scripts/app.js
@@ -2,7 +2,7 @@
 
 
 angular.module('app',
-    ['ui.router', 'angular-loading-bar', 'angularFileUpload', 'ui.bootstrap', 'angular-growl', 'wysiwyg.module', 'multi-select', 'ang-drag-drop'])
+    ['ui.router', 'angular-loading-bar', 'angularFileUpload', 'ui.bootstrap', 'angular-growl', 'multi-select', 'ang-drag-drop', 'textAngular'])
     .run(function ($rootScope, $state, $stateParams, $sce) {
         $rootScope.i18n = i18n;
         $rootScope.$state = $state;
