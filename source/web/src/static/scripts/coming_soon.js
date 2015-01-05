157926cdd67a909fe18f09e0f0b9765248a76071
zhou <mzhou@bbtechgroup.com>
2015-01-05 17:48:48 +0800

    [web] Mod: 将 订阅 事件改为 goal button

--- source/web/src/static/scripts/coming_soon.js
+++ source/web/src/static/scripts/coming_soon.js
@@ -13,8 +13,7 @@
         })
         if (!valid) {return}
         var params = $(this).serializeObject()
-        var plan = $('.hint').length > 0 ? 'A' : 'B'
-        ga('send', 'event', 'subscription', 'subscription', 'PLAN ' + plan);
+        ga('send','event','goal','click');
         params.locales = window.lang
         $.betterPost('/api/1/subscription/add', params)
             .done(function () {
