0675ebbb891784d64bfa84331c10b82740be4565
zhou <mzhou@bbtechgroup.com>
2015-01-14 15:09:32 +0800

    [web] Mod: issue #5691 修改众筹项目。

--- source/web/src/static/admin/scripts/controllers/crowdfunding/edit.js
+++ source/web/src/static/admin/scripts/controllers/crowdfunding/edit.js
@@ -6,7 +6,7 @@
 (function () {
 
     function ctrlCrowdfundingEdit($scope, $state, api, $stateParams, misc, growl) {
-        $scope.api = api
+        $scope.item = {}
 
         var itemFromParent = misc.findById($scope.$parent.list, $stateParams.id)
 
@@ -14,43 +14,135 @@
         if (itemFromParent) {
             onGetItem(itemFromParent)
         } else {
-            api.getOne($stateParams.shop_id,$stateParams.id)
+            api.getOne($stateParams.shop_id, $stateParams.id, {errorMessage: true})
                 .success(function (data) {
                     onGetItem(data.val)
                 })
         }
 
+        var currentItem
 
         function onGetItem(item) {
-
-            $scope.itemOrigin = item
+            currentItem = item
+            var editItem = angular.copy(item)
+            if (!_.isEmpty(editItem.property_type)) {
+                $scope.propertyType = editItem.property_type.slug
+                editItem.property_type = editItem.property_type.id
+            }
+            if (!_.isEmpty(editItem.intention)) {
+                var temp = []
+                angular.forEach(editItem.intention, function (value, key) {
+                    temp.push(value.id)
+                })
+                editItem.intention = temp
+            }
+            if (!_.isEmpty(editItem.investment_type)) {
+                var temp1 = []
+                angular.forEach(editItem.investment_type, function (value, key) {
+                    temp1.push(value.id)
+                })
+                editItem.investment_type = temp1
+            }
+            if (!_.isEmpty(editItem.equity_type)) {
+                editItem.equity_type = editItem.equity_type.id
+            }
+            if (!_.isEmpty(editItem.decorative_style)) {
+                editItem.decorative_style = editItem.decorative_style.id
+            }
+            if (!_.isEmpty(editItem.property_price_type)) {
+                $scope.propertyPriceType = editItem.property_price_type.slug
+                editItem.property_price_type = editItem.property_price_type.id
+            }
+            if (!_.isEmpty(editItem.facing_direction)) {
+                editItem.facing_direction = editItem.facing_direction.id
+            }
+            if (!_.isEmpty(editItem.country)) {
+                editItem.country = editItem.country.id
+            }
+            if (!_.isEmpty(editItem.city)) {
+                editItem.city = editItem.city.id
+            }
+            $scope.itemOrigin = editItem
             $scope.item = angular.copy($scope.itemOrigin)
         }
-
 
         $scope.submit = function ($event, form) {
             $event.preventDefault()
             $scope.submitted = true
-
-            var changed = misc.getChangedI18nAttributes($scope.item, $scope.itemOrigin)
-            if (!changed) {
+            var changed = JSON.parse(angular.toJson($scope.item))
+            changed = misc.cleanTempData(changed)
+            changed = misc.cleanI18nEmptyUnit(changed)
+            changed = misc.getChangedI18nAttributes(changed, $scope.itemOrigin)
+            if (_.isEmpty(changed)) {
                 growl.addWarnMessage('Nothing to update')
                 return
             }
-
+            if (changed.zipcode) {
+                changed.zipcode_index = changed.zipcode.substring(0, 3).toUpperCase()
+            }
             $scope.loading = true
-
-            api.update($stateParams.shop_id,angular.extend(changed, {id: $stateParams.id}), {
+            api.update($stateParams.shop_id, angular.extend(changed, {id: $stateParams.id}), {
                 successMessage: 'Update successfully',
                 errorMessage: 'Update failed'
-            }).success(function () {
-                angular.extend($scope.itemOrigin, changed)
+            }).success(function (data) {
+                angular.extend(currentItem, data.val)
+                $state.go('^')
+            }).error(function () {
+                if ($scope.item.status !== $scope.itemOrigin.status) {
+                    $scope.item.status = $scope.itemOrigin.status
+                }
+            })['finally'](function () {
+                $scope.loading = false
+            })
+        }
+
+        $scope.submitForReview = function ($event, form) {
+            $scope.item.status = 'not reviewed'
+            $scope.submit($event, form)
+        }
+
+        $scope.makeStatusToDraft = function () {
+            api.update({status: 'draft', id: $stateParams.id}, {
+                successMessage: 'Update successfully',
+                errorMessage: 'Update failed'
+            }).success(function (data) {
+                angular.extend(currentItem, data.val)
                 $state.go('^')
             })['finally'](function () {
                 $scope.loading = false
             })
         }
 
+        $scope.submitForAccept = function ($event, form) {
+            $scope.item.status = 'selling'
+            $scope.submit($event, form)
+        }
+
+        $scope.submitForReject = function ($event, form) {
+            $scope.item.status = 'rejected'
+            $scope.submit($event, form)
+        }
+
+        $scope.submitForPreview = function ($event, form) {
+            $scope.submitted = true
+            var changed = JSON.parse(angular.toJson($scope.item))
+            changed = misc.cleanTempData(changed)
+            changed = misc.cleanI18nEmptyUnit(changed)
+            changed = misc.getChangedI18nAttributes(changed, $scope.itemOrigin)
+            if (_.isEmpty(changed)) {
+                growl.addWarnMessage('Nothing to update')
+                return
+            }
+            $scope.loading = true
+            api.update(angular.extend(changed, {id: $stateParams.id}), {
+                successMessage: 'Update successfully',
+                errorMessage: 'Update failed'
+            }).success(function (data) {
+                $window.open('property/' + $stateParams.id, '_blank')
+            })['finally'](function () {
+                $scope.loading = false
+            })
+        }
 
     }
 
