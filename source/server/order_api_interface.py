61a1da3c78f97d59fa9d6d79fc980dd75828b850
Luigi Fan <fdb713@gmail.com>
2015-01-16 09:28:57 +0000

    [server] Add: Task #5649

--- source/server/order_api_interface.py
+++ source/server/order_api_interface.py
@@ -12,12 +12,18 @@ logger = logging.getLogger(__name__)
     price=(float, True),
     payment_method_id=(str, "virtual"),
     async=(bool, 1),
-    type=(str, "normal"),
+    type=(str, True),
     item_id=(ObjectId, True),
 ))
 @f_app.user.login.check(force=True)
 def order_add(user, params):
-    return f_app.order.output([f_app.shop.item_buy(params["item_id"], params)])[0]
+    """
+    ``type`` can be ``recharge``, ``withdrawal``, ``recovery``, ``earnings``, ``investment``
+    """
+    force_price = params.pop("price")
+    if params["type"] not in ["recharge", "withdrawal", "recovery", "earnings", "investment"]:
+        abort(40000, logger.warning("Invalid params: type", exc_info=False))
+    return f_app.order.output([f_app.shop.item_buy(params["item_id"], params, force_price=force_price)])[0]
 
 
 @f_api('/order/search', params=dict(
@@ -26,10 +32,33 @@ def order_add(user, params):
     time=datetime,
     starttime=datetime,
     endtime=datetime,
+    user_id=ObjectId,
+    shop_id=ObjectId,
+    type=str,
 ))
 @f_app.user.login.check(force=True)
 def order_search(user, params):
     per_page = params.pop("per_page", 0)
+    if "user_id" in params:
+        params["user.id"] = str(params.pop("user_id"))
+    if "shop_id" in params:
+        params["shop.id"] = str(params.pop("shop_id"))
+
+    time_start = params.pop("starttime", None)
+    time_end = params.pop("endtime", None)
+
+    if "type" in params and params["type"] not in ["recharge", "withdrawal", "recovery", "earnings", "investment"]:
+        abort(40000, logger.warning("Invalid params: type", exc_info=False))
+
+    if time_start or time_end:
+        if time_start and time_end:
+            if time_end < time_start:
+                abort(40000, logger.warning("Invalid params: end time is earlier than start time.", exc_info=False))
+        if time_start is not None:
+            params["last_time"] = time_start
+        if time_end is not None:
+            params["time"] = time_end
+
     user_role = f_app.user.get_role(user["id"])
     if set(user_role) & set(["admin", "jr_admin"]):
         order_id_list = f_app.order.custom_search(params, per_page=per_page)
@@ -47,3 +76,21 @@ def order_get(user, order_id):
         return f_app.order.output([order_id], permission_check=False)[0]
     else:
         return f_app.order.output([order_id])[0]
+
+
+@f_api('/order/item_snapshot')
+@f_app.user.login.check(force=True)
+def order_item_snapshot(user, order_id):
+    params = {"user.id": user["id"], "type": "investment"}
+    order_list = f_app.order.output(f_app.order.custom_search(params, per_page=0), permission_check=False)
+    item_list = []
+    item_id_set = set()
+    for order in order_list:
+        if isinstance(order.get('item'), dict):
+            if order["item"]["id"] not in item_id_set:
+                item_list.append(order["item"])
+                item_id_set.add(order["item"]["id"])
+        else:
+            logger.warning("Invalid order found, this should be a BUG!", exc_info=False)
+
+    return item_list
