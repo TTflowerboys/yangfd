0c27645e9d3f923219024a2439d138bcb1b5f361
Foster Yin <fyin@bbtechgroup.com>
2015-01-16 15:54:05 +0800

    [web] Add: Task #5659 Investment Chart

---
+++ source/web/src/static/scripts/update_investment_chart.js
@@ -0,0 +1,65 @@
+(function ($) {
+
+    if (window.historical_price) {
+        if ($.browser.msie && parseInt($.browser.version, 10) <= 8) {
+            $(window).on('load', function () {
+                showCharts()
+            })
+        } else {
+            $(function () { showCharts() })
+        }
+    }
+
+    function parseData() {
+        var red = '#e20013'
+        var result = {
+            labels: [],
+            datasets: [
+                {
+                    fillColor: red,
+                    strokeColor: red,
+                    pointColor: red,
+                    pointStrokeColor: red,
+                    data: []
+                }
+            ]
+        }
+
+        result.labels.push('2015')
+        result.datasets[0].data.push('1000.14')
+
+        result.labels.push('2016')
+        result.datasets[0].data.push('1200.14')
+        result.labels.push('2017')
+        result.datasets[0].data.push('1300.14')
+
+        return result
+
+    }
+
+    window.updateInvestmentChart =  function () {
+        var data = parseData()
+
+        var chart = document.getElementById('barChart')
+        var ctx;
+
+        ctx = chart.getContext('2d');
+        if (window.team.isPhone()) {
+            chart.setAttribute('width', $(window).width())
+            chart.setAttribute('height', $(window).width() / 2)
+        }
+        else {
+            chart.setAttribute('width', '800')
+            chart.setAttribute('height', '400')
+        }
+
+        new Chart(ctx).Bar(data, {
+            barShowStroke: true,
+            barDatasetSpacing : 10,
+            barStrokeWidth : 20,
+        });
+
+    }
+
+
+})(jQuery)
