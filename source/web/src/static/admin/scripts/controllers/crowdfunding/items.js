f827e8526deb68fc93cbe4ecb8ebab7d57203d0a
zhou <mzhou@bbtechgroup.com>
2015-01-16 19:04:51 +0800

    [web] Mod: issue #5692 修改项目资产

--- source/web/src/static/admin/scripts/controllers/crowdfunding/items.js
+++ source/web/src/static/admin/scripts/controllers/crowdfunding/items.js
@@ -20,6 +20,21 @@
         $scope.onRemoveManageTeam = function (index) {
             $scope.item.management_team.splice(index, 1)
         }
+
+        $scope.addCapital = function () {
+            if (!$scope.item.capital_structure) {
+                $scope.item.capital_structure = []
+            }
+            var temp = {
+                name: {}
+            }
+            $scope.item.capital_structure.push(temp);
+        }
+
+        $scope.onRemoveCapital = function (index) {
+            $scope.item.capital_structure.splice(index, 1)
+        }
+
     }
 
     angular.module('app').controller('ctrlCrowdfundingItems', ctrlCrowdfundingItems)
