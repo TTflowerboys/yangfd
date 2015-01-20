82198b19b4b002dbce3b885ee94189ba19ba8e17
Foster Yin <fyin@bbtechgroup.com>
2015-01-20 15:15:01 +0800

    [web] Add: Move property html js to individual file

---
+++ source/web/src/static/scripts/property_favorite.js
@@ -0,0 +1,58 @@
+(function () {
+
+
+    $('[data-fn=addFav]').on('click', function () {
+        ga('send', 'event', 'property_detail', 'click', 'add-fav')
+        if (window.project.checkLoginIfNot()) {
+            return
+        }
+        var $button = $(this)
+        var property_id = $button.attr('data-property-id')
+
+        $.betterPost('/api/1/user/favorite/add', {
+            property_id: property_id
+        })
+            .done(function (val) {
+                $button.hide().parent().find('[data-fn=removeFav]').show().attr('data-favorite-id', val)
+
+                ga('send', 'event', 'property_detail', 'click', 'add-fav-success')
+            })
+            .fail(function (errorCode) {
+                if (errorCode !== 40100) {
+                    window.alert($button.attr('data-message-' + errorCode) || i18n('服务器忙，请稍后重试。'))
+                }
+
+                ga('send', 'event', 'property_detail', 'click', 'add-fav-failed',errorCode)
+            })
+
+
+    })
+
+    $('[data-fn=removeFav]').on('click', function () {
+        ga('send', 'event', 'property_detail', 'click', 'remove-fav')
+        if (window.project.checkLoginIfNot()) {
+            return
+        }
+        var $button = $(this)
+        var favorite_id = $button.attr('data-favorite-id')
+        $.betterPost('/api/1/user/favorite/' + favorite_id + '/remove')
+            .done(function () {
+                $button.hide().parent().find('[data-fn=addFav]').show()
+                ga('send', 'event', 'property_detail', 'click', 'remove-fav-success')
+            })
+            .fail(function (errorCode) {
+                if (errorCode !== 40100) {
+                    window.alert($button.attr('data-message-' + errorCode) || i18n('服务器忙，请稍后重试。'))
+                }
+                ga('send', 'event', 'property_detail', 'click', 'remove-fav-failed',errorCode)
+            })
+    })
+
+    $('[data-fn=shareToWeChat]').on('click', function () {
+        $('#popupShareToWeChat')
+            .find('img').prop('src', '/qrcode/generate?content=' + encodeURIComponent(location.href)).end()
+            .modal()
+        ga('send', 'event', 'property_detail', 'share', 'open-wechat-web')
+    })
+
+})()
