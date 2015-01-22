c34469246bd3dcd35ddb12b8db30895684e0b317
zhou <mzhou@bbtechgroup.com>
2015-01-22 15:45:41 +0800

    [web] Mod: issue #5727 查看浏览过的房产。           Fix: 投资人界面，无法正确查询的问题。

--- source/web/src/static/admin/scripts/factories/user_support_api.js
+++ source/web/src/static/admin/scripts/factories/user_support_api.js
@@ -1,14 +1,14 @@
 /* Created by frank on 14-8-23. */
 (function () {
 
-    function userSupportApi($http,$stateParams) {
+    function userSupportApi($http, $stateParams) {
         return {
-            getAll: function (id, config) {
+            getAll: function (config) {
                 config = config || {}
                 config.params = config.params || {}
-                config.params = {
+                config.params = angular.extend({}, config.params, {
                     user_id: $stateParams.id
-                }
+                })
                 return $http.get('/api/1/support_ticket/search', config)
             }
         }
