8aa2f33d02dbd87ef32f8ab923a4aedc9e27cc36
zhou <mzhou@bbtechgroup.com>
2015-01-29 19:51:50 +0800

    [web] Mod: issue #5133 添加没有结果提示框。

--- source/web/src/static/admin/scripts/directives/add_assignee.js
+++ source/web/src/static/admin/scripts/directives/add_assignee.js
@@ -1,6 +1,6 @@
 /* Created by frank on 14-9-15. */
 angular.module('app')
-    .directive('addAssignee', function ($http, $filter) {
+    .directive('addAssignee', function ($http, $filter, growl) {
         return {
             restrict: 'AE',
             scope: {
@@ -12,6 +12,9 @@ angular.module('app')
                 scope.searchUser = function (name) {
                     return $http.get('/api/1/user/admin/search', {params: {query: name}, errorMessage: true})
                         .then(function (res) {
+                            if (res.data.val.length === 0) {
+                                growl.addInfoMessage(i18n('没有结果'), {enableHtml: true})
+                            }
                             return $filter('limitTo')(res.data.val, 5)
                         })
                 }
