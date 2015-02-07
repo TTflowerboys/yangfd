f6a9374a75f3cb6af59cf87c39beefada0a45d71
ChaoWang <wangchao0721@gmail.com>
2015-02-07 18:43:43 +0800

    [web]Add: issue #5823 add house type(wait for test)

--- source/web/src/static/scripts/main_house_type_popup.js
+++ source/web/src/static/scripts/main_house_type_popup.js
@@ -1,66 +1,69 @@
 (function ($) {
 
     $(document).ready(function () {
-        var popup = $('#main_house_type_popup')
         var cIndex = 0
-        var totalLength = 0
+
+        var houseTypes = JSON.parse($('#propertyDetails_houseTypes_data').text())
+        var totalLength = houseTypes.length
+
+        if(houseTypes.length > 0){
+            for(var i = 0;i<houseTypes.length;i++){
+                var houseTypeTpl = _.template($('#main_house_type_popup_template').html())({houseType: houseTypes[i],index:i})
+                $('#main_house_type_popup').append(houseTypeTpl)
+
+                $('#main_house_type_popup').find('#floorplanSlider'+i).responsiveSlides({
+                    pager: true,
+                    auto: false,
+                    nav: true,
+                    prevText: '<',
+                    nextText: '>'
+                })
+            }
+
+            $('#main_house_type_popup').find('.rslides_wrapper .leftPressArea').click(function (event) {
+                $(event.target).parent().find('a.prev').click()
+            })
+
+            $('#main_house_type_popup').find('.rslides_wrapper .rightPressArea').click(function (event) {
+                $(event.target).parent().find('a.next').click()
+            })
+        }
+
 
         window.closeMainHouseType = function () {
-            popup.hide()
+            $('#main_house_type_popup').hide()
         }
 
         window.openMainHouseType = function (event, index,isPopupExist) {
 
             cIndex = parseInt(index)
+            if(houseTypes && houseTypes.length>0 && cIndex>=0 && cIndex < houseTypes.length){
+
+                $('.main_house_type_wrapper.item'+cIndex).fadeIn( 'slow', function() {
+                    $('.main_house_type_wrapper.item'+cIndex).show()
+                })
+            }
 
             //Show slide when total length > 1
-            totalLength = $('#propertyDetails_houseTypes').find('ul').children().length
-            if (totalLength === 1) {
-                popup.find('main_house_type a').hide()
+            if(houseTypes.length > 1){
+                $('#main_house_type_popup').find('#main_house_type a').show()
             } else {
-                popup.find('main_house_type a').show()
+                $('#main_house_type_popup').find('#main_house_type a').hide()
             }
 
-            var wrapper = popup.find('.main_house_type_wrapper')
+            // Update popup position
+            var wrapper = $('#main_house_type_popup').find('.main_house_type_wrapper')
             wrapper.css('top', $(window).scrollTop() + 30)
 
-            //Set up floorplan
-            var floorplan = $('#propertyDetails_houseTypes').find('.item' + index).find('.floor_plan').attr('src').replace('_thumbnail','')
-            if(floorplan !== ''){
-                popup.find('.main_house_type_floorplan').attr('src', floorplan)
-            }else{
-                //To workaround onerror can not fire on firefox
-                popup.find('.main_house_type_floorplan').addClass('withPlaceholder')
-            }
-
-            //Set name
-            var name = $('#propertyDetails_houseTypes').find('.item' + index).find('.propertyDetails_houseType_name').text()
-            popup.find('.main_house_type_header h1').text(name)
-
-            //Clone living room part from property detail
-            popup.find('.main_house_type_info .table_wrapper .text').replaceWith($('#propertyDetails_houseTypes').find('.item' + index).find('.text')[0].cloneNode(true))
-
-            //Clone area and value from property detail
-            popup.find('.main_house_type_info .space').empty().append($($('#propertyDetails_houseTypes').find('.item' + index).find('.text2 tr td')[0]).children().clone())
-            popup.find('.main_house_type_info .price').empty().append($($('#propertyDetails_houseTypes').find('.item' + index).find('.text2 tr td')[1]).children().clone())
-
-            //Set description
-            var description = $('#propertyDetails_houseTypes').find('.item' + index).find('.propertyDetails_houseType_description').text()
-            if (description === '') {
-                popup.find('.main_house_type_info .description').hide()
-            } else {
-                popup.find('.main_house_type_info .description').show()
-                $(popup.find('.main_house_type_info .description .value')).text(description)
-            }
-
-            popup.show()
+            $('#main_house_type_popup').show()
 
             if(!isPopupExist){
                 ga('send', 'event', 'property_detail', 'open', 'main-house-type-open',name)
             }
         }
 
-        $('.main_house_type_next_area').click(function(){
+        $('.main_house_type_prev_area').click(function(){
+            $('.main_house_type_wrapper.item'+cIndex).hide()
             if (cIndex > 0) {
                 window.openMainHouseType(null,cIndex - 1,true)
             } else if (cIndex === 0) {
@@ -71,7 +74,8 @@
             ga('send', 'event', 'property_detail', 'prev', 'main-house-type-popup-prev')
         })
 
-        $('.main_house_type_prev_area').click(function(){
+        $('.main_house_type_next_area').click(function(){
+            $('.main_house_type_wrapper.item'+cIndex).hide()
             if (cIndex < totalLength - 1) {
                 window.openMainHouseType(null,cIndex + 1,true)
             } else if (cIndex === totalLength - 1) {
