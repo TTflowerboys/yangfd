36b3c94c3e3f38090ddc991bd2fec9994d7630d6
Michael Chou <113563@qq.com>
2015-03-09 18:12:42 +0800

    [web] Mod: 去掉空格。

--- source/web/src/static/admin/scripts/factories/user_intention_api.js
+++ source/web/src/static/admin/scripts/factories/user_intention_api.js
@@ -7,7 +7,7 @@
                 config = config || {}
                 config.params = angular.extend({}, config.params, {
                     user_id: $stateParams.id,
-                    status: 'new, assigned, in_progress, deposit, suspended,canceled'
+                    status: 'new,assigned,in_progress,deposit,suspended,canceled'
                 })
                 return $http.get('/api/1/intention_ticket/search', config)
             }
