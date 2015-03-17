37b85e5c7a215c1ace829ede0276e5a4b569c2af
Michael Chou <113563@qq.com>
2015-03-17 16:47:28 +0800

    [web] Mod: issue #5983 添加受限制状态

--- source/web/src/static/admin/scripts/values/property_status.js
+++ source/web/src/static/admin/scripts/values/property_status.js
@@ -7,6 +7,7 @@ angular.module('app')
         {name: i18n('待审核'), value: 'not reviewed'},
         {name: i18n('审核失败'), value: 'rejected'},
         {name: i18n('在售中'), value: 'selling'},
+        {name: i18n('受限制'), value: 'restricted'},
         {name: i18n('隐藏'), value: 'hidden'},
         {name: i18n('已售罄'), value: 'sold out'}
 //{ name: '删除', value: 'deleted' }
@@ -26,6 +27,7 @@ angular.module('app')
     ])
     .constant('propertySellingStatus', [
         {name: i18n('在售中'), value: 'selling'},
+        {name: i18n('受限制'), value: 'restricted'},
         {name: i18n('隐藏'), value: 'hidden'},
         {name: i18n('已售罄'), value: 'sold out'}
     ]).run(function ($rootScope, propertyStatus, propertyNormalStatus) {
