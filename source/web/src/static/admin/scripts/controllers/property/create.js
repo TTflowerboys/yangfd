c67e7772c29fecc50f3d4d3fe94b2c77c69dc4ec
Michael Chou <113563@qq.com>
2015-03-19 19:06:53 +0800

    [web] Mod: issue #5906 房产编辑增加定时器。

--- source/web/src/static/admin/scripts/controllers/property/create.js
+++ source/web/src/static/admin/scripts/controllers/property/create.js
@@ -4,6 +4,73 @@
 (function () {
 
     function ctrlPropertyCreate($scope, $state, api, misc) {
+        var delayer = new misc.Delayer({
+            task: function () {
+                createOrUpdate()
+            },
+            delay: 6000
+        })
+
+
+        function createOrUpdate() {
+            if ($scope.item.id) {
+                update($scope.item)
+            } else {
+                create($scope.item)
+            }
+            delayer.update()
+        }
+
+        function create(data) {
+            api.create(data, {
+                successMessage: 'Update successfully',
+                errorMessage: 'Update failed'
+            }).success(function (data) {
+                createSuccess(data)
+            })['finally'](function () {
+                $scope.loading = false
+            })
+        }
+
+        function update(data) {
+            api.update(data, {
+                successMessage: 'Update successfully',
+                errorMessage: 'Update failed'
+            }).success(function (data) {
+                updateSuccess(data)
+            })['finally'](function () {
+                $scope.loading = false
+            })
+        }
+
+        function createSuccess(data) {
+            if ($scope.submitted) {
+                if ($scope.$parent.currentPageNumber === 1) {
+                    $scope.$parent.refreshList()
+                }
+                $scope.cancelDelayer()
+                $state.go('^')
+            } else {
+                $scope.item.id = data.val
+            }
+        }
+
+        function updateSuccess(data) {
+            if ($scope.submitted) {
+                if ($scope.$parent.currentPageNumber === 1) {
+                    $scope.$parent.refreshList()
+                }
+                $scope.cancelDelayer()
+                $state.go('^')
+            } else {
+                console.log(data)
+                $scope.item.id = data.val
+            }
+        }
+
+        $scope.cancelDelayer = function(){
+            delayer.cancel()
+        }
 
         $scope.item = {}
 
@@ -13,17 +80,7 @@
             $scope.loading = true
             $scope.item = misc.cleanTempData($scope.item)
             $scope.item = misc.cleanI18nEmptyUnit($scope.item)
-            api.create($scope.item, {
-                successMessage: 'Update successfully',
-                errorMessage: 'Update failed'
-            }).success(function () {
-                if ($scope.$parent.currentPageNumber === 1) {
-                    $scope.$parent.refreshList()
-                }
-                $state.go('^')
-            })['finally'](function () {
-                $scope.loading = false
-            })
+            createOrUpdate()
         }
 
         $scope.submitForReview = function ($event, form) {
