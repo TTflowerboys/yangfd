b05d1df79d1510b4f474d355a8b711da6a1b0d7d
Foster Yin <fyin@bbtechgroup.com>
2015-03-20 16:36:05 +0800

    Initial Commit

---
+++ source/ios/currant/currant/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  currant
+//
+//  Created by Foster Yin on 3/20/15.
+//  Copyright (c) 2015 Foster Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ source/ios/currant/currant/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  currant
+//
+//  Created by Foster Yin on 3/20/15.
+//  Copyright (c) 2015 Foster Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
