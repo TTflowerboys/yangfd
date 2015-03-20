279a19a82598b84c08a24dd886d9b228f0f7d7a2
Foster Yin <fyin@bbtechgroup.com>
2015-03-20 09:58:13 +0800

    [web] Add: Refine bing map network indicator

--- source/web/src/static/scripts/property_map.js
+++ source/web/src/static/scripts/property_map.js
@@ -1,16 +1,40 @@
 (function () {
     if ($('.maps_wrapper')) {
         var property = JSON.parse($('#pythonProperty').text())
-        window.setupMap(property.latitude, property.longitude, function () {
-            //setUp Map Tabs
-            $('.maps').tabs({trigger: 'click'}).on('openTab', function (event, target, tabName){
-                $('[data-tab-name=' + tabName + ']').show()
+        var location = {latitude:property.latitude, longitude:property.longitude}
+        window.setupMap(location, function () {
+
+            $('#showMap').click(function (e) {
+                if (!$('#mapLoadIndicator').is(':visible')) {
+                    window.showMapIndicator()
+                    var scriptString = '<script src="http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0&onscriptload=onBingMapScriptLoad"></script>'
+                    window.onBingMapScriptLoad = function () {
+                        //showMap
+                        $('.staticMap').hide()
+                        $('.maps').show()
+                        //setUp Map Tabs
+                        $('.maps').tabs({trigger: 'click'}).on('openTab', function (event, target, tabName){
+                            $('[data-tab-name=' + tabName + ']').show()
+                        })
+
+                        //TODO: find why need get region for different map, may because for the delay after bing map load, or load bing map module for different map
+                        window.showMapIndicator()
+                        window.showTransitMap(location, null, true, 14, property.country.slug, function () {
+                                window.hideMapIndicator()
+                        })
+                        window.showMapIndicator()
+                        window.showSchoolMap(location, null, true, 14, property.country.slug, function () {
+                            window.hideMapIndicator()
+                        })
+                        window.showMapIndicator()
+                        window.showFacilityMap(location, null, true, 14, property.country.slug, function () {
+                                window.hideMapIndicator()
+                        })
+                        window.hideMapIndicator()
+                    }
+                    $('body').append(scriptString)
+                }
             })
-            //load data
-            var geoLocation = new Microsoft.Maps.Location(property.latitude, property.longitude);
-            window.showTransitMap(geoLocation, null, true, 14, property.country.slug)
-            window.showSchoolMap(geoLocation, null, true, 14, property.country.slug)
-            window.showFacilityMap(geoLocation, null, true, 14, property.country.slug)
         })
     }
 })()
