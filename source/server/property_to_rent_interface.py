cbc62c7daec9f18417eb35cdb273d8083739bcf8
Felix Yan <felixonmars@archlinux.org>
2015-04-17 15:31:12 +0800

    [server] Fix: utf-8 stringify

--- source/server/property_to_rent_interface.py
+++ source/server/property_to_rent_interface.py
@@ -88,7 +88,9 @@ def rent_ticket_get(rent_ticket_id, user):
     # if rent_ticket.get('zipcode_index') and rent_ticket.get('country').get('slug') == 'GB':
     #     report = f_app.i18n.process_i18n(currant_data_helper.get_report(rent_ticket.get('zipcode_index')))
 
-    title = six.text_type(rent_ticket.get('title', ''), "utf-8")
+    title = rent_ticket.get('title', '')
+    if not isinstance(title, six.string_types):
+        title = six.text_type(title)
     if rent_ticket["property"].get('city') and rent_ticket["property"].get('city').get('value'):
         title += '_' + _(rent_ticket["property"].get('city').get('value'))
     if rent_ticket["property"].get('country') and rent_ticket["property"].get('country').get('value'):
