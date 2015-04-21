ddf8701884a0fa3ada6f2b040aba7c9f0fb99dc8
ChaoWang <wangchao0721@gmail.com>
2015-04-21 21:50:37 +0800

    [web]Add: issue #6315 add whole non-register user request host contact process

---
+++ source/web/src/static/scripts/host_contact_request.js
@@ -0,0 +1,106 @@
+$(function () {
+    var rentId = $('input[name=rent_id]').val()
+
+    var $requestContactBtn = $('#contactRequestBtn')
+    var $contactRequestForm = $('form[name=contactRequestForm]')
+    var $requestSMSCodeBtn = $contactRequestForm.find('[name=getCodeBtn]')
+    var $submit = $contactRequestForm.find('[type=submit]')
+    var $feedback = $contactRequestForm.find('[data-role=serverFeedback]')
+
+    /*
+     * Control request contact button based on user login or not
+     * */
+    $requestContactBtn.on('click', function (e) {
+        if (window.user && rentId) {
+            $.betterPost('/api/1/rent_ticket/' + rentId + '/contact_info')
+                .done(function (val) {
+                    var phone = val
+                    $('.hostPhone span').text(phone)
+
+                    $('.contactRequest').hide()
+                })
+            //TODO: issue #6317
+            //.fail(function () {})
+        }
+        else {
+            $('#contactRequestBtn').hide()
+            $('.contactRequestForm').show()
+        }
+    })
+
+    /*
+     *  Get sms verfication code
+     * */
+    $requestSMSCodeBtn.on('click', function (e) {
+        // Check email and phone
+        var valid = $.validate($contactRequestForm, {
+            onError: function (dom, validator, index) {
+                $feedback.empty()
+                $feedback.append(window.getErrorMessageFromErrorCode(errorCode, api))
+                $feedback.show()
+            },
+            exclude: ['code']
+        })
+
+        // Fast register user
+        if (valid) {
+            $requestSMSCodeBtn.prop('disabled', false)
+            $requestSMSCodeBtn.text(window.i18n('发送中...'))
+
+            var params = $(this).serializeObject()
+            $.betterPost('/api/1/user/fast-register', params)
+                .done(function (val) {
+                    window.user = val
+                    //ga('send', 'event', 'signup', 'result', 'signup-success')
+                    //TODO: Count down 1 min to enable resend
+                    //$requestSMSCodeBtn.prop('disabled', true)
+                })
+                .fail(function (ret) {
+                    $feedback.empty()
+                    $feedback.append(window.getErrorMessageFromErrorCode(ret))
+                    $feedback.show()
+                    //refresh it for may user submit fail, or submit again with another account
+
+                    //ga('send', 'event', 'signup', 'result', 'signup-failed', window.getErrorMessageFromErrorCode(ret))
+                    $requestSMSCodeBtn.text(window.i18n('重新发送'))
+                    $requestSMSCodeBtn.prop('disabled', true)
+                })
+        }
+    })
+
+    $contactRequestForm.submit(function (e) {
+        e.preventDefault()
+        //ga('send', 'event', 'property_detail', 'submit', 'requirement-submit')
+        $submit.prop('disabled', true)
+        $feedback.hide()
+        var params = $(this).serializeObject({exclude: ['nickname','email','country','phone']})
+
+        $.betterPost('/api/1/user/' + window.user.id + '/sms_verification/verify', data)
+            .done(function () {
+                $feedback.show().text($form.attr('data-message-success'))
+
+                $requestContactBtn.click()
+                //ga('send', 'event', 'property_detail', 'result', 'requirement-submit-success')
+            })
+            .fail(function (errorCode) {
+                $feedback.empty()
+                $feedback.append(window.getErrorMessageFromErrorCode(errorCode, api))
+                $feedback.show()
+                //ga('send', 'event', 'property_detail', 'click', 'requirement-submit-failed',
+                //    window.getErrorMessageFromErrorCode(errorCode, api))
+            })
+            .always(function () {
+                $submit.prop('disabled', false)
+            })
+
+    }).on('change blur keyup', '[name]', function (e) {
+        var valid = $.validate($contactRequestForm, {onError: function () { }})
+        if (valid && window.user) {
+            $contactRequestForm.find('[type=submit]').prop('disabled', false)
+        } else {
+            $contactRequestForm.find('[type=submit]').prop('disabled', true)
+        }
+    })
+
+    validateForm() //user may have logged in all data is ready
+})
