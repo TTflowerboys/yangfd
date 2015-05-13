9adf2d4a24181152cb85c2502bce1dde70d911f5
levy.li <levy.li@bbtechgroup.com>
2015-05-13 18:48:55 +0800

    [Web]Mod: Issue #6048 调整手机上的轮播图

--- source/web/src/static/scripts/appDownload.js
+++ source/web/src/static/scripts/appDownload.js
@@ -1,13 +1,9 @@
 /**
  * Created by levy on 15-5-13.
  */
-(function(){
-    $(".rslides").responsiveSlides({
-        auto: false,
-        pager: true,
-        nav: true,
-        speed: 500,
-        maxwidth: 800,
-        namespace: "transparent-btns"
+(function(Swiper){
+    window.swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true
     });
-})()
+})(window.Swiper)
