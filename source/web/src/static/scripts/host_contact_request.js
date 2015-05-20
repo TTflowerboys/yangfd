9bad633b995b70c49d29dffcdada6f953f067935
ChaoWang <wangchao0721@gmail.com>
2015-05-20 17:09:12 +0800

    [[web]Fix: issue #6580 add countdown to host contact request and fix several error feedbacks(feedback)

--- source/web/src/static/scripts/host_contact_request.js
+++ source/web/src/static/scripts/host_contact_request.js
@@ -20,8 +20,13 @@ $(function () {
 
                     ga('send', 'pageview', '/host-contact-request/'+ rentId + '/contact-show-success')
                 })
-            //TODO: issue #6317
-            //.fail(function () {})
+                .fail(function (ret) {
+                    $feedback.empty()
+                    $feedback.append(window.getErrorMessageFromErrorCode(ret))
+                    $feedback.show()
+
+                    ga('send', 'event', 'request_host_contact', 'error', 'request-host-contact-failed', window.getErrorMessageFromErrorCode(ret))
+                })
         }
         else {
             $('#contactRequestBtn').hide()
@@ -58,18 +63,70 @@ $(function () {
             })
             $.betterPost('/api/1/user/fast-register', params)
                 .done(function (val) {
+                    //Clear any feedback before
+                    $feedback.empty()
+
                     window.user = val
                     ga('send', 'pageview', '/host-contact-request/'+ rentId + '/user-create-success')
-                    //TODO: Count down 1 min to enable resend
-                    //$requestSMSCodeBtn.prop('disabled', true)
+
+                    //Unbind fast-register api call from send button
+                    $requestSMSCodeBtn.off('click')
+
+                    //Count down 1 min to enable resend
+                    var sec = 60
+                    var countdown = setInterval(function(){
+                        $requestSMSCodeBtn.text(sec + 's')
+                        if(sec == 0){
+                            //Stop interval
+                            clearInterval(countdown)
+
+                            //Reable send button
+                            $requestSMSCodeBtn.prop('disabled', false)
+                            $requestSMSCodeBtn.text(window.i18n('重新发送'))
+
+                            //Re-bind sms verification send api to resend button
+                            $requestSMSCodeBtn.on('click',function(e){
+                                ga('send', 'event', 'request_host_contact', 'sms-verify', 're-send')
+                                $.betterPost('/api/1/user/sms_verification/send', {
+                                    phone:window.user.phone,
+                                    country:window.user.country.id
+                                }).done(function (val) {
+                                    //Disable re-send sms for re-send
+                                    $requestSMSCodeBtn.prop('disabled', true)
+
+                                    var sec = 60
+                                    var countdown = setInterval(function(){
+                                        $requestSMSCodeBtn.text(sec + 's')
+                                        if(sec == 0){
+                                            //Stop interval
+                                            clearInterval(countdown)
+
+                                            $requestSMSCodeBtn.prop('disabled', false)
+                                            $requestSMSCodeBtn.text(window.i18n('重新发送'))
+                                        }
+                                        sec--
+                                    },1000)
+                                }).fail(function (ret) {
+                                    $feedback.empty()
+                                    $feedback.append(window.getErrorMessageFromErrorCode(ret))
+                                    $feedback.show()
+                                    //refresh it for may user submit fail, or submit again with another account
+
+                                    ga('send', 'event', 'request_host_contact', 'error', 'send-sms-failed', window.getErrorMessageFromErrorCode(ret))
+                                    $requestSMSCodeBtn.text(window.i18n('重新发送'))
+                                    $requestSMSCodeBtn.prop('disabled', false)
+                                })
+                            })
+                        }
+                        sec--
+                    },1000)
                 })
                 .fail(function (ret) {
                     $feedback.empty()
                     $feedback.append(window.getErrorMessageFromErrorCode(ret))
                     $feedback.show()
-                    //refresh it for may user submit fail, or submit again with another account
 
-                    //ga('send', 'event', 'signup', 'result', 'signup-failed', window.getErrorMessageFromErrorCode(ret))
+                    ga('send', 'event', 'request_host_contact', 'error', 'fast-register-failed', window.getErrorMessageFromErrorCode(ret))
                     $requestSMSCodeBtn.text(window.i18n('重新发送'))
                     $requestSMSCodeBtn.prop('disabled', false)
                 })
@@ -78,7 +135,6 @@ $(function () {
 
     $contactRequestForm.submit(function (e) {
         e.preventDefault()
-        //ga('send', 'event', 'property_detail', 'submit', 'requirement-submit')
         $submit.prop('disabled', true)
         $feedback.hide()
         var params = $(this).serializeObject({
@@ -93,14 +149,11 @@ $(function () {
                 $feedback.show().text($(this).attr('data-message-success'))
 
                 $requestContactBtn.click()
-                //ga('send', 'event', 'property_detail', 'result', 'requirement-submit-success')
             })
-            .fail(function (errorCode) {
+            .fail(function (ret) {
                 $feedback.empty()
-                $feedback.append(window.getErrorMessageFromErrorCode(errorCode, api))
+                $feedback.append(window.getErrorMessageFromErrorCode(ret))
                 $feedback.show()
-                //ga('send', 'event', 'property_detail', 'click', 'requirement-submit-failed',
-                //    window.getErrorMessageFromErrorCode(errorCode, api))
             })
             .always(function () {
                 $submit.prop('disabled', false)
