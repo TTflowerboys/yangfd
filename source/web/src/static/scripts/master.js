ee573d913026e8b10ce75a4b6ce8647cb1745f60
ChaoWang <wangchao0721@gmail.com>
2015-05-21 18:48:11 +0800

    [web]Add: issue #6597 add mobile app download float bar(wait for test)

--- source/web/src/static/scripts/master.js
+++ source/web/src/static/scripts/master.js
@@ -1,18 +1,35 @@
-window.project.updateMenuTitle($('title').text());
+$(function () {
+    window.project.updateMenuTitle($('title').text());
 
-$(window).scroll(function(){
-    if ($(this).scrollTop() > $(this).height()) {
-        $('#floatBar #scrollToTop').show();
-    } else {
-        $('#floatBar #scrollToTop').hide();
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > $(this).height()) {
+            $('#floatBar #scrollToTop').show();
+        } else {
+            $('#floatBar #scrollToTop').hide();
+        }
+    });
+
+    $('#floatBar #scrollToTop').click(function () {
+        $('html, body').animate({scrollTop : 0},400);
+        return false;
+    })
+
+    $('#floatBar .section').mouseenter(function (e) {
+        ga('send', 'event', 'floatBar', 'mouseEnter', e.currentTarget.id);
+    })
+
+    //Display float app download bar based on cookie
+    if($.cookie('show-app-floatbar') === 'false'){
+        $('.app-floatbar').hide()
+    }else{
+        if (window.team.isPhone()) {
+            $('.app-floatbar').show()
+        }
     }
-});
 
-$('#floatBar #scrollToTop').click(function () {
-    $('html, body').animate({scrollTop : 0},400);
-    return false;
+    $('.app-floatbar-close').on('click',function(e){
+        $('.app-floatbar').hide()
+        $.cookie('show-app-floatbar',false)
+    })
 })
 
-$('#floatBar .section').mouseenter(function (e) {
-    ga('send', 'event', 'floatBar', 'mouseEnter', e.currentTarget.id);
-})
