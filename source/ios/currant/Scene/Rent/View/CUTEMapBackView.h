4cbf61545249316bd11ebba757fb15b5eefc07d6
Foster Yin <fyin@bbtechgroup.com>
2015-05-27 18:09:27 +0800

    [ios] Add: Map Back to list View

---
+++ source/ios/currant/Scene/Rent/View/CUTEMapBackView.h
@@ -0,0 +1,19 @@
+//
+//  CUTEMapBackView.h
+//  currant
+//
+//  Created by Foster Yin on 5/27/15.
+//  Copyright (c) 2015 Foster Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CUTEMapBackView : UIView
+
+@property (nonatomic, readonly) UIButton *button;
+
+@property (nonatomic, readonly) UILabel *label;
+
+
+
+@end
---
+++ source/ios/currant/Scene/Rent/View/CUTEMapBackView.m
@@ -0,0 +1,23 @@
+//
+//  CUTEMapBackView.m
+//  currant
+//
+//  Created by Foster Yin on 5/27/15.
+//  Copyright (c) 2015 Foster Yin. All rights reserved.
+//
+
+#import "CUTEMapBackView.h"
+
+@implementation CUTEMapBackView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+
+    }
+    return self;
+}
+
+
+@end
