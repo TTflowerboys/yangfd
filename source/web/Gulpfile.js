e47cf036af495a3baa50dfbc34ceedc9ab558cbe
levy.li <levy.li@bbtechgroup.com>
2015-06-02 20:03:21 +0800

    [Web]Mod: Issue #6710 try gulp-rev-all

--- source/web/Gulpfile.js
+++ source/web/Gulpfile.js
@@ -33,12 +33,17 @@ var preprocess = require('gulp-preprocess')
 var uglify = require('gulp-uglify');
 var pageSprite = require('gulp-page-sprite')
 var replace = require('gulp-replace')
-var argv = require('yargs').argv;
-var bower = require('gulp-bower');
+var bower = require('gulp-bower')
+var RevAll = require('gulp-rev-all')
+var fingerprint = require('gulp-fingerprint')
 
+var argv = require('yargs').argv
+
+require('events').EventEmitter.prototype._maxListeners = 1000000;
 
 var myPaths = {
     src: './src/',
+    tmp: './tmp/',
     dist: './dist/',
     html: './src/{,*/,static/emails/,static/templates/,static/templates/master/}{*.tpl.html,*.html}',
     symlink: './src/static/{themes,fonts,images,scripts,vendors,bower_components,admin/scripts,admin/templates}',
@@ -48,7 +53,7 @@ var myPaths = {
     js: './src/static/{,admin/}scripts/**/*.js',
     sprite: './sprite/',
     sprite_html: './sprite/{,*/,static/emails/,static/templates/,static/templates/master/}{*.tpl.html,*.html}',
-    sprite_dist: './dist/static/sprite/',
+    sprite_dist: './tmp/static/sprite/',
     sprite_static: './sprite/static/**/*.*',
     sprite_less: ['./sprite/static/styles/**/*.less', '!**/flycheck_*.*'],
     sprite_css: './sprite/static/styles/**/*.css',
@@ -78,7 +83,7 @@ gulp.task('debug:lint', function () {
 gulp.task('symlink', ['bower'], function () {
     return gulp.src(myPaths.symlink)
         .pipe(symlink(function (file) {
-            return file.path.replace('/src/', '/dist/')
+            return file.path.replace('/src/', '/tmp/')
         }))
 })
 
@@ -86,13 +91,13 @@ gulp.task('symlink', ['bower'], function () {
 gulp.task('less2css', function (done) {
     gulp.src(myPaths.css)
         .pipe(prefix('last 2 version', 'Firefox >= 20', 'ie 8'))
-        .pipe(gulp.dest(myPaths.dist + 'static/styles/'))
+        .pipe(gulp.dest(myPaths.tmp + 'static/styles/'))
     gulp.src(myPaths.less)
         .pipe(less().on('error', function (error) {
             console.info(chalk.white.bgRed(error.message))
         }))
         .pipe(prefix('last 2 version', 'Firefox >= 20', 'ie 8'))
-        .pipe(gulp.dest(myPaths.dist + 'static/styles/'))
+        .pipe(gulp.dest(myPaths.tmp + 'static/styles/'))
     done()
 })
 
@@ -111,16 +116,38 @@ gulp.task('html-extend', function () {
     return gulp.src(myPaths.html)
         .pipe(extender({verbose:false}))
         .pipe(preprocess({context: {ENV: 'debug'}}))
-        .pipe(gulp.dest(myPaths.dist))
+        .pipe(gulp.dest(myPaths.tmp))
 })
 
 
 
 gulp.task('clean', function () {
-    return gulp.src(myPaths.dist, {read: false})
+    return gulp.src([myPaths.tmp, myPaths.dist, myPaths.sprite], {read: false})
         .pipe(rimraf({force: true, verbose: true}))
 })
 
+gulp.task('revAll', ['build:html-extend'], function () {
+    var revAll = new RevAll();
+    return gulp.src(myPaths.tmp)
+        .pipe(gulp.dest(myPaths.dist))
+        .pipe(revAll.revision())
+        .pipe(gulp.dest(myPaths.dist))
+        .pipe(revAll.manifestFile())
+        .pipe(gulp.dest(myPaths.dist));
+})
+
+gulp.task('fingerprint', ['revAll'], function () {
+    var manifest = require(myPaths.dist + 'rev-manifest');
+
+    var options = {
+        base: './tmp/',
+        verbose: true
+    };
+
+    return gulp.src(myPaths.tmp + '**/*')
+        .pipe(fingerprint(manifest, options))
+        .pipe(gulp.dest(myPaths.dist));
+});
 
 // Build Target:
 // 'debug': local python server
@@ -128,7 +155,7 @@ gulp.task('clean', function () {
 // 'test': xxx-test.bbtechgroup.com
 // 'production': online production version
 
-gulp.task('build', ['bower', 'lint', 'clean', 'build:clean-sprite', 'build:copy-src-to-sprite', 'sprite', 'build:copy-sprite-static', 'build:less2css', 'build:html-extend', 'setupCDN'],
+gulp.task('build', ['bower', 'lint', 'clean', 'build:clean-sprite', 'build:copy-src-to-sprite', 'sprite', 'build:copy-sprite-static', 'build:less2css', 'build:html-extend', 'revAll', 'fingerprint', 'setupCDN'],
     function () {
         console.info(chalk.black.bgWhite.bold('Building tasks done!'))
     })
@@ -145,14 +172,14 @@ gulp.task('lint', function () {
 gulp.task('build:copy-sprite-static', ['clean', 'sprite'], function () {
     return gulp.src(myPaths.sprite_static)
         .pipe(imagemin())
-        .pipe(gulp.dest(myPaths.dist + 'static/'))
+        .pipe(gulp.dest(myPaths.tmp + 'static/'))
 })
 
 
 gulp.task('build:ngAnnotate', ['build:copy-sprite-static'], function () {
     return gulp.src(myPaths.src + '/static/{,admin/}scripts/**/*.js')
         .pipe(ngAnnotate())
-        .pipe(gulp.dest(myPaths.dist + 'static/'));
+        .pipe(gulp.dest(myPaths.tmp + 'static/'));
 })
 
 
@@ -173,12 +200,12 @@ gulp.task('build:less2css', ['build:copy-sprite-static'], function (done) {
     gulp.src(myPaths.sprite_css)
         .pipe(prefix('last 2 version', '> 1%', 'ie 8'))
         .pipe(minifyCss({keepSpecialComments: 0}))
-        .pipe(gulp.dest(myPaths.dist + 'static/styles/'))
+        .pipe(gulp.dest(myPaths.tmp + 'static/styles/'))
     return gulp.src(myPaths.sprite_less)
         .pipe(less())
         .pipe(prefix('last 2 version', '> 1%', 'ie 8'))
         .pipe(minifyCss({keepSpecialComments: 0}))
-        .pipe(gulp.dest(myPaths.dist + 'static/styles/'))
+        .pipe(gulp.dest(myPaths.tmp + 'static/styles/'))
     done()
 })
 
@@ -196,6 +223,8 @@ gulp.task('build:html-extend', ['build:less2css'], function () {
         .pipe(extender({verbose: false}))
         .pipe(preprocess({context: {ENV: argv.env}}))
         .pipe(publicHtmlFilter)
+        .pipe(replace(/build:css\(dist\)/g, 'build:css(tmp)'))
+        .pipe(replace(/build:js\(dist\)/g, 'build:js(tmp)'))
         .pipe(usemin({
             //TODO: Rev images
             css: ['concat', rev()],
@@ -203,35 +232,34 @@ gulp.task('build:html-extend', ['build:less2css'], function () {
         }))
         .pipe(revReplace())
         .pipe(publicHtmlFilter.restore())
-        .pipe(gulp.dest(myPaths.dist))
+        .pipe(gulp.dest(myPaths.tmp))
 })
 
-gulp.task('setupCDN', ['build:html-extend'], function () {
+gulp.task('setupCDN', ['build:html-extend', 'fingerprint', 'revAll'], function () {
     if (argv.cdn) {
-        var time = new Date().getTime()
-        var onRenameFinished =  function () {
+        var relaceRev =  function () {
             //html should only in root folder
             gulp.src(myPaths.dist + '*.html')
-                .pipe(replace(/\/static\/images\//g, argv.cdn + '/' + time + '/images/'))
-                .pipe(replace(/\/static\/sprite\//g, argv.cdn + '/' + time + '/sprite/'))
-                .pipe(replace(/\/static\/styles\//g, argv.cdn + '/' + time + '/styles/'))
-                .pipe(replace(/\/static\/vendors\//g, argv.cdn + '/' + time + '/vendors/'))
-                .pipe(replace(/\/static\/fonts\//g, argv.cdn + '/' + time + '/fonts/'))
+                .pipe(replace(/\/static\/images\//g, argv.cdn + '/images/'))
+                .pipe(replace(/\/static\/sprite\//g, argv.cdn + '/sprite/'))
+                .pipe(replace(/\/static\/styles\//g, argv.cdn + '/styles/'))
+                .pipe(replace(/\/static\/scripts\//g, argv.cdn + '/scripts/'))
+                .pipe(replace(/\/static\/vendors\//g, argv.cdn + '/vendors/'))
+                .pipe(replace(/\/static\/bower_components\//g, argv.cdn + '/bower_components/'))
+                .pipe(replace(/\/static\/fonts\//g, argv.cdn + '/fonts/'))
                 .pipe(gulp.dest(myPaths.dist))
 
-            gulp.src(myPaths.dist + 'static/' + time + '/styles/' + '**/*.css')
-                .pipe(replace(/\/static\/images\//g,  argv.cdn + '/' + time + '/images/'))
-                .pipe(replace(/\/static\/fonts\//g, argv.cdn + '/' + time + '/fonts/'))
-                .pipe(gulp.dest(myPaths.dist + 'static/' + time + '/styles/'))
+            gulp.src(myPaths.dist + 'static/styles/' + '**/*.css')
+                .pipe(replace(/\/static\/images\//g,  argv.cdn + '/images/'))
+                .pipe(replace(/\/static\/fonts\//g, argv.cdn + '/fonts/'))
+                .pipe(gulp.dest(myPaths.dist + 'static/styles/'))
 
-            gulp.src(myPaths.dist + 'static/' + time + '/scripts/' + '**/*.js')
-                .pipe(replace(/\/static\/images\//g,  argv.cdn + '/' + time + '/images/'))
-                .pipe(gulp.dest(myPaths.dist + 'static/' + time + '/scripts/'))
+            gulp.src(myPaths.dist + 'static/scripts/' + '**/*.js')
+                .pipe(replace(/\/static\/images\//g,  argv.cdn + '/images/'))
+                .pipe(gulp.dest(myPaths.dist + 'static/scripts/'))
         }
+        relaceRev()
 
-        gulp.src(myPaths.dist + '/static/**')
-            .pipe(gulp.dest(myPaths.dist + 'static/' + time))
-            .on('end', onRenameFinished);         
     }
 })
 
