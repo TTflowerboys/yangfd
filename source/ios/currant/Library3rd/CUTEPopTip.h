7f3b52a616385f37bfa7618f4842e603639c79d7
Foster Yin <fyin@bbtechgroup.com>
2015-06-05 17:00:06 +0800

    [ios] Add: ToolTips libs

---
+++ source/ios/currant/Library3rd/CUTEPopTip.h
@@ -0,0 +1,13 @@
+//
+//  CUTEPopTip.h
+//  currant
+//
+//  Created by Foster Yin on 6/5/15.
+//  Copyright (c) 2015 Foster Yin. All rights reserved.
+//
+
+#import "AMPopTip.h"
+
+@interface CUTEPopTip : AMPopTip
+
+@end
---
+++ source/ios/currant/Library3rd/CUTEPopTip.m
@@ -0,0 +1,13 @@
+//
+//  CUTEPopTip.m
+//  currant
+//
+//  Created by Foster Yin on 6/5/15.
+//  Copyright (c) 2015 Foster Yin. All rights reserved.
+//
+
+#import "CUTEPopTip.h"
+
+@implementation CUTEPopTip
+
+@end
