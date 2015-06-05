0b462586cfc331ae4e09a28326c7dd04acf1df00
levy.li <levy.li@bbtechgroup.com>
2015-06-05 12:21:06 +0800

    [Web]Mod: Issue #6710 缓存问题

--- source/web/src/static/vendors/browser_update.js
+++ source/web/src/static/vendors/browser_update.js
@@ -1,9 +1,9 @@
 var $buoop = {vs:{i:8,f:20,o:12.1,s:5.1},c:2};
 function $buo_f(){
-    var e = document.createElement('script')
-    var url = '/static/vendors' + '/browser_update_check.js'
-    e.src = url
-    document.body.appendChild(e);
+    var s = document.createElement('script')
+    var url = '/static/vendors/browser_update_check.js'
+    s.src = url
+    document.body.appendChild(s);
 };
 try {document.addEventListener('DOMContentLoaded', $buo_f,false)}
 catch(e){window.attachEvent('onload', $buo_f)}
