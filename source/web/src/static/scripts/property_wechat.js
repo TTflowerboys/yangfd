cbdd50e65c3b351167202a7260da6115f7e37418
ChaoWang <wangchao0721@gmail.com>
2015-06-06 20:58:50 +0800

    [web]Fix: remove wechat share description for now

--- source/web/src/static/scripts/property_wechat.js
+++ source/web/src/static/scripts/property_wechat.js
@@ -52,9 +52,9 @@
                 imgUrl:propertyImage,
             }
             window.wx.onMenuShareTimeline(wechatShareData);
-            if (property.decription) {
+            /*if (property.decription) {
                 wechatShareData.desc = property.description.replace(/(\r\n|\n|\r)/gm,'')
-            }
+            }*/
             window.wx.onMenuShareAppMessage(wechatShareData);
             window.wx.onMenuShareQQ(wechatShareData);
         });
