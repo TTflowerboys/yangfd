f3294f626e5bf20dc0659dfdc50268c8c87088bf
levy.li <levy.li@bbtechgroup.com>
2015-06-10 19:51:23 +0800

    [Web]Mod: Issue #6813 手机和web两种效果(in progress)

--- source/web/src/static/scripts/introjsInit.js
+++ source/web/src/static/scripts/introjsInit.js
@@ -9,36 +9,41 @@
         'prevLabel': window.i18n('上一条'),
     }).start()
 //}
+function startIntroOnPhone() {
 
-function startIntro() {
+}
+
+function startIntroOnWeb() {
     var intro = introJs();
-    var webSteps = [
-        {
-            element: $('#contactRequest')[0],
-            intro: '看到中意的房产，可以点击此处的收藏按钮来收藏，也可以点击分享按钮将房产分享给您的朋友',
-            position: 'bottom'
-        }
-    ]
     intro.setOptions({
         steps: [
             {
-                element: $('.floatBar_phone')[0],
-                intro: '点击左边的电话可以查看房东的完整联系方式',
-                tooltipPosition: '',
-                position: 'top'
+                element: $('#contactRequest')[0],
+                intro: '看到中意的房产，可以点击此处的收藏按钮来收藏，也可以点击分享按钮将房产分享给您的朋友',
+                position: 'bottom'
             },
             {
-                element: $('.floatBar_phone')[0],
-                intro: '点击右边的红心可以收藏中意的房产',
-                position: 'top'
+                element: $('.actions tr').eq(0)[0],
+                intro: '看到中意的房产，可以点击此处的收藏按钮来收藏，也可以点击分享按钮将房产分享给您的朋友',
+                position: 'bottom'
             },
         ],
-        'showStepNumbers': false,
-        'scrollToElement': false,
         'skipLabel': window.i18n('跳过'),
+        'doneLabel': window.i18n('关闭'),
         'nextLabel': window.i18n('下一条'),
         'prevLabel': window.i18n('上一条'),
     })
     intro.start()
 }
-startIntro()
+
+function initIntro() {
+    //if(!($.cookie('introjs') === 'hasShow')) {
+    //    $.cookie('introjs', 'hasShow')
+        if(window.team.isPhone()) {
+            startIntroOnPhone()
+        } else {
+            startIntroOnWeb()
+        }
+    //}
+}
+initIntro()
