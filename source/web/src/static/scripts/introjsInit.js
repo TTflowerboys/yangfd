bd1b3c24e3f5c49df90f15e33732dc012f37bd59
levy.li <levy.li@bbtechgroup.com>
2015-06-10 20:33:55 +0800

    [Web]Mod: Issue #6813 progress

--- source/web/src/static/scripts/introjsInit.js
+++ source/web/src/static/scripts/introjsInit.js
@@ -1,30 +1,22 @@
 /**
  * Created by levy on 15-6-10.
  */
-//if(!($.cookie('introjs') === 'hasShow')) {
-    $.cookie('introjs', 'hasShow')
-    introJs().setOptions({
-        'skipLabel': window.i18n('跳过'),
-        'nextLabel': window.i18n('下一条'),
-        'prevLabel': window.i18n('上一条'),
-    }).start()
-//}
 function startIntroOnPhone() {
 
 }
 
 function startIntroOnWeb() {
-    var intro = introJs();
+    var intro = window.introJs();
     intro.setOptions({
         steps: [
             {
                 element: $('#contactRequest')[0],
-                intro: '看到中意的房产，可以点击此处的收藏按钮来收藏，也可以点击分享按钮将房产分享给您的朋友',
+                intro: i18n('小提示：点击此处可以查看房东的完整联系方式'),
                 position: 'bottom'
             },
             {
                 element: $('.actions tr').eq(0)[0],
-                intro: '看到中意的房产，可以点击此处的收藏按钮来收藏，也可以点击分享按钮将房产分享给您的朋友',
+                intro: i18n('小提示：看到中意的房产，可以点击此处的收藏按钮来收藏，也可以点击分享按钮将房产分享给您的朋友'),
                 position: 'bottom'
             },
         ],
