20dd899fa3c6a943854ee77f7d80cd15bd971131
Foster Yin <fyin@bbtechgroup.com>
2015-06-12 21:36:35 +0800

    [ios] Add: Update hint

--- source/ios/currant/Scene/Rent/Form/CUTERentContactDisplaySettingForm.m
+++ source/ios/currant/Scene/Rent/Form/CUTERentContactDisplaySettingForm.m
@@ -17,13 +17,13 @@
     return @[
              @{FXFormFieldKey: @"displayPhone", FXFormFieldTitle: STR(@"展示电话"), FXFormFieldCell: [CUTEFormSwitchCell class]},
              @{FXFormFieldKey: @"displayEmail", FXFormFieldTitle: STR(@"展示邮箱"), FXFormFieldCell: [CUTEFormSwitchCell class]},
-             @{FXFormFieldKey: @"wechat", FXFormFieldTitle: STR(@"微信号"), FXFormFieldCell: [CUTEFormTextFieldCell class], FXFormFieldFooter:STR(@"您选择的联系方式仅展示给平台注册的租客")}
+             @{FXFormFieldKey: @"wechat", FXFormFieldTitle: STR(@"微信号"), FXFormFieldPlaceholder:STR(@"点击填写微信号"), FXFormFieldCell: [CUTEFormTextFieldCell class], FXFormFieldFooter:STR(@"您选择的联系方式仅展示给平台注册的租客")}
              ];
 }
 
 - (NSError *)validateFormWithScenario:(NSString *)scenario {
     if (!self.displayPhone && !self.displayEmail && IsNilNullOrEmpty(self.wechat)) {
-        return [NSError errorWithDomain:@"CUTE" code:-1 userInfo:@{NSLocalizedDescriptionKey: STR(@"请至少展示一种联系方式给租客")}];
+        return [NSError errorWithDomain:@"CUTE" code:-1 userInfo:@{NSLocalizedDescriptionKey: STR(@"至少需要展示一种联系方式")}];
     }
     return nil;
 }
