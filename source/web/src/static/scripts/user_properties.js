fe46a91b497a77b4b0cc3a726406f5d315a0cdde
ChaoWang <wangchao0721@gmail.com>
2015-06-17 19:09:53 +0800

    [web]Mod: issue #6892 done (wait for test)

--- source/web/src/static/scripts/user_properties.js
+++ source/web/src/static/scripts/user_properties.js
@@ -11,60 +11,86 @@ $(function () {
 
     //Init page with rent
     //TODO: do this for for production sync
-    if(team.isProduction()){
+    if (team.isProduction()) {
 
         // Display header buttons and tabs based on whether user have beta_renting role or not
-        if(!_.isEmpty(window.user.role) && _.indexOf(window.user.role,'beta_renting') !== -1){
-            if(team.isPhone()){
+        if (!_.isEmpty(window.user.role) && _.indexOf(window.user.role, 'beta_renting') !== -1) {
+            if (team.isPhone()) {
                 $headerTabs.show()
-            }else{
+            } else {
                 $headerButtons.show()
             }
 
             loadRentProperty()
-        }else{
+        } else {
             $headerButtons.hide()
             $headerTabs.hide()
             switchTypeTab('own')
             loadOwnProperty()
         }
 
-    }else {
-        if(team.isPhone()){
+    } else {
+        if (team.isPhone()) {
             $headerTabs.show()
-        }else{
+        } else {
             $headerButtons.show()
         }
         loadRentProperty()
     }
+
+    // Check url query from email
+    if(team.getQuery('type') === 'rent_ticket' && team.getQuery('id')){
+
+        $(document).on("rentListLoadedSuccessfully", function (e) {
+
+            if(_.first(_.where(window.rentArray, {id: team.getQuery('id')}))){
+                var cRent = _.first(_.where(window.rentArray, {id: team.getQuery('id')}))
+                $('html, body').animate({
+                    scrollTop: $('.rentCard[data-id=' + cRent.id + ']').offset().top - 100
+                }, 500)
+
+                if(team.getQuery('action') === 'refresh'  && cRent.status === 'to rent'){
+                    $('.rentCard[data-id=' + cRent.id + ']').find('.actions #refresh').click()
+                }else if(team.getQuery('action') === 'confirm_rent' && cRent.status === 'to rent'){
+                    $('.rentCard[data-id=' + cRent.id + ']').find('.actions #editAction').click()
+                }else{
+                    window.alert(window.i18n('无法执行该操作，请检查您的房产状态后重试'))
+                }
+            }else{
+                window.alert(window.i18n('您要找的房产不存在或者已被删除'))
+            }
+
+        })
+    }
+
     $('body').dropload({ //下拉刷新
-        domUp : {
-            domClass   : 'dropload-up',
-            domRefresh : '<div class="dropload-refresh">↓ ' + i18n('下拉刷新') + '</div>',
-            domUpdate  : '<div class="dropload-update">↑ ' + i18n('松开刷新') + '</div>',
-            domLoad    : '<div class="dropload-load"><span class="loading"></span>' + i18n('加载中...') + '</div>'
+        domUp: {
+            domClass: 'dropload-up',
+            domRefresh: '<div class="dropload-refresh">↓ ' + i18n('下拉刷新') + '</div>',
+            domUpdate: '<div class="dropload-update">↑ ' + i18n('松开刷新') + '</div>',
+            domLoad: '<div class="dropload-load"><span class="loading"></span>' + i18n('加载中...') + '</div>'
         },
-        loadUpFn : function(me){
-            if(isLoading){
+        loadUpFn: function (me) {
+            if (isLoading) {
                 return me.resetload();
             }
             loadProperty().done(function () {
                 $('.dropload-load').html(i18n('加载成功'))
                 setTimeout(function () {
                     me.resetload()
-                },500)
+                }, 500)
             }).fail(function () {
                 $('.dropload-load').html(i18n('加载失败'))
                 setTimeout(function () {
                     me.resetload()
-                },500)
+                }, 500)
             })
         },
 
     });
     function loadProperty() {
         var defer = $.Deferred()
-        if($('.buttons .button').hasClass('own')) {
+        if ($('.buttons .button').hasClass('own')) {
             loadOwnProperty().then(function () {
                 defer.resolve()
             }, function () {
@@ -79,9 +105,10 @@ $(function () {
         }
         return defer.promise()
     }
+
     function loadOwnProperty() {
         var defer = $.Deferred()
-        if(xhr && xhr.readyState !== 4) {
+        if (xhr && xhr.readyState !== 4) {
             xhr.abort()
         }
         $placeholder.hide()
@@ -98,16 +125,16 @@ $(function () {
                 //Check if tab is still rent
                 //TODO:Disable check for production sync
                 //if ($('.buttons .own').hasClass('button')) {
-                    var val = data.val
-                    var array = val
-                    if (array && array.length > 0) {
-                        _.each(array, function (ticket) {
-                            var houseResult = _.template($('#houseCard_template').html())({ticket: ticket})
-                            $list.append(houseResult)
-                        })
-                    } else {
-                        $ownPlaceholder.show()
-                    }
+                var val = data.val
+                var array = val
+                if (array && array.length > 0) {
+                    _.each(array, function (ticket) {
+                        var houseResult = _.template($('#houseCard_template').html())({ticket: ticket})
+                        $list.append(houseResult)
+                    })
+                } else {
+                    $ownPlaceholder.show()
+                }
                 //}
                 defer.resolve()
             }).fail(function () {
@@ -123,7 +150,7 @@ $(function () {
 
     function loadRentProperty() {
         var defer = $.Deferred()
-        if(xhr && xhr.readyState !== 4) {
+        if (xhr && xhr.readyState !== 4) {
             xhr.abort()
         }
         $placeholder.hide()
@@ -145,7 +172,7 @@ $(function () {
                     if (val && val.length > 0) {
                         window.rentArray = val
                         _.each(val, function (rent) {
-                            if(rent.property){
+                            if (rent.property) {
                                 var houseResult = _.template($('#my_rentCard_template').html())({rent: rent})
                                 $list.append(houseResult)
                             }
@@ -161,6 +188,11 @@ $(function () {
                     }
                 }
                 defer.resolve()
+
+                // Trigger custom event when load successfully
+                $(document).trigger({
+                    type: 'rentListLoadedSuccessfully'
+                })
             }).fail(function () {
                 $rentPlaceholder.show()
                 defer.reject()
@@ -178,12 +210,13 @@ $(function () {
         $('.buttons .button').removeClass('button').addClass('ghostButton')
         $('.buttons .' + state).removeClass('ghostButton').addClass('button')
     }
+
     $(window).on('hashchange', function () {
         var state = location.hash.slice(1)
-        if(state === 'rent') {
+        if (state === 'rent') {
             switchTypeTab(state)
             loadRentProperty()
-        } else if(state === 'own') {
+        } else if (state === 'own') {
             switchTypeTab(state)
             loadOwnProperty()
         }
@@ -223,7 +256,7 @@ $(function () {
 
             if (window.bridge !== undefined) {
                 window.bridge.callHandler('shareRentTicket', _.first(_.where(window.rentArray, {id: ticketId})));
-            }else{
+            } else {
                 $('#popupShareToWeChat')
                     .find('img').prop('src',
                         '/qrcode/generate?content=' + encodeURIComponent(location.origin + '/wechat-poster/' + ticketId)).end()
@@ -237,19 +270,21 @@ $(function () {
     function bindRentItemRefreshClick() {
         $('.actions #refresh').click(function (e) {
             var ticketId = $(e.target).attr('data-id')
-            var params = {
-                'status': 'to rent'
+            if(!team.isToday(parseInt(_.first(_.where(window.rentArray, {id: ticketId})).last_modified_time))){
+                var params = {
+                    'status': 'to rent'
+                }
+                $.betterPost('/api/1/rent_ticket/' + ticketId + '/edit', params)
+                    .done(function (data) {
+                        $(e.target).unbind('click')
+                        $(e.target).text(window.i18n('刚刚刷新过'))
+                        $(e.target).parent().parent().parent().find('.imgAction_wrapper .date').text(window.i18n('刚刚刷新过'))
+                        $(e.target).addClass('disabled')
+                    })
+                    .fail(function (ret) {
+                        window.alert(window.i18n('刷新失败'))
+                    })
             }
-            $.betterPost('/api/1/rent_ticket/' + ticketId + '/edit', params)
-                .done(function (data) {
-                    $(e.target).unbind('click')
-                    $(e.target).text(window.i18n('刚刚刷新过'))
-                    $(e.target).parent().parent().parent().find('.imgAction_wrapper .date').text(window.i18n('刚刚刷新过'))
-                    $(e.target).addClass('disabled')
-                })
-                .fail(function (ret) {
-                    window.alert(window.i18n('刷新失败'))
-                })
         })
     }
 
@@ -257,24 +292,31 @@ $(function () {
         $('.actions #editAction').click(function (e) {
             var ticketId = $(e.target).attr('data-id')
 
-            if($(e.target).attr('data-type') === 'to rent'){
-                if (window.confirm(window.i18n('确定已经成功出租了吗？确定将不再接收系统刷新提醒')) === true) {
+            if ($(e.target).attr('data-type') === 'to rent') {
+                if (window.confirm(window.i18n('确定\"'+_.first(_.where(window.rentArray, {id: ticketId})).title + '\"已经成功出租了吗？确定将不再接收系统刷新提醒')) === true) {
                     var params = {
                         'status': 'rent'
                     }
                     $.betterPost('/api/1/rent_ticket/' + ticketId + '/edit', params)
                         .done(function (data) {
-                            location.reload()
+                            window.rentArray[_.indexOf(window.rentArray,_.first(_.where(window.rentArray, {id: ticketId})))] = data
+                            $(e.target).text(window.i18n('重新发布'))
+                            $(e.target).attr('data-type','rent')
+                            $(e.target).parent().parent().parent().find('.imgAction_wrapper .date').text(window.i18n('今天更新过'))
+                            $(e.target).parent().parent().parent().parent().parent().find('.status').text(window.i18n('已出租'))
+
+                            //Remove refresh from rent status
+                            $(e.target).parent().prev().remove()
                         })
                         .fail(function (ret) {
-                            window.alert(window.i18n('失败'))
+                            window.alert(window.i18n('无法更新，请检查后重试'))
                         })
                 }
-            }else {
+            } else {
                 //Bind to it's edit button
                 if (window.bridge !== undefined) {
-                    window.bridge.callHandler('editRentTicket',  _.first(_.where(window.rentArray, {id: ticketId})))
-                }else{
+                    window.bridge.callHandler('editRentTicket', _.first(_.where(window.rentArray, {id: ticketId})))
+                } else {
                     location.href = '/property-to-rent/' + ticketId + '/edit'
                 }
             }
@@ -286,8 +328,8 @@ $(function () {
             var ticketId = $(e.target).attr('data-id')
 
             if (window.bridge !== undefined) {
-                window.bridge.callHandler('editRentTicket',  _.first(_.where(window.rentArray, {id: ticketId})))
-            }else{
+                window.bridge.callHandler('editRentTicket', _.first(_.where(window.rentArray, {id: ticketId})))
+            } else {
                 location.href = '/property-to-rent/' + ticketId + '/edit'
             }
         })
@@ -306,7 +348,7 @@ $(function () {
                         location.reload()
                     })
                     .fail(function (ret) {
-                        window.alert(window.i18n('失败'))
+                        window.alert(window.i18n('无法删除，请检查后重试'))
                     })
             }
         })
