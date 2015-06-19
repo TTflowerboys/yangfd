c2bf119ce72a9b3495120f20599c943f94bef16f
Foster Yin <fyin@bbtechgroup.com>
2015-06-19 16:08:40 +0800

    [ios] Add: Task #6919 Unit Test Target

---
+++ source/ios/currantUnitTest/Entity/CUTETicketTest.m
@@ -0,0 +1,22 @@
+//
+//  CUTETicketTest.m
+//  currant
+//
+//  Created by Foster Yin on 6/19/15.
+//  Copyright (c) 2015 Foster Yin. All rights reserved.
+//
+
+#import <Specta/Specta.h>
+
+
+SpecBegin(Ticket)
+
+describe(@"Ticket", ^ {
+
+    it(@"", ^ {
+
+    });
+
+});
+
+SpecEnd
