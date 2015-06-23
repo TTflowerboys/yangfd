6dc4294f1527db302d2d7010013d96bad52c30fe
Foster Yin <fyin@bbtechgroup.com>
2015-06-23 11:28:16 +0800

    [ios] Add: Task #6919 Property Unit Test

---
+++ source/ios/currant/Misc/Category/NSString+OccurrenceCount.h
@@ -0,0 +1,15 @@
+//
+//  NSString+OccurrenceCount.h
+//  currant
+//
+//  Created by Foster Yin on 6/23/15.
+//  Copyright (c) 2015 Foster Yin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (OccurrenceCount)
+
+- (NSUInteger)occurrenceCountOfCharacter:(UniChar)character;
+
+@end
