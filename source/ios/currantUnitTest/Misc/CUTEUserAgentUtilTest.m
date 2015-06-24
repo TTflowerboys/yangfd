0aefa4984ea588d2ae5f400998b8f865688b8343
Foster Yin <fyin@bbtechgroup.com>
2015-06-24 11:24:37 +0800

    [ios] Add: Remove no need test

--- source/ios/currantUnitTest/Misc/CUTEUserAgentUtilTest.m
+++ source/ios/currantUnitTest/Misc/CUTEUserAgentUtilTest.m
@@ -24,14 +24,6 @@ describe(@"setupUserAgent", ^ {
         assertThat(userAgent, containsString(@"currant"));
     });
 
-//    it(@"request should have correct user agent", ^{
-//
-//        NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"]];
-//        NSDictionary *headers = request.allHTTPHeaderFields;
-//        NSString *userAgent = [headers objectForKey:@"UserAgent"];
-//        assertThat(userAgent, containsString(@"currant"));
-//    });
-
     it(@"webview should have correct user agent", ^{
         UIWebView *webView = [UIWebView new];
         NSString *userAgent = [webView stringByEvaluatingJavaScriptFromString:@"navigator.userAgent"];
