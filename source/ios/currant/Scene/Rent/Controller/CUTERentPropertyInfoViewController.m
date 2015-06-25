aa19b1a673d9050abd2d9b2e15bf6a5080b8b9e6
Foster Yin <fyin@bbtechgroup.com>
2015-06-25 17:00:42 +0800

    [ios] Add: Fix Screen track bug for empty url

--- source/ios/currant/Scene/Rent/Controller/CUTERentPropertyInfoViewController.m
+++ source/ios/currant/Scene/Rent/Controller/CUTERentPropertyInfoViewController.m
@@ -364,8 +364,9 @@
 
             TrackScreenStayDuration(KEventCategoryPostRentTicket, GetScreenName(self));
             CUTERentTicketPreviewViewController *controller = [[CUTERentTicketPreviewViewController alloc] init];
+            controller.url = [NSURL URLWithString:CONCAT(@"/wechat-poster/", self.ticket.identifier) relativeToURL:[CUTEConfiguration hostURL]];
             controller.ticket = self.ticket;
-            [controller loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:CONCAT(@"/wechat-poster/", self.ticket.identifier) relativeToURL:[CUTEConfiguration hostURL]]]];
+            [controller loadRequest:[NSURLRequest requestWithURL:controller.url]];
             [self.navigationController pushViewController:controller animated:YES];
         }
         return task;
