1aa6997fde794cb71de2eaf49c349857f603a395
Foster Yin <fyin@bbtechgroup.com>
2015-06-30 15:29:57 +0800

    [ios] Add: Task #6999 Apply Beta UI test

---
+++ source/ios/currantUITest/Scene/User/KIFTestActor+Login.h
@@ -0,0 +1,15 @@
+//
+//  KIFTestActor+Login.h
+//  currant
+//
+//  Created by Foster Yin on 6/30/15.
+//  Copyright (c) 2015 Foster Yin. All rights reserved.
+//
+
+#import "KIFTestActor.h"
+
+@interface KIFTestActor (Login)
+
+- (void)logout;
+
+@end
