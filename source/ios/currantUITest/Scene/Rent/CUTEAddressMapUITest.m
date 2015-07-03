07a5a2ac84b4ed437c6a99ea9c26273baf0010f5
Foster Yin <fyin@bbtechgroup.com>
2015-07-03 18:11:03 +0800

    [ios] Add: Task #6999 property location edit test case

--- source/ios/currantUITest/Scene/Rent/CUTEAddressMapUITest.m
+++ source/ios/currantUITest/Scene/Rent/CUTEAddressMapUITest.m
@@ -25,7 +25,55 @@ describe(@"AddressMap", ^ {
 
     it(@"should get current location ok", ^ {
         [tester setPropertyLocationWithCurrentLocation];
-    });    
+    });
+
+    it(@"should edit location success when create", ^ {
+        [tester setPropertyLocationWithCurrentLocation];
+
+        //then move map
+        [tester swipeViewWithAccessibilityLabel:STR(@"地图") inDirection:KIFSwipeDirectionRight];
+        [tester waitForAnimationsToFinish];
+        [tester waitForAbsenceOfViewWithAccessibilityLabel:@"MapTextFieldIndicator"];
+        [tester waitForTimeInterval:3];
+    });
+
+    it(@"should edit location success when re-edit", ^ {
+        [tester waitForTimeInterval:2];
+        //location permission
+        if ([tester tryFindingViewWithAccessibilityLabel:STR(@"允许") error:nil]) {
+            [tester tapViewWithAccessibilityLabel:STR(@"允许")];
+        }
+
+        [tester waitForAnimationsToFinish];
+        [tester waitForAbsenceOfViewWithAccessibilityLabel:@"MapTextFieldIndicator"];
+        [tester waitForAnimationsToFinish];
+        [tester waitForTimeInterval:3];
+        [tester tapViewWithAccessibilityLabel:STR(@"继续")];
+        [tester waitForTimeInterval:5];
+        [tester waitForViewWithAccessibilityLabel:STR(@"房产信息表单")];
+        UITableView *tableView = nil;
+        [tester tryFindingAccessibilityElement:nil view:&tableView withIdentifier:STR(@"房产信息表单") tappable:YES error:nil];
+        assertThat(tableView, notNilValue());
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:5 inSection:1];
+        [tester swipeRowAtIndexPath:indexPath inTableView:tableView inDirection:KIFSwipeDirectionUp];
+        UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+        [tester tapRowAtIndexPath:indexPath inTableViewWithAccessibilityIdentifier:STR(@"房产信息表单")];
+        [tester waitForAnimationsToFinish];
+        [tester tryFindingAccessibilityElement:nil view:&tableView withIdentifier:STR(@"地址编辑表单") tappable:YES error:nil];
+        indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+        [tester tapRowAtIndexPath:indexPath inTableViewWithAccessibilityIdentifier:STR(@"地址编辑表单")];
+        [tester waitForAnimationsToFinish];
+        [tester setPropertyLocationWithCurrentLocation];
+        //then move map
+        [tester swipeViewWithAccessibilityLabel:STR(@"地图") inDirection:KIFSwipeDirectionRight];
+        [tester waitForAnimationsToFinish];
+        [tester waitForAbsenceOfViewWithAccessibilityLabel:@"MapTextFieldIndicator"];
+        [tester waitForTimeInterval:3];
+
+        [tester tapViewWithAccessibilityLabel:STR(@"返回")];
+        [tester waitForAnimationsToFinish];
+        [tester tapViewWithAccessibilityLabel:STR(@"返回")];
+    });
 });
 
 SpecEnd
