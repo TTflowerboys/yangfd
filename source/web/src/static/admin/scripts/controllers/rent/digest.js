45dcf4991f98e89c384ab6b773a3fe6e774cd2fa
ChaoWang <wangchao0721@gmail.com>
2015-07-08 17:04:00 +0800

    [web]Mod: remove source and medium

--- source/web/src/static/admin/scripts/controllers/rent/digest.js
+++ source/web/src/static/admin/scripts/controllers/rent/digest.js
@@ -15,24 +15,11 @@
         }
 
         $scope.generate = function () {
-            if(!$scope.digest.utm_source || !$scope.digest.utm_medium) {
-                return
-            }
-            var sourceArr = $scope.digest.utm_source.split(/\n/)
-            var mediumArr = $scope.digest.utm_medium.split(/\n/)
-            $scope.result = _.reduce(sourceArr, function (preOutside, source) {
-               return _.reduce(mediumArr, function (preInside, media) {
-                    return generateDigest(source, media, $scope.digest.type, $scope.digest.utm_campaign, $scope.digest.needTag) + preInside
-               }, '') + preOutside
-            },'')
-        }
-        function generateDigest (source, media, type, campaign, needTag) {
-            var res = location.protocol + '//' + location.host + '/track/' + $stateParams.id + '/none/' + type + '.png'
-            if(needTag) {
+            var res = location.protocol + '//' + location.host + '/track/' + $stateParams.id + '/none/' +  $scope.digest.type + '.png'
+            if($scope.digest.needTag) {
                 res = '<img src="' + res + '"/>'
             }
-            res += '\n'
-            return res
+            $scope.result = res
         }
     }
 
