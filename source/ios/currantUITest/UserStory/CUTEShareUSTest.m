4c09056bf7e7d050bcc0ae9a02e3a0e058603417
Foster Yin <fyin@bbtechgroup.com>
2015-07-09 17:25:56 +0800

    [ios] Add: Task #6999 Share test case

--- source/ios/currantUITest/UserStory/CUTEShareUSTest.m
+++ source/ios/currantUITest/UserStory/CUTEShareUSTest.m
@@ -10,15 +10,25 @@
 #import <KIF.h>
 #import "CUTECommonMacro.h"
 #import "CUTEShareManager.h"
+#import "CUTEDataManager.h"
+#import "KIFUITestActor+Login.h"
 
 
 SpecBegin(ShareUS)
 
 describe(@"share ticket", ^ {
+    before(^{
+        [tester login];
+    });
 
     it(@"should success", ^ {
-        
+        CUTETicket *ticket = [[[CUTEDataManager sharedInstance] getAllUnfinishedRentTickets] firstObject];
+        if(ticket) {
 
+        }
+        [[CUTEShareManager sharedInstance] shareTicket:ticket];
+        [tester waitForViewWithAccessibilityLabel:STR(@"分享")];
+        
     });
     
 });
