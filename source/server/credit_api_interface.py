42e6bd4e678a40fff7d5c3ab5efc136d5a1eb63f
Felix Yan <felixonmars@archlinux.org>
2015-07-10 14:18:59 +0800

    [server] Add: credit api interface

---
+++ source/server/credit_api_interface.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals, absolute_import
+from app import f_app
+from libfelix.f_interface import f_api, abort
+
+
+@f_api("/credit/<credit_type>/amount")
+@f_app.user.login.check(force=True)
+def credit_amount(credit_type, user):
+    """
+    ``credit_type`` should be ``view_rent_ticket_contact_info``.
+    """
+    assert credit_type in ["view_rent_ticket_contact_info", ], abort(40000)
+
+    return f_app.user.credit.get(credit_type, user_id=user["id"], amount_only=True)
