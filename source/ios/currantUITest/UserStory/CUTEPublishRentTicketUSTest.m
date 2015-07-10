8114f8a7db13d5d5514dd248bbe5118cda0d6172
Foster Yin <fyin@bbtechgroup.com>
2015-07-10 16:43:30 +0800

    [ios] Add: Task #6999 Publish rent  ticket test

--- source/ios/currantUITest/UserStory/CUTEPublishRentTicketUSTest.m
+++ source/ios/currantUITest/UserStory/CUTEPublishRentTicketUSTest.m
@@ -20,10 +20,17 @@ SpecBegin(PublishRentTiecktUS)
 
 describe(@"PublishRentTicket", ^ {
 
+    before(^{
+        [tester logout];
+    });
+
     it(@"should publish success", ^ {
         [tester login];
+        //wait for the unfinished ticket load
+        [tester waitForTimeInterval:10];
         [tester selectRentTypeWhole];
         [tester setPropertyLocationWithCurrentLocation];
+        [tester waitForTimeInterval:3];
         [tester tapViewWithAccessibilityLabel:STR(@"继续")];
         [tester setBedroomCount];
         [tester tapViewWithAccessibilityLabel:STR(@"预览")];
@@ -31,11 +38,45 @@ describe(@"PublishRentTicket", ^ {
         [tester tapViewWithAccessibilityLabel:STR(@"继续")];
         [tester waitForAnimationsToFinish];
         [tester tapViewWithAccessibilityLabel:STR(@"发布")];
+        [tester waitForTimeInterval:5];
+        [tester waitForViewWithAccessibilityLabel:STR(@"发布成功")];
+        [tester waitForAnimationsToFinish];
+        [tester waitForViewWithAccessibilityLabel:STR(@"分享")];
+        [tester waitForAnimationsToFinish];
+
+        [system waitForApplicationToOpenAnyURLWhileExecutingBlock:^{
+            [tester tapViewWithAccessibilityLabel:STR(@"微信好友")];
+        } returning:YES];
+        [tester waitForTimeInterval:3];
+        [tester tapViewWithAccessibilityLabel:STR(@"完成")];
+    });
+
+    it(@"should publish success with photoes", ^ {
+        [tester login];
+        //wait for the unfinished ticket load
+        [tester waitForTimeInterval:10];
+        [tester selectRentTypeWhole];
+        [tester waitForViewWithAccessibilityLabel:STR(@"房产位置")];
+        [tester setPropertyLocationWithCurrentLocation];
+        [tester waitForTimeInterval:3];
+        [tester tapViewWithAccessibilityLabel:STR(@"继续")];
+        [tester setBedroomCount];
+        [tester tapViewWithAccessibilityLabel:STR(@"添加照片")];
+        [tester swipeViewWithAccessibilityLabel:STR(@"房产信息列表") inDirection:KIFSwipeDirectionDown];
+        [tester waitForViewWithAccessibilityLabel:STR(@"选择照片")];
+        [tester tapRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0] inTableViewWithAccessibilityIdentifier:STR(@"房产信息表单")];
+        [tester choosePhotoInAlbum:STR(@"相机胶卷") atRow:0 column:0];
+        [tester tapViewWithAccessibilityLabel:STR(@"预览")];
+        [tester waitForAnimationsToFinish];
+        [tester tapViewWithAccessibilityLabel:STR(@"继续")];
+        [tester waitForAnimationsToFinish];
+        [tester tapViewWithAccessibilityLabel:STR(@"发布")];
+        [tester waitForTimeInterval:5];
+        [tester waitForViewWithAccessibilityLabel:STR(@"发布成功")];
+        [tester waitForAnimationsToFinish];
+        [tester waitForViewWithAccessibilityLabel:STR(@"分享")];
         [tester waitForAnimationsToFinish];
     });
-    
 });
 
 SpecEnd
-
-
