a8cb32f80140318a2075eb4a072fdf8011e24f46
levy.li <levy.li@bbtechgroup.com>
2015-07-11 16:13:39 +0800

    [Web]Mod: Issue #7060 重构dashboard中的misc.getChangedI18nAttributes方法

--- source/web/src/static/admin/scripts/factories/misc.js
+++ source/web/src/static/admin/scripts/factories/misc.js
@@ -84,70 +84,63 @@ angular.module('app')
                 }
                 var allKeys = _.union(_.keys(newJson), _.keys(oldJson))
 
-                for (var keyIndex = 0, keyLength = allKeys.length; keyIndex < keyLength; keyIndex += 1) {
-                    var key = allKeys[keyIndex]
+                _.each(allKeys, function (key) {
                     if (oldJson[key] === undefined) {
-                        addToResult(key, newJson[key])
-                    } else if (newJson[key] === undefined) {
+                        return addToResult(key, newJson[key])
+                    }
+                    if (newJson[key] === undefined) {
                         if (oldJson[key]._i18n_unit !== undefined) {
                             newJson[key] = angular.copy(oldJson[key])
                             newJson[key].value = '0'
                             delete newJson[key].type
                             delete newJson[key].value_float
-                            addToResult(key, newJson[key])
-                        } else {
-                            if (_.isNumber(oldJson[key])) {
-                                addToResult(key, 0)
-                            } else if (oldJson[key] === true) {
-                                addToResult(key, false)
-                            } else {
-                                if (_.isEmpty(oldJson[key])) {
-                                    continue
-                                }
-                                if (_.isObject(oldJson[key])) {
-                                    addToResult(key, {})
-                                } else {
-                                    addToResult(key, '')
-                                }
-                            }
+                            return addToResult(key, newJson[key])
                         }
-                    } else if (oldJson[key] === null) {
-                        if (newJson[key] !== null) {
-                            addToResult(key, newJson[key])
+                        if (_.isNumber(oldJson[key])) {
+                            return addToResult(key, 0)
                         }
+                        if (oldJson[key] === true) {
+                            return addToResult(key, false)
+                        }
+                        if (_.isEmpty(oldJson[key])) {
+                            return
+                        }
+                        if (_.isObject(oldJson[key])) {
+                            return addToResult(key, {})
+                        }
+                        return addToResult(key, '')
                     }
-                    else if (newJson.hasOwnProperty(key) &&
+                    if (oldJson[key] === null && newJson[key] !== null) {
+                        return addToResult(key, newJson[key])
+                    }
+                    if (newJson.hasOwnProperty(key) &&
                         newJson[key] !== oldJson[key] &&
                         newJson[key].toString() !== oldJson[key].toString()) {
-                        addToResult(key, newJson[key])
-                    } else if (_.isArray(newJson[key])) {
-                        if (!angular.equals(newJson[key], oldJson[key])) {
-                            addToResult(key, newJson[key])
+                        return addToResult(key, newJson[key])
+                    }
+                    if (_.isArray(newJson[key]) && !angular.equals(newJson[key], oldJson[key])) {
+                        return addToResult(key, newJson[key])
+                    }
+                    if (_.isObject(newJson[key])) {
+                        var obj = newJson[key],
+                            temp
+                        if (obj._i18n !== undefined && !angular.equals(newJson[key], oldJson[key])) {
+                            return addToResult(key, newJson[key])
                         }
-                    } else if (_.isObject(newJson[key])) {
-                        var obj = newJson[key]
-                        if (obj._i18n !== undefined) {
-                            if (!angular.equals(newJson[key], oldJson[key])) {
-                                addToResult(key, newJson[key])
-                            }
-                        } else if (obj._i18n_unit !== undefined) {
-                            if (!angular.equals(newJson[key], oldJson[key])) {
-                                delete newJson[key].value_float
-                                delete newJson[key].type
-                                addToResult(key, newJson[key])
-                            }
-                        } else if (key === 'unit_price') {
-                            if (!angular.equals(newJson[key], oldJson[key])) {
-                                addToResult(key, newJson[key])
-                            }
-                        } else {
-                            var temp = self.getChangedI18nAttributes(newJson[key], oldJson[key])
-                            if (temp) {
-                                addToResult(key, temp)
-                            }
+                        if (obj._i18n_unit !== undefined && !angular.equals(newJson[key], oldJson[key])) {
+                            delete newJson[key].value_float
+                            delete newJson[key].type
+                            return addToResult(key, newJson[key])
+                        }
+                        if (key === 'unit_price' && !angular.equals(newJson[key], oldJson[key])) {
+                            return addToResult(key, newJson[key])
+                        }
+                        temp = self.getChangedI18nAttributes(newJson[key], oldJson[key])
+                        if (temp) {
+                            return addToResult(key, temp)
                         }
                     }
-                }
+                })
                 return _.isEmpty(result) ? undefined : result
             },
             resetArray: function (array, items) {
