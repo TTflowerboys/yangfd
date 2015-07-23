a60e8bc2e8c9d3bdf0223a5f12f244c9775a0c54
Foster Yin <fyin@bbtechgroup.com>
2015-07-23 17:32:22 +0800

    [ios] Add: Update CUTEShareViewController show wechat-poster preview

--- source/ios/currant/Scene/Rent/Controller/CUTERentShareViewController.m
+++ source/ios/currant/Scene/Rent/Controller/CUTERentShareViewController.m
@@ -24,6 +24,7 @@
 #import "CUTEApptentiveEvent.h"
 #import "CUTERentShareForm.h"
 #import "CUTETracker.h"
+#import "UIBarButtonItem+ALActionBlocks.h"
 
 @interface CUTERentShareViewController ()
 
@@ -31,24 +32,30 @@
 
 @implementation CUTERentShareViewController
 
-- (CUTERentShareForm *)form {
-    return (CUTERentShareForm *)self.formController.form;
-}
+//- (CUTERentShareForm *)form {
+//    return (CUTERentShareForm *)self.formController.form;
+//}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:STR(@"完成") style:UIBarButtonItemStylePlain target:self action:@selector(onDoneButtonPressed:)];
     self.navigationItem.title = STR(@"发布成功");
 
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:STR(@"完成") style:UIBarButtonItemStylePlain block:^(id weakSender) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:STR(@"分享") style:UIBarButtonItemStylePlain block:^(id weakSender) {
+        [self shareRentTicket];
+    }];
+
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:CONCAT(@"/wechat-poster/", self.ticket.identifier) relativeToURL:[CUTEConfiguration hostURL]]]];
         [self shareRentTicket];
     });
 }
 
 - (void)shareRentTicket {
-    [[[CUTEShareManager sharedInstance] shareTicket:self.form.ticket viewController:self onButtonPressBlock:^(NSString * buttonName) {
+    [[[CUTEShareManager sharedInstance] shareTicket:self.ticket viewController:self onButtonPressBlock:^(NSString * buttonName) {
         if ([buttonName isEqualToString:CUTEShareServiceWechatFriend]) {
             TrackScreen(@"share-to-wechat");
         }
@@ -100,73 +107,82 @@
     } ];
 }
 
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath  {
-    FXFormField *field = [self.formController fieldForIndexPath:indexPath];
-    if ([field.key isEqualToString:@"view"]) {
-        [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
-    }
-    else if ([field.key isEqualToString:@"edit"]) {
-        [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
-    }
-    else if ([field.key isEqualToString:@"qrcode"]) {
-        CUTEQrcodeCell *qrcodeCell = (CUTEQrcodeCell *)cell;
-        NSURL *originalURL = [NSURL URLWithString:CONCAT(@"/wechat-poster/", self.form.ticket.identifier) relativeToURL:[CUTEConfiguration hostURL]];
-        NSString *content = [[originalURL absoluteString] URLEncode];
-        NSString *path = CONCAT(@"/qrcode/generate?content=", content);
-        NSURL *url = [NSURL URLWithString:path relativeToURL:[CUTEConfiguration hostURL]];
-        [qrcodeCell.qrcodeView setImageWithURL:[NSURL URLWithString:url.absoluteString]];
-    }
+//- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath  {
+//    FXFormField *field = [self.formController fieldForIndexPath:indexPath];
+//    if ([field.key isEqualToString:@"view"]) {
+//        [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
+//    }
+//    else if ([field.key isEqualToString:@"edit"]) {
+//        [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
+//    }
+//    else if ([field.key isEqualToString:@"qrcode"]) {
+//        CUTEQrcodeCell *qrcodeCell = (CUTEQrcodeCell *)cell;
+//        NSURL *originalURL = [NSURL URLWithString:CONCAT(@"/wechat-poster/", self.form.ticket.identifier) relativeToURL:[CUTEConfiguration hostURL]];
+//        NSString *content = [[originalURL absoluteString] URLEncode];
+//        NSString *path = CONCAT(@"/qrcode/generate?content=", content);
+//        NSURL *url = [NSURL URLWithString:path relativeToURL:[CUTEConfiguration hostURL]];
+//        [qrcodeCell.qrcodeView setImageWithURL:[NSURL URLWithString:url.absoluteString]];
+//    }
+//}
+//
+//- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+//    FXFormField *field = [self.formController fieldForIndexPath:indexPath];
+//    if ([field.key isEqualToString:@"view"]) {
+//        CUTEWebViewController *controller = [[CUTEWebViewController alloc] init];
+//        controller.url = [NSURL URLWithString:CONCAT(@"/wechat-poster/", self.form.ticket.identifier) relativeToURL:[CUTEConfiguration hostURL]];
+//        [controller loadRequest:[NSURLRequest requestWithURL:controller.url]];
+//        [self.navigationController pushViewController:controller animated:YES];
+//    }
+//    if ([field.key isEqualToString:@"edit"]) {
+//        [SVProgressHUD show];
+//        [[[CUTEAPIManager sharedInstance] POST:CONCAT(@"/api/1/rent_ticket/", self.form.ticket.identifier) parameters:nil resultClass:[CUTETicket class]] continueWithBlock:^id(BFTask *task) {
+//            if (task.error) {
+//                [SVProgressHUD showErrorWithError:task.error];
+//            }
+//            else if (task.exception) {
+//                [SVProgressHUD showErrorWithException:task.exception];
+//            }
+//            else if (task.isCancelled) {
+//                [SVProgressHUD showErrorWithCancellation];
+//            }
+//            else {
+//                if (task.result) {
+//                    [SVProgressHUD dismiss];
+//                    [[NSNotificationCenter defaultCenter] postNotificationName:KNOTIF_TICKET_EDIT object:self userInfo:@{@"ticket": task.result}];
+//                }
+//                else {
+//                    [SVProgressHUD showErrorWithStatus:STR(@"获取失败")];
+//                }
+//            }
+//
+//            return task;
+//        }];
+//    }
+//    else if ([field.key isEqualToString:@"copyLink"]) {
+//        [UIPasteboard generalPasteboard].string = [[NSURL URLWithString:CONCAT(@"/wechat-poster/", self.form.ticket.identifier) relativeToURL:[CUTEConfiguration hostURL]] absoluteString];
+//        [SVProgressHUD showSuccessWithStatus:STR(@"已复制至粘贴版")];
+//    }
+//    else if ([field.key isEqualToString:@"qrcode"]) {
+//        [tableView deselectRowAtIndexPath:indexPath animated:NO];
+//    }
+//    else if ([field.key isEqualToString:@"wechat"]) {
+//        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+//        [self shareRentTicket];
+//    }
+//
+//}
+
+//overrride with empty implementation, do not update barButtonItem
+- (void)updateBackButton {
+
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    FXFormField *field = [self.formController fieldForIndexPath:indexPath];
-    if ([field.key isEqualToString:@"view"]) {
-        CUTEWebViewController *controller = [[CUTEWebViewController alloc] init];
-        controller.url = [NSURL URLWithString:CONCAT(@"/wechat-poster/", self.form.ticket.identifier) relativeToURL:[CUTEConfiguration hostURL]];
-        [controller loadRequest:[NSURLRequest requestWithURL:controller.url]];
-        [self.navigationController pushViewController:controller animated:YES];
-    }
-    if ([field.key isEqualToString:@"edit"]) {
-        [SVProgressHUD show];
-        [[[CUTEAPIManager sharedInstance] POST:CONCAT(@"/api/1/rent_ticket/", self.form.ticket.identifier) parameters:nil resultClass:[CUTETicket class]] continueWithBlock:^id(BFTask *task) {
-            if (task.error) {
-                [SVProgressHUD showErrorWithError:task.error];
-            }
-            else if (task.exception) {
-                [SVProgressHUD showErrorWithException:task.exception];
-            }
-            else if (task.isCancelled) {
-                [SVProgressHUD showErrorWithCancellation];
-            }
-            else {
-                if (task.result) {
-                    [SVProgressHUD dismiss];
-                    [[NSNotificationCenter defaultCenter] postNotificationName:KNOTIF_TICKET_EDIT object:self userInfo:@{@"ticket": task.result}];
-                }
-                else {
-                    [SVProgressHUD showErrorWithStatus:STR(@"获取失败")];
-                }
-            }
-
-            return task;
-        }];
-    }
-    else if ([field.key isEqualToString:@"copyLink"]) {
-        [UIPasteboard generalPasteboard].string = [[NSURL URLWithString:CONCAT(@"/wechat-poster/", self.form.ticket.identifier) relativeToURL:[CUTEConfiguration hostURL]] absoluteString];
-        [SVProgressHUD showSuccessWithStatus:STR(@"已复制至粘贴版")];
-    }
-    else if ([field.key isEqualToString:@"qrcode"]) {
-        [tableView deselectRowAtIndexPath:indexPath animated:NO];
-    }
-    else if ([field.key isEqualToString:@"wechat"]) {
-        [tableView deselectRowAtIndexPath:indexPath animated:YES];
-        [self shareRentTicket];
-    }
+- (void)clearBackButton {
 
 }
 
-- (void)onDoneButtonPressed:(id)sender {
-    [self dismissViewControllerAnimated:YES completion:nil];
+- (void)updateRightButtonWithURL:(NSURL *)url {
+
 }
 
 
