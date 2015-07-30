93dde3078be0acbeb8960f7490917af536568b7b
levy.li <levy.li@bbtechgroup.com>
2015-07-30 16:40:12 +0800

    [Web]Mod: Issue #5993 发布出租房产时房间数最少为1

--- source/web/src/static/scripts/property_to_rent_create.js
+++ source/web/src/static/scripts/property_to_rent_create.js
@@ -456,9 +456,15 @@
 
         $errorMsg.text('').hide()
         function highlightErrorElem(elem){
+            if (elem.is('select') && elem.next('.chosen-container').length) {
+                elem = elem.add(elem.next('.chosen-container'))
+            }
             elem.css('border', '2px solid #f00').on('focus', function(){
-                $(this).css('border', '')
+                elem.css('border', '')
             })
+                .on('chosen:showing_dropdown', function(){
+                    elem.css('border', '')
+                })
         }
         function removeHighlightElem(elem){
             elem.css('border', '')
@@ -520,6 +526,14 @@
                 }
             })
         })
+        if (parseInt($('#bedroom_count').val()) < 1) {
+            validate = false
+            errorArr.push({
+                elem: $('#bedroom_count'),
+                msg: i18n('房间数最少为1')
+            })
+            highlightErrorElem($('#bedroom_count'))
+        }
         function checkContaction (elem) {
             var includePhoneReg = /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?/
             var includeEmailReg = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
