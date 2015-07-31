249a0bafc78c403d47f37e2dc28eee80359791be
levy.li <levy.li@bbtechgroup.com>
2015-07-31 15:50:20 +0800

    [Web]Mod:Issue #7201 在mobile app中如果js bridge的js没载出来的时候，禁止需要bridge的按钮点击(wait for test)

--- source/web/src/static/scripts/wvjsBridge.js
+++ source/web/src/static/scripts/wvjsBridge.js
@@ -1,3 +1,15 @@
+/** #7201 在mobile app中如果js bridge的js没载出来的时候，点击有js bridge的按钮会造成问题
+ *  需要在所有有bridge的按钮上添加 data-need-bridge 属性
+**/
+document.body.addEventListener('click', function (event) {
+    if (window.team.isCurrantClient() && !window.bridge && event.target.hasAttribute('data-need-bridge')) {
+        event.preventDefault()
+        event.stopPropagation()
+        window.console.log('Forbidden click before window.bridge is available.')
+        return false
+    }
+}, true)
+
 function connectWebViewJavascriptBridge(callback) {
     if (window.WebViewJavascriptBridge) {
         callback(window.WebViewJavascriptBridge)
