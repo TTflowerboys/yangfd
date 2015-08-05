28b22b91ab6d165a43c8c3b420f6347a56c75b8c
Foster Yin <fyin@bbtechgroup.com>
2015-08-05 18:48:11 +0800

    [ios] Add: Fix shell escape space bug http://unix.stackexchange.com/questions/108635/why-i-cant-escape-spaces-on-a-bash-script

--- source/ios/upload_fir.sh
+++ source/ios/upload_fir.sh
@@ -7,18 +7,13 @@
 #
 
 if which fir >/dev/null; then
-	echo "Start :)"
+    echo "Start :)"
 else
     echo "Please install fir-cli sudo gem install fir-cli --no-ri --no-rdoc"
 fi
 
 IPA=$1
- 
-if [ -z "$IPA" ]
-then
-	echo "Syntax: upload_fir.sh {my-application.ipa}"
-	exit 1
-fi
+echo $IPA
  
 USER_TOKEN="02706885a88f4c6e361e4c90d5f44380"
-fir publish $IPA -T $USER_TOKEN
+fir publish "$IPA" -T $USER_TOKEN
