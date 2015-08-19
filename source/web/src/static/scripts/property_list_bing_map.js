b85fec0e19353d424667a942e600030b0246f4b1
Foster Yin <fyin@bbtechgroup.com>
2015-08-19 15:51:41 +0800

    [web] Add: Make property bing map work as a module

---
+++ source/web/src/static/scripts/property_list_bing_map.js
@@ -0,0 +1,200 @@
+(function (module) {
+        /*
+    * Map View
+    * */
+    var bingMapKey = 'AhibVPHzPshn8-vEIdCx0so7vCuuLPSMK7qLP3gej-HyzvYv4GJWbc4_FmRvbh43'
+
+    window.mapCache = {}
+    window.mapPinCache = {}
+    window.mapInfoBoxLayerCache = {}
+    window.loadItemDetail = null
+
+    window.getMap = function (mapId) {
+        if (!window.mapCache[mapId]) {
+            var map = new Microsoft.Maps.Map(document.getElementById(mapId), {credentials: bingMapKey});
+            window.mapCache[mapId] = map
+
+        }
+        return window.mapCache[mapId]
+    }
+
+    function createMapPin(map, layer, mapId, result) {
+        if (result && result.latitude && result.longitude) {
+            var location = new Microsoft.Maps.Location(result.latitude, result.longitude);
+            var pin = new Microsoft.Maps.Pushpin(location, {icon: '/static/images/property_details/icon-location-building.png', width: 30, height: 45});
+
+            layer.push(pin)
+            Microsoft.Maps.Events.addHandler(pin, 'click', function () { showInfoBox(map, mapId, result) });
+            if  (!window.mapPinCache[mapId]) {
+                window.mapPinCache[mapId] = []
+            }
+            window.mapPinCache[mapId].push(pin)
+        }
+    }
+
+
+    function showInfoBox(map, mapId, result) {
+        if (window.mapInfoBoxLayerCache[mapId]) {
+            map.entities.remove(window.mapInfoBoxLayerCache[mapId]);
+        }
+        var location = new Microsoft.Maps.Location(result.latitude, result.longitude);
+        var layer = new Microsoft.Maps.EntityCollection()
+        var infoboxOptions = null
+        if (window.team.isPhone()) {
+            infoboxOptions = {offset:new Microsoft.Maps.Point(-90,50) };
+        }
+        else {
+            infoboxOptions = {offset:new Microsoft.Maps.Point(-160,50) };
+        }
+        var infobox = new Microsoft.Maps.Infobox(location, infoboxOptions);
+        window.loadItemDetail(result, function (html) {
+            infobox.setHtmlContent(html)
+            layer.push(infobox)
+            layer.setOptions({ visible: true });
+            map.entities.push(layer);
+            ajustMapPosition(map, layer.get(0), location)
+            window.mapInfoBoxLayerCache[mapId] = layer
+        })
+    }
+
+    //http://stackoverflow.com/questions/11148042/bing-maps-invoke-click-event-on-pushpin
+    function ajustMapPosition(map, infobox, location) {
+
+        var buffer = 70;
+        var infoboxOffset = infobox.getOffset();
+        var infoboxAnchor = infobox.getAnchor();
+        var infoboxLocation = map.tryLocationToPixel(location, Microsoft.Maps.PixelReference.control);
+        var dx = infoboxLocation.x + infoboxOffset.x - infoboxAnchor.x;
+        var dy = infoboxLocation.y - 25 - infoboxAnchor.y;
+
+        if (dy < buffer) { //Infobox overlaps with top of map.
+            //#### Offset in opposite direction.
+            dy *= -1;
+            //#### add buffer from the top edge of the map.
+            dy += buffer;
+        } else {
+            //#### If dy is greater than zero than it does not overlap.
+
+            dy = map.getHeight() - infoboxLocation.y + infoboxAnchor.y;
+            if (dy > buffer) {
+                dy = 0;
+            } else {
+                dy -= buffer;
+            }
+        }
+
+        if (dx < buffer) { //Check to see if overlapping with left side of map.
+            //#### Offset in opposite direction.
+            dx *= -1;
+            //#### add a buffer from the left edge of the map.
+            dx += buffer;
+        } else { //Check to see if overlapping with right side of map.
+            dx = map.getWidth() - infoboxLocation.x + infoboxAnchor.x - infobox.getWidth() / 2;
+            //#### If dx is greater than zero then it does not overlap.
+            if (dx > buffer) {
+                dx = 0;
+            } else {
+                //#### add a buffer from the right edge of the map.
+                dx -= buffer;
+            }
+        }
+
+        //#### Adjust the map so infobox is in view
+        if (dx !== 0 || dy !== 0) {
+            map.setView({
+                centerOffset: new Microsoft.Maps.Point(dx, dy),
+                center: map.getCenter()
+            });
+        }
+    }
+
+    function updateMapResults(map, mapId, searchResults) {
+        var layer = new Microsoft.Maps.EntityCollection()
+        for (var i = 0; i < searchResults.length; i++) {
+            createMapPin(map, layer, mapId, searchResults[i]);
+        }
+        map.entities.push(layer)
+    }
+
+    function getBestMapOptions(locations, mapWidth, mapHeight) {
+        var center = new Microsoft.Maps.Location();
+        var zoomLevel = 0;
+
+        var maxLat = -85;
+        var minLat = 85;
+        var maxLon = -180;
+        var minLon = 180;
+
+        //calculate bounding rectangle
+        for (var i = 0; i < locations.length; i++)
+        {
+            if (locations[i].latitude > maxLat)
+            {
+                maxLat = locations[i].latitude;
+            }
+
+            if (locations[i].latitude < minLat)
+            {
+                minLat = locations[i].latitude;
+            }
+
+            if (locations[i].longitude > maxLon)
+            {
+                maxLon = locations[i].longitude;
+            }
+
+            if (locations[i].longitude < minLon)
+            {
+                minLon = locations[i].longitude;
+            }
+        }
+
+        center.latitude = (maxLat + minLat) / 2;
+        center.longitude = (maxLon + minLon) / 2;
+
+        var zoom1=0, zoom2=0;
+
+        //Determine the best zoom level based on the map scale and bounding coordinate information
+        if (maxLon !== minLon && maxLat !== minLat)
+        {
+            //best zoom level based on map width
+            zoom1 = Math.log(360.0 / 256.0 * mapWidth / (maxLon - minLon)) / Math.log(2);
+            //best zoom level based on map height
+            zoom2 = Math.log(180.0 / 256.0 * mapHeight / (maxLat - minLat)) / Math.log(2);
+        }
+
+        //use the most zoomed out of the two zoom levels
+        zoomLevel = Math.round((zoom1 < zoom2) ? zoom1 : zoom2);
+        if (zoomLevel > 0) {
+            zoomLevel = zoomLevel - 1; //left more around margin
+        }else {
+            zoomLevel = 13 //Default zoom level is 10
+        }
+        return {zoom:zoomLevel , center:center}
+
+    }
+
+    module.loadMapPins = function (array, itemFunc) {
+        window.loadItemDetail = itemFunc
+        var mapId = 'mapCanvas'
+        var map = window.getMap(mapId)
+        map.entities.clear();
+        updateMapResults(map, mapId, array)
+
+        var locations = []
+        _.each(array, function (property) {
+            if(property.latitude && property.longitude) {
+                var location = new Microsoft.Maps.Location(property.latitude, property.longitude)
+                locations.push(location)
+            }
+        })
+        map.setView(getBestMapOptions(locations, $('#' + mapId).width(), $('#' + mapId).height()))
+        $('html, body').animate({scrollTop: $('#' + mapId).offset().top - 60 }, 'fast')
+    }
+
+    module.clearMapPins = function () {
+        var mapId = 'mapCanvas'
+        var map = window.getMap(mapId)
+        map.entities.clear();
+    }
+})(window.currantModule = window.currantModule || {})
--- source/web/src/static/scripts/property_to_rent_list.js
+++ source/web/src/static/scripts/property_to_rent_list.js
@@ -682,228 +682,15 @@
         }, 20)
     })
 
-
-    /*
-    * Map View
-    * */
-    var bingMapKey = 'AhibVPHzPshn8-vEIdCx0so7vCuuLPSMK7qLP3gej-HyzvYv4GJWbc4_FmRvbh43'
-
-    window.mapCache = {}
-    window.mapPinCache = {}
-    window.mapInfoBoxLayerCache = {}
-
-    window.getMap = function (mapId) {
-        if (!window.mapCache[mapId]) {
-           var map = new Microsoft.Maps.Map(document.getElementById(mapId), {credentials: bingMapKey});
-            window.mapCache[mapId] = map
-        }
-        return window.mapCache[mapId]
-    }
-
-    function createMapPin(map, layer, mapId, result) {
-        if (result && result.latitude && result.longitude) {
-            var location = new Microsoft.Maps.Location(result.latitude, result.longitude);
-            var pin = new Microsoft.Maps.Pushpin(location, {icon: '/static/images/property_details/icon-location-building.png', width: 30, height: 45});
-
-            layer.push(pin)
-            Microsoft.Maps.Events.addHandler(pin, 'click', function () { showInfoBox(map, mapId, result) });
-            if  (!window.mapPinCache[mapId]) {
-                window.mapPinCache[mapId] = []
-            }
-            window.mapPinCache[mapId].push(pin)
-        }
-    }
-
-
-    function showInfoBox(map, mapId, result) {
-        if (window.mapInfoBoxLayerCache[mapId]) {
-            map.entities.remove(window.mapInfoBoxLayerCache[mapId]);
-        }
-        var location = new Microsoft.Maps.Location(result.latitude, result.longitude);
-        var layer = new Microsoft.Maps.EntityCollection()
-        var infoboxOptions = null
-        if (window.team.isPhone()) {
-            infoboxOptions = {offset:new Microsoft.Maps.Point(-90,50) };
-        }
-        else {
-            infoboxOptions = {offset:new Microsoft.Maps.Point(-160,50) };
-        }
-        var infobox = new Microsoft.Maps.Infobox(location, infoboxOptions);
-        $.betterPost('/api/1/rent_ticket/'+result.id)
-            .done(function (val) {
-                if (!_.isEmpty(val)) {
-
-                    var houseResult = _.template($('#houseInfobox_template').html())({rent: val})
-                    infobox.setHtmlContent(houseResult)
-
-                    layer.push(infobox)
-                    layer.setOptions({ visible: true });
-                    map.entities.push(layer);
-                    ajustMapPosition(map, layer.get(0), location)
-                    window.mapInfoBoxLayerCache[mapId] = layer
-                }
-            }).fail(function () {
-
-            }).always(function () {
-
-            })
-    }
-
-    //http://stackoverflow.com/questions/11148042/bing-maps-invoke-click-event-on-pushpin
-    function ajustMapPosition(map, infobox, location) {
-
-        var buffer = 70;
-        var infoboxOffset = infobox.getOffset();
-        var infoboxAnchor = infobox.getAnchor();
-        var infoboxLocation = map.tryLocationToPixel(location, Microsoft.Maps.PixelReference.control);
-        var dx = infoboxLocation.x + infoboxOffset.x - infoboxAnchor.x;
-        var dy = infoboxLocation.y - 25 - infoboxAnchor.y;
-
-        if (dy < buffer) { //Infobox overlaps with top of map.
-            //#### Offset in opposite direction.
-            dy *= -1;
-            //#### add buffer from the top edge of the map.
-            dy += buffer;
-        } else {
-            //#### If dy is greater than zero than it does not overlap.
-
-            dy = map.getHeight() - infoboxLocation.y + infoboxAnchor.y;
-            if (dy > buffer) {
-                dy = 0;
-            } else {
-                dy -= buffer;
-            }
-        }
-
-        if (dx < buffer) { //Check to see if overlapping with left side of map.
-            //#### Offset in opposite direction.
-            dx *= -1;
-            //#### add a buffer from the left edge of the map.
-            dx += buffer;
-        } else { //Check to see if overlapping with right side of map.
-            dx = map.getWidth() - infoboxLocation.x + infoboxAnchor.x - infobox.getWidth() / 2;
-            //#### If dx is greater than zero then it does not overlap.
-            if (dx > buffer) {
-                dx = 0;
-            } else {
-                //#### add a buffer from the right edge of the map.
-                dx -= buffer;
-            }
-        }
-
-        //#### Adjust the map so infobox is in view
-        if (dx !== 0 || dy !== 0) {
-            map.setView({
-                centerOffset: new Microsoft.Maps.Point(dx, dy),
-                center: map.getCenter()
-            });
-        }
-    }
-
-    function updateMapResults(map, mapId, searchResults) {
-        var layer = new Microsoft.Maps.EntityCollection()
-        for (var i = 0; i < searchResults.length; i++) {
-            createMapPin(map, layer, mapId, searchResults[i]);
-        }
-        map.entities.push(layer)
-    }
-
-    function getBestMapOptions(locations, mapWidth, mapHeight) {
-        var center = new Microsoft.Maps.Location();
-        var zoomLevel = 0;
-
-        var maxLat = -85;
-        var minLat = 85;
-        var maxLon = -180;
-        var minLon = 180;
-
-        //calculate bounding rectangle
-        for (var i = 0; i < locations.length; i++)
-        {
-            if (locations[i].latitude > maxLat)
-            {
-                maxLat = locations[i].latitude;
-            }
-
-            if (locations[i].latitude < minLat)
-            {
-                minLat = locations[i].latitude;
-            }
-
-            if (locations[i].longitude > maxLon)
-            {
-                maxLon = locations[i].longitude;
-            }
-
-            if (locations[i].longitude < minLon)
-            {
-                minLon = locations[i].longitude;
-            }
-        }
-
-        center.latitude = (maxLat + minLat) / 2;
-        center.longitude = (maxLon + minLon) / 2;
-
-        var zoom1=0, zoom2=0;
-
-        //Determine the best zoom level based on the map scale and bounding coordinate information
-        if (maxLon !== minLon && maxLat !== minLat)
-        {
-            //best zoom level based on map width
-            zoom1 = Math.log(360.0 / 256.0 * mapWidth / (maxLon - minLon)) / Math.log(2);
-            //best zoom level based on map height
-            zoom2 = Math.log(180.0 / 256.0 * mapHeight / (maxLat - minLat)) / Math.log(2);
-        }
-
-        //use the most zoomed out of the two zoom levels
-        zoomLevel = Math.round((zoom1 < zoom2) ? zoom1 : zoom2);
-        if (zoomLevel > 0) {
-            zoomLevel = zoomLevel - 1; //left more around margin
-        }else {
-            zoomLevel = 13 //Default zoom level is 10
-        }
-        return {zoom:zoomLevel , center:center}
-
-    }
-
-    function updateMap() {
-        var mapId = 'mapCanvas'
-        var map = window.getMap(mapId)
-        map.entities.clear();
-        updateMapResults(map, mapId, window.rentMapList)
-
-        var locations = []
-        _.each(window.rentMapList, function (rent) {
-            if(rent.latitude && rent.longitude){
-                var location = new Microsoft.Maps.Location(rent.latitude, rent.longitude)
-                locations.push(location)
-            }
-        })
-        map.setView(getBestMapOptions(locations, $('#' + mapId).width(), $('#' + mapId).height()))
-        $('html, body').animate({scrollTop: $('#' + mapId).offset().top - 60 }, 'fast')
-    }
-
-    function emptyMapPins() {
-        window.rentMapList = []
-
-        var mapId = 'mapCanvas'
-        var map = window.getMap(mapId)
-        map.entities.clear();
-    }
-
-
-
     $('[data-tabs]').tabs({trigger: 'click'}).on('openTab', function (event, target, tabName) {
         viewMode = tabName
         loadRentListByView()
     })
 
 
-
     function openTabContent(tabName) {
         var $tabContainer = $('[data-tabs]')
         var $tabContents = null
-
 
         $tabContents = $tabContainer.find('[data-tab-name=' + tabName + ']')
         $tabContents.addClass('selectedTab').show()
@@ -983,15 +770,27 @@
         }
         params = _.extend(params, filterOfNeighborhoodSubwaySchool.getParam())
         //Empty map list
-        emptyMapPins()
+
+        window.currantModule.clearMapPins()
 
         $.betterPost('/api/1/rent_ticket/search', params)
             .done(function (val) {
                 var array = val
                 //TODO: All rents must have location, filter those have no location
                 if (!_.isEmpty(array)) {
-                    window.rentMapList = array
-                    updateMap()
+                    window.currantModule.loadMapPins(array, function (result, callback) {
+                        $.betterPost('/api/1/rent_ticket/'+result.id)
+                            .done(function (val) {
+                                if (!_.isEmpty(val)) {
+                                    var houseResult = _.template($('#houseInfobox_template').html())({rent: val})
+                                    callback(houseResult)
+                                }
+                            }).fail(function () {
+
+                            }).always(function () {
+
+                            })
+                    })
                 }else{
                     window.alert(window.i18n('暂无结果'))
                 }
