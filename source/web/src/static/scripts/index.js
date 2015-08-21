7ecb48a37e45e96d5efbe2d98f8759ff521f3986
Foster Yin <fyin@bbtechgroup.com>
2015-08-21 17:56:58 +0800

    [ios] Add: Bug #7373 Fix tabs hover event mis-dispatch bug

--- source/web/src/static/scripts/index.js
+++ source/web/src/static/scripts/index.js
@@ -27,30 +27,36 @@
     initBanner()
 
     $('#roleChooser').tabs({trigger: 'click'}).on('openTab', function (event, target, tabName) {
-        window.scrollTo(0, $('#roleChooser').offset().top)
-        var windowHeight = $(window).height()
-        var tabbarHeight = $('#roleChooser .tab_wrapper').height()
+        if ($(target).parents('[data-tabs]').first()[0] == $('#roleChooser')[0]) {
+            window.scrollTo(0, $('#roleChooser').offset().top)
+            var windowHeight = $(window).height()
+            var tabbarHeight = $('#roleChooser .tab_wrapper').height()
 
-         if (tabName === 'landlord') {
-            window.console.log('landlord')
-            if (window.team.isCurrantClient()) {
-                var publishHeight = $('.publishInClient').height()
-                $('.publishInClient').css('margin-top', ((windowHeight - tabbarHeight - publishHeight) / 2) + 'px')
+            if (tabName === 'landlord') {
+                window.console.log('landlord')
+                if (window.team.isCurrantClient()) {
+                    var publishHeight = $('.publishInClient').height()
+                    $('.publishInClient').css('margin-top', ((windowHeight - tabbarHeight - publishHeight) / 2) + 'px')
+                }
+                else  {
+                    if (typeof window.indexAppDownloadSwiper === 'undefined') {
+                        window.setupDownload(window.Swiper)
+                    }
+                }
             }
-            else  {
-                if (typeof window.indexAppDownloadSwiper === 'undefined') {
-                    window.setupDownload(window.Swiper)
+            else if (tabName === 'renter'){
+                if (window.team.isCurrantClient()) {
+                    var questionHeight = $('.renterService ul.questionChooser').height()
+                    $('ul.questionChooser').css('margin-top', ((windowHeight - tabbarHeight - questionHeight)/ 2) + 'px')
+                    $('.renterService').css('border-bottom', '0px');
                 }
             }
         }
-        else if (tabName === 'renter'){
-            if (window.team.isCurrantClient()) {
-                var questionHeight = $('.renterService ul.questionChooser').height()
-                $('ul.questionChooser').css('margin-top', ((windowHeight - tabbarHeight - questionHeight)/ 2) + 'px')
-                $('.renterService').css('border-bottom', '0px');
-            }
-        }
     });
+
+    $('.intentionChooser').tabs({trigger:'hover'}).on('openTab', function () {
+
+    })
 
     $('#announcement').on('click', 'ul>li>.close', function (event) {
         var $item = $(event.target.parentNode)
