a38a5a80cdbf9406cf51f026be0b2a770984ca47
Foster Yin <fyin@bbtechgroup.com>
2015-08-27 17:09:12 +0800

    [web] Add: Task #7290 Fix suggestion tip show bug, put index intention code into module

--- source/web/src/static/scripts/index.js
+++ source/web/src/static/scripts/index.js
@@ -38,7 +38,7 @@
 
             })
             if (window.user) {
-                setupUserPropertyChooser()
+                window.currantModule.setupUserPropertyChooser(loadPropertyList)
             }
             else {
                 $('.intentionChooser').tabs({trigger: 'hover'})
@@ -115,55 +115,7 @@
         }
     })
 
-    function getSelectedBudgetTypeId() {
-        var $selectedChild = $('#tags #budgetTag').children('.selected')
-        if ($selectedChild.length) {
-            return $selectedChild.first().attr('data-id')
-        }
-        return ''
-    }
 
-    // function getSelectedBudgetTypeValue() {
-    //     var $selectedChild = $('#tags #budgetTag').children('.selected')
-    //     if ($selectedChild.length) {
-    //         return $selectedChild.first().text()
-    //     }
-    //     return ''
-    // }
-
-    function getSelectedIntentionIds() {
-        var $selectedChildren = $('#tags #intentionTag ul').children('.selected')
-        if ($selectedChildren.length) {
-            var ids = ''
-            _.each($selectedChildren, function (child) {
-                ids += child.getAttribute('data-id')
-                ids += ','
-            })
-
-            if (_.last(ids) === ',') {
-                ids = ids.substring(0, ids.length - 1)
-            }
-            return ids
-        }
-        return ''
-    }
-
-    // function getSelectedIntentionValues() {
-    //     var $selectedChildren = $('#tags #intentionTag ul').children('.selected')
-    //     if ($selectedChildren.length) {
-    //         var ids = ''
-    //         _.each($selectedChildren, function (child) {
-    //             ids += $(child).clone().children().remove().end().text()
-    //             ids += ','
-    //         })
-
-    //         if (_.last(ids) === ',') {
-    //             ids = ids.substring(0, ids.length - 1)
-    //         }
-    //         return ids
-    //     }
-    //     return ''
-    // }
 
     function getAllIntentionIds() {
         var rawIntentionList = $('#dataIntentionList').text()
@@ -351,6 +303,7 @@
     }
 
 
+
     function loadPropertyListWithBudgetAndIntention(budgetType, intention) {
 
         $('#suggestionHouses #loadIndicator').show()
@@ -359,10 +312,8 @@
         var responseArray = []
 
         var usedIntention = []
-        var needShowSuggetionTip = false
         if (_.isEmpty(intention)) {
             usedIntention = commaStringToArray(getAllIntentionIds())
-            needShowSuggetionTip = true
         }
         else {
             usedIntention = intention
@@ -376,12 +327,6 @@
             usedBudget = budgetType
         }
 
-        if (needShowSuggetionTip) {
-            $('.tags_wrapper #intentionTag #suggestionTip').show()
-        }
-        else {
-            $('.tags_wrapper #intentionTag #suggestionTip').hide()
-        }
 
         _.each(usedIntention, function (oneIntention) {
             var params = {'random': true, 'intention': oneIntention}
@@ -477,123 +422,6 @@
         window.project.currentIntentionIds = intention
 
         updateUserTags(window.project.currentBudgetId, window.project.currentIntentionIds)
-    }
-
-    function addIntetionTag(id, value) {
-        var $intentionTag = $('#tags #intentionTag')
-        $intentionTag.find('#list').append('<li class="toggleTag selected" data-id="' + id + '">' +
-                                           value +
-                                           '<img alt="" src="/static/images/intention/close.png"/></li>'
-                                          )
-    }
-
-    function removeIntentionTag(id) {
-        var $intentionTag = $('#tags #intentionTag')
-        $intentionTag.find('#list li[data-id=' + id + ']').remove()
-    }
-
-    function getIntentionTabsHeight() {
-        if (window.team.isPhone()) {
-            return 80 + Math.ceil($('.intention .controls').children().length / 3.0) * 140  + 18 + 15
-        }
-        else {
-            return 530
-        }
-    }
-
-    function setupUserPropertyChooser() {
-        $('.intentionChooser').tabs({trigger: 'hover'})
-        var $intentionDetails = $('.intentionChooser')
-
-        var $budgetTag = $('#tags #budgetTag')
-        var $intentionTag = $('#tags #intentionTag')
-
-        var initBudgetId = ''
-        var initIntentionList = ''
-        if (window.user.budget) {
-            $budgetTag.find('.toggleTag[data-id=' + window.user.budget.id + ']').addClass('selected')
-            initBudgetId = window.user.budget.id
-        }
-
-        if (window.user.intention) {
-            _.each(window.user.intention, function (item) {
-                if (item.value && item.slug) {
-                    addIntetionTag(item.id, item.value)
-                    $intentionDetails.find('li[data-id=' + item.id + ']').addClass('selected')
-                    $intentionDetails.find('input[value=' + item.id + ']').prop('checked', true)
-                    initIntentionList = initIntentionList + item.id + ','
-                }
-            })
-
-            if (_.last(initIntentionList) === ',') {
-                initIntentionList = initIntentionList.substring(0, initIntentionList.length - 1)
-            }
-        }
-
-
-        loadPropertyList(initBudgetId, initIntentionList)
-
-        //if user don't choose one intention, show the tabs
-        if (!commaStringToArray(initIntentionList).length) {
-            $('.intentionTabs_wrapper').animate({height: getIntentionTabsHeight() + 'px'}, 400, 'swing')
-        }
-
-        $budgetTag.on('click', '.toggleTag', function (event) {
-
-            var $item = $(event.target)
-            var alreadySelected = $item.hasClass('selected')
-            var $parent = $(event.target.parentNode)
-            $parent.find('.toggleTag').removeClass('selected')
-
-            if (!alreadySelected) {
-                $item.addClass('selected')
-            }
-
-            loadPropertyList(getSelectedBudgetTypeId(), getSelectedIntentionIds())
-
-            ga('send', 'event', 'index', 'change', 'change-budget',$item.text())
-        })
-
-        $intentionTag.on('click', '.toggleTag img', function (event) {
-
-            var $li = $(event.target).parent()
-            var id = $li.attr('data-id')
-            $intentionDetails.find('li[data-id=' + id + ']').removeClass('selected')
-            $intentionDetails.find('input[value=' + id + ']').prop('checked', false)
-            $li.remove()
-            loadPropertyList(getSelectedBudgetTypeId(), getSelectedIntentionIds())
-
-            ga('send', 'event', 'index', 'change', 'remove-intention-on-tag',$li.text())
-        })
-
-        $intentionDetails.find('[name=intention]').on('change', function (event) {
-            var $li = $(this).closest('li')
-
-            $li.toggleClass('selected', this.checked)
-
-            if (this.checked) {
-                addIntetionTag($li.attr('data-id'), $li.attr('data-value'))
-                ga('send', 'event', 'index', 'change', 'add-intention',$li.text())
-            }
-            else {
-                removeIntentionTag($li.attr('data-id'))
-                ga('send', 'event', 'index', 'change', 'remove-intention-on-icon',$li.text())
-            }
-            loadPropertyList(getSelectedBudgetTypeId(), getSelectedIntentionIds())
-        })
-
-        $intentionTag.find('#add').click(function () {
-            $('html, body').animate({scrollTop: $('.intentionTabs_wrapper').offset().top - 60 }, 'fast')
-            $('.intentionTabs_wrapper').animate({height: getIntentionTabsHeight() + 'px'}, 400, 'swing')
-
-            ga('send', 'event', 'index', 'click', 'extend-intention-selection')
-        })
-
-        $('.intentionTabs_wrapper').find('#collapseButton').click(function () {
-            $('.intentionTabs_wrapper').animate({height: '0'}, 400, 'swing')
-
-            ga('send', 'event', 'index', 'click', 'collapse-intention-selection')
-        })
     }
 
     if (!window.user) {
