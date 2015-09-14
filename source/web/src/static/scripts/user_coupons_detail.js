dc9566149a7f8cf7edfd9783b019499cf9d10df8
levy.li <levy.li@bbtechgroup.com>
2015-09-14 14:59:39 +0800

    [Web]Mod: Issue #7427 自定义分享优惠券到朋友圈的文案

--- source/web/src/static/scripts/user_coupons_detail.js
+++ source/web/src/static/scripts/user_coupons_detail.js
@@ -5,7 +5,7 @@
     }
     $('.shareBtn').on('click', function () {
         if(window.bridge) {
-            window.bridge.callHandler('share', {'text': window.i18n('分享文案'), 'url': 'http://yangfd.com/app-download', 'services': ['Wechat Circle']}, function(response) {
+            window.bridge.callHandler('share', {'text': $(this).attr('data-shareText'), 'url': 'http://yangfd.com/app-download', 'services': ['Wechat Circle']}, function(response) {
                 if (response.msg === 'ok') {
                    return shareSuccessCallback()
                 }
