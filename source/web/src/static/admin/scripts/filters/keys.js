7033dee3e5474e05711fbbffc4a40ddd194030b6
levy.li <levy.li@bbtechgroup.com>
2015-09-14 11:23:34 +0800

    [Web]Mod: Issue #7459 filter中的错误(wait for test)

--- source/web/src/static/admin/scripts/filters/keys.js
+++ source/web/src/static/admin/scripts/filters/keys.js
@@ -3,6 +3,6 @@ angular.module('app')
         return function (list, key) {
             return key ? _.map(list, function (obj) {
                 return obj[key]
-            }) : item
+            }) : list
         }
     })
