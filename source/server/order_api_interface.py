6c56349acf7e6d7c48d1466687f17b750b018049
Felix Yan <felixonmars@archlinux.org>
2015-09-16 16:13:04 +0800

    [server] Mod: specify ignore_nonexist too

--- source/server/order_api_interface.py
+++ source/server/order_api_interface.py
@@ -151,7 +151,7 @@ def order_search_view_rent_ticket_contact_info(user, params):
     orders = f_app.order.output(f_app.order.custom_search({
         "user.id": user_id,
         "items.id": f_app.common.view_rent_ticket_contact_info_id
-    }, per_page=per_page), permission_check=False)
+    }, per_page=per_page), permission_check=False, ignore_nonexist=True)
 
     for order in orders:
         order["ticket"] = f_app.ticket.output([order.pop("ticket_id")], fuzzy_user_info=True)[0]
