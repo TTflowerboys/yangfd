ddcfd154d9638e558755164405a469752080ea08
levy.li <levy.li@bbtechgroup.com>
2015-09-21 18:08:17 +0800

    [Web]Mod: Issue #7501 给网站增加全局的提示错误信息的控件

--- source/web/src/static/scripts/email_unsubscribe.js
+++ source/web/src/static/scripts/email_unsubscribe.js
@@ -37,6 +37,9 @@ $(function () {
                 ga('send', 'event', 'email-unsubscribe', 'click', 'update ' + $(event.currentTarget).attr('data-type') + ' successfully')
             }).fail(function (ret) {
                 //window.alert(window.i18n('更新失败'))
+                if(ret !== 0) {
+                    window.dhtmlx.message({ type:'error', text: window.getErrorMessageFromErrorCode(ret)});
+                }
                 ga('send', 'event', 'email-unsubscribe', 'click', 'update ' + $(event.currentTarget).attr('data-type') + ' failed')
             })
         }
