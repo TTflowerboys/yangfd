71f40cc2be78a49d38a0786040c1c502df8f908d
levy.li <levy.li@bbtechgroup.com>
2015-10-02 17:27:29 +0800

    [Web]Mod: Issue #7403 ios_patch增加release字段

--- source/web/src/static/admin/scripts/controllers/appversion/item.js
+++ source/web/src/static/admin/scripts/controllers/appversion/item.js
@@ -1,6 +1,6 @@
 (function () {
 
-    function ctrlAppversionItems($scope, $rootScope, apiFactory) {
+    function ctrlAppversionItems($scope, $rootScope, appversionApi) {
 
         $scope.addChangeLog = function () {
             /*if (_.isEmpty($scope.item.changelog)) {
@@ -17,6 +17,25 @@
             $scope.item.changelog.splice(index, 1)
         }
 
+        function updateReleaseList (parentList) {
+            $scope.releaseList = _.map(_.filter(parentList, function (val) {
+                return val.platform === 'ios'
+            }), function (val) {
+                return val.version_name
+            })
+        }
+        if($scope.$parent.list && $scope.$parent.list.length) {
+            updateReleaseList($scope.$parent.list)
+        } else {
+            appversionApi.getAll({
+                params:{
+                    'platform': 'ios'
+                }
+            })
+                .success(function (data) {
+                    updateReleaseList(data.val)
+                })
+        }
 
     }
 
