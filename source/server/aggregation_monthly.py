2b801e73d3735c55d8d0e9508c6a3d54adc412a7
wangchao0721 <wangchao0721@gmail.com>
2015-10-09 17:24:30 +0800

    [ser]Mod: issue #7223 add more monthly analysis

--- source/server/aggregation_monthly.py
+++ source/server/aggregation_monthly.py
@@ -10,6 +10,9 @@ f_app.common.mongo_server = "172.20.101.98"
 
 with f_app.mongo() as m:
 
+    # 用户总数
+    total_user_count = m.users.count()
+
     # 本月用户总数
     selected_month = 10
     print(str(selected_month) + '月至今用户数据统计:')
@@ -127,3 +130,117 @@ with f_app.mongo() as m:
     for document in cursor:
         if(document['_id']):
             print(f_app.enum.get(document['_id']['_id'])['value']['zh_Hans_CN'].encode('utf-8') + ":" + str(document['count']))
+
+    # 按收藏类型统计
+    print('\n按收藏类型统计被收藏过的次数:')
+    cursor = m.favorites.aggregate(
+        [
+            {'$match': {
+                "time":
+                    {
+                        '$gte': datetime(2015, selected_month, 1, 0, 0, 0)
+                    }
+                }},
+            {'$group': {'_id': "$type", 'count': {'$sum': 1}}}
+        ]
+    )
+    fav_type_dic = {
+        'rent_ticket': '出租房源',
+        'property': '海外房产',
+        'item': '众筹'
+    }
+
+    for document in cursor:
+        if(document['_id']):
+            print(fav_type_dic[document['_id']] + ':' + str(document['count']))
+
+    # 按用户统计的收藏出租房的数量排名
+    print('\n按用户统计的收藏出租房的数量排名:')
+    cursor = m.favorites.aggregate(
+        [
+            {'$match': 
+                {
+                    'type': "rent_ticket",
+                    "time":
+                        {
+                            '$gte': datetime(2015, selected_month, 1, 0, 0, 0)
+                        }
+                }},
+            {'$group': {'_id': "$user_id", 'count': {'$sum': 1}}},
+            {'$sort': {'count': -1}},
+            {'$limit': 10}
+        ]
+    )
+
+    for document in cursor:
+        print(f_app.user.output([document['_id']], custom_fields=f_app.common.user_custom_fields)[0]['nickname'].encode('utf-8') + ':' + str(document['count']))
+
+    # 查看过联系方式的总用户数和比例和查看总次数
+    print('\n查看过联系方式的总用户数和查看总量:')
+    cursor = m.orders.aggregate(
+        [
+            {'$unwind': "$items"},
+            {'$match': {
+                "time":
+                    {
+                        '$gte': datetime(2015, selected_month, 1, 0, 0, 0)
+                    }
+                }},
+            {'$group': {'_id': "$user.nickname", 'count': {'$sum': 1}}},
+            {'$group': {'_id': None, 'totalUsersCount': {'$sum': 1}, 'totalRequestCount': {'$sum': "$count"}}}
+        ]
+    )
+
+    for document in cursor:
+        total_requested_contact_user_count = document['totalUsersCount']
+        print('查看过联系方式的总用户数量' + ':' + str(document['totalUsersCount']) + ', ' + '占总用户数的比例为' + format(document['totalUsersCount']*1.0/total_user_count, '.2%'))
+        print('所有查看过联系方式的总次数' + ':' + str(document['totalRequestCount']))
+
+        # 被查看过联系方式的房源数量和被查看总次数的统计
+    # TODO
+    print('\n被查看过联系方式的房源数量和被查看总次数的统计:')
+    cursor = m.orders.aggregate(
+        [
+            {'$unwind': "$items"},
+            {'$match': {
+                "time":
+                    {
+                        '$gte': datetime(2015, selected_month, 1, 0, 0, 0)
+                    }
+                }},
+            {'$group': {'_id': "$ticket_id", 'count': {'$sum': 1}}},
+            {'$group': {'_id': None, 'totalUsersCount': {'$sum': 1}, 'totalRequestCount': {'$sum': "$count"}}}
+        ]
+    )
+
+    for document in cursor:
+        print('被查看过联系方式的房源数量' + ':' + str(document['totalUsersCount']))
+        print('查看过联系方式的总数量' + ':' + str(document['totalRequestCount']))
+
+    # 房源被查看联系方式的个数
+    print('\n出租房源被查看联系方式的次数排名:')
+    cursor = m.orders.aggregate(
+        [
+            {'$unwind': "$items"},
+            {'$match': {
+                "time":
+                    {
+                        '$gte': datetime(2015, selected_month, 1, 0, 0, 0)
+                    }
+                }},
+            {'$group': {'_id': "$ticket_id", 'count': {'$sum': 1}}},
+            {'$sort': {'count': -1}},
+            {'$limit': 10}
+        ]
+    )
+
+    target_ticket_dic = {}
+    for document in cursor:
+        if(document['_id']):
+            target_ticket_dic[str(document['_id'])] = document['count']
+
+    target_ticket_dic = OrderedDict(sorted(target_ticket_dic.items(), key=lambda t: t[1], reverse=True))
+    target_tickets = f_app.i18n.process_i18n(f_app.ticket.output(target_ticket_dic.keys(), ignore_nonexist=True, permission_check=False))
+    for ticket in target_tickets:
+        if ticket is not None:
+            print(ticket['title'].encode('utf-8') + ", " + str(ticket['id']) + ": " + str(target_ticket_dic.get(ticket['id'])))
