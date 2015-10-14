c33ec483ee9e18fdac9c5b0c9909a14e5ec5cc26
Felix Yan <felixonmars@archlinux.org>
2015-10-14 13:59:14 +0800

    [server] Add: some examples of related data

--- source/server/csv_get_user_data.py
+++ source/server/csv_get_user_data.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
+from bson.objectid import ObjectId
 from app import f_app
-
 import csv
 
 header = ['用户', '邮箱', '性别', '国家', '城市', '用户类型', '职业', '房东类型', '注册时间', '有没有发房产',
@@ -44,6 +44,25 @@ def get_enum_data(u, s):
     return t.encode('utf-8')
 
 
+def get_related_data(u, s):
+    if s == "rent_ticket":
+        module = f_app.ticket
+        filters = {"type": "rent"}
+    elif s == "rent_intention_ticket":
+        module = f_app.ticket
+        filters = {"type": "rent_intention"}
+    elif s == "intention_ticket":
+        module = f_app.ticket
+        filters = {"type": "intention"}
+    elif s == "favorite_property":
+        module = f_app.user.favorite
+        filters = {"type": "property"}
+    else:
+        raise NotImplementedError
+
+    filters.update({"$or": [{"user_id": ObjectId(u["id"])}, {"creator_user_id": ObjectId(u["id"])}]})
+    return module.get(module.search(filters))
+
 with open('userData.csv', 'wb') as csvfile:
     spamwriter = csv.writer(csvfile,
                             delimiter=',',
@@ -51,6 +70,10 @@ with open('userData.csv', 'wb') as csvfile:
                             quoting=csv.QUOTE_MINIMAL)
     spamwriter.writerow(header)
     for user in f_app.user.get(f_app.user.get_active()):
+        rent_tickets = get_related_data(user, 'rent_ticket')
+        rent_intention_tickets = get_related_data(user, 'rent_intention_ticket')
+        intention_tickets = get_related_data(user, 'intention_ticket')
+        favorite_properties = get_related_data(user, 'favorite_property')
         spamwriter.writerow([
             get_data(user, 'nickname'),
             get_data(user, 'email'),
@@ -60,5 +83,14 @@ with open('userData.csv', 'wb') as csvfile:
             get_enum_data(user, 'user_type'),
             '',
             get_data(user, 'landlord_type'),
-            get_data(user, 'register_time')
+            get_data(user, 'register_time'),
+            "Y" if rent_tickets else "N",
+            len(rent_tickets),
+            '',  # TODO
+            '',  # TODO
+            '',  # TODO
+            '',  # TODO
+            "Y" if rent_intention_tickets else "N",
+            "Y" if intention_tickets else "N",
+            "Y" if favorite_properties else "N",
         ])
