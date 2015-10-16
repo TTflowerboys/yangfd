bab248709980cac1e8166cc088eff73e9b3f28e5
simon Artisan <endlessbytes@gmail.com>
2015-10-16 12:06:44 +0800

    some tweaks

--- source/server/csv_get_user_data.py
+++ source/server/csv_get_user_data.py
@@ -63,88 +63,87 @@ def get_related_data(u, s):
                             {"creator_user_id": ObjectId(u["id"])}]})
     return module.get(module.search(filters, per_page=-1))
 
-with open('userData.csv', 'wb') as csvfile:
-    wb = Workbook()
-    ws = wb.active
-    ws.append(header)
-    # land_enum = f_app.i18n.process_i18n(f_app.enum.get_all('landlord_type'))
-    # print f_app.util.json_dumps(land_enum,ensure_ascii = False)
-    rent_type_enum = f_app.i18n.process_i18n(f_app.enum.get_all('rent_type'))
-    landlord_type_enum = f_app.i18n.process_i18n(f_app.enum.get_all('landlord_type'))
-    landlord_type_enum_map = {enum["id"]: enum for enum in landlord_type_enum}
-    user_whole = f_app.user.get(f_app.user.get_active())
-    '''for user in user_whole:
-        pass'''
-    for user in user_whole:
-        rent_tickets = get_related_data(user, 'rent_ticket')
-        single_flag = 0
-        all_flag = 0
-        draft_flag = 0
-        rent_count = 0
-        single_all_info = ''
-        landlord_flag = [0, 0, 0, 0, 0]
-        landlord_info = []
-        for rt in rent_tickets:
-            #单个用户的每份出租信息
-            if rt.get('status') == "rent":
-                rent_count += 1
-            if rt.get('status') == "draft":
-                draft_flag = 1
-            for lte in landlord_type_enum:
-                if rt.get('landlord_type') is not None and rt.get('landlord_type').get('id') == lte.get('id'):
-                    if lte.get('slug') == "live_out_landlord":
-                        landlord_flag[0] = 1
-                    elif lte.get('slug') == "live_in_landlord":
-                        landlord_flag[1] = 1
-                    elif lte.get('slug') == "flatmate":
-                        landlord_flag[2] = 1
-                    elif lte.get('slug') == "agent":
-                        landlord_flag[3] = 1
-                    elif lte.get('slug') == "former_flatmate":
-                        landlord_flag[4] = 1
-            for rte in rent_type_enum:
-                if rt.get('rent_type').get('id') == rte.get('id'):
-                    if rte.get('slug') == "rent_type:single":
-                        single_flag = 1
-                    else:
-                        all_flag = 1
-        if landlord_flag[0] == 1:
-            landlord_info.append("我是房东，但不住在这")
-        if landlord_flag[1] == 1:
-            landlord_info.append("我是房东，也住在这里")
-        if landlord_flag[2] == 1:
-            landlord_info.append("我是目前租住的租客")
-        if landlord_flag[3] == 1:
-            landlord_info.append("我是经过房东授权的中介")
-        if landlord_flag[4] == 1:
-            landlord_info.append("我是准备搬出去的前租客")
-        landlord_info2 = "/".join(landlord_info)
-        if single_flag == 1 and all_flag == 1:
-            single_all_info = '单间/整套'
-        elif single_flag == 1:
-            single_all_info = '单间'
-        elif all_flag == 1:
-            single_all_info = '整套'
-        rent_intention_tickets = get_related_data(user, 'rent_intention_ticket')
-        intention_tickets = get_related_data(user, 'intention_ticket')
-        favorite_properties = get_related_data(user, 'favorite_property')
-        ws.append([get_data(user, 'nickname'),
-                   get_data(user, 'email'),
-                   '',
-                   get_data(user.get('country'), 'code'),
-                   '',
-                   get_enum_data(user, 'user_type'),
-                   '',
-                   landlord_info2,
-                   get_data(user, 'register_time'),
-                   "Y" if rent_tickets else "N",
-                   len(rent_tickets),
-                   single_all_info,
-                   rent_count,
-                   '',
-                   "Y" if draft_flag else "N",
-                   "Y" if rent_intention_tickets else "N",
-                   "Y" if intention_tickets else "N",
-                   "Y" if favorite_properties else "N",
-                   ])
-    wb.save("userData.xlsx")
+
+wb = Workbook()
+ws = wb.active
+ws.append(header)
+# land_enum = f_app.i18n.process_i18n(f_app.enum.get_all('landlord_type'))
+# print f_app.util.json_dumps(land_enum,ensure_ascii = False)
+rent_type_enum = f_app.i18n.process_i18n(f_app.enum.get_all('rent_type'))
+landlord_type_enum = f_app.i18n.process_i18n(f_app.enum.get_all('landlord_type'))
+landlord_type_enum_map = {enum["id"]: enum for enum in landlord_type_enum}
+user_whole = f_app.user.get(f_app.user.get_active())
+
+for user in user_whole:
+    rent_tickets = get_related_data(user, 'rent_ticket')
+    single_flag = 0
+    all_flag = 0
+    draft_flag = 0
+    rent_count = 0
+    single_all_info = ''
+    landlord_flag = [0, 0, 0, 0, 0]
+    landlord_info = []
+    for rt in rent_tickets:
+        #单个用户的每份出租信息
+        if rt.get('status') == "rent":
+            rent_count += 1
+        if rt.get('status') == "draft":
+            draft_flag = 1
+        for lte in landlord_type_enum:
+            if rt.get('landlord_type') is not None and rt.get('landlord_type').get('id') == lte.get('id'):
+                if lte.get('slug') == "live_out_landlord":
+                    landlord_flag[0] = 1
+                elif lte.get('slug') == "live_in_landlord":
+                    landlord_flag[1] = 1
+                elif lte.get('slug') == "flatmate":
+                    landlord_flag[2] = 1
+                elif lte.get('slug') == "agent":
+                    landlord_flag[3] = 1
+                elif lte.get('slug') == "former_flatmate":
+                    landlord_flag[4] = 1
+        for rte in rent_type_enum:
+            if rt.get('rent_type').get('id') == rte.get('id'):
+                if rte.get('slug') == "rent_type:single":
+                    single_flag = 1
+                else:
+                    all_flag = 1
+    if landlord_flag[0] == 1:
+        landlord_info.append("我是房东，但不住在这")
+    if landlord_flag[1] == 1:
+        landlord_info.append("我是房东，也住在这里")
+    if landlord_flag[2] == 1:
+        landlord_info.append("我是目前租住的租客")
+    if landlord_flag[3] == 1:
+        landlord_info.append("我是经过房东授权的中介")
+    if landlord_flag[4] == 1:
+        landlord_info.append("我是准备搬出去的前租客")
+    landlord_info2 = "/".join(landlord_info)
+    if single_flag == 1 and all_flag == 1:
+        single_all_info = '单间/整套'
+    elif single_flag == 1:
+        single_all_info = '单间'
+    elif all_flag == 1:
+        single_all_info = '整套'
+    rent_intention_tickets = get_related_data(user, 'rent_intention_ticket')
+    intention_tickets = get_related_data(user, 'intention_ticket')
+    favorite_properties = get_related_data(user, 'favorite_property')
+    ws.append([get_data(user, 'nickname'),
+               get_data(user, 'email'),
+               '',
+               get_data(user.get('country'), 'code'),
+               '',
+               get_enum_data(user, 'user_type'),
+               '',
+               landlord_info2,
+               get_data(user, 'register_time'),
+               "Y" if rent_tickets else "N",
+               len(rent_tickets),
+               single_all_info,
+               rent_count,
+               '',
+               "Y" if draft_flag else "N",
+               "Y" if rent_intention_tickets else "N",
+               "Y" if intention_tickets else "N",
+               "Y" if favorite_properties else "N",
+               ])
+wb.save("userData.xlsx")
