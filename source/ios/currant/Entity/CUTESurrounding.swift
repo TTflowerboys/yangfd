c5e66abd31d1b1f18aeeb9d7ae16bc1c7a617f4b
Foster Yin <fyin@bbtechgroup.com>
2015-10-29 15:53:22 +0800

    [ios] Add: Task #7558 Parse surrounding object key and id, name

--- source/ios/currant/Entity/CUTESurrounding.swift
+++ source/ios/currant/Entity/CUTESurrounding.swift
@@ -10,12 +10,33 @@ import UIKit
 
 class CUTESurrounding: MTLModel, MTLJSONSerializing {
 
-    var name:String?
     var type:CUTEEnum?
     var trafficTimes:[CUTETrafficTime]?
+    
+    //different type surrounding have different type key and identifier
+    var surroundingKey:String?
+    var surroundingName:String?
+    var surroundingIdentifier:String?
+
+    override init(dictionary dictionaryValue: [NSObject : AnyObject]!, error: ()) throws {
+        if let typeDic = dictionaryValue["type"] as? [String: AnyObject] {
+            self.surroundingKey = typeDic["slug"] as? String
+            if self.surroundingKey != nil {
+                if let surroundingValue = dictionaryValue[self.surroundingKey!] as? [String: AnyObject]{
+                    self.surroundingIdentifier = surroundingValue["id"] as? String
+                    self.surroundingName = surroundingValue["name"] as? String
+                }
+            }
+        }
+        try super.init(dictionary: dictionaryValue, error: error)
+    }
+
+    required init!(coder: NSCoder!) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     static func JSONKeyPathsByPropertyKey() -> [NSObject : AnyObject]! {
-        return ["name":"name", "type":"type", "trafficTimes":"traffic_time"]
+        return ["type":"type", "trafficTimes":"traffic_time"]
     }
 
     static func typeJSONTransformer() -> NSValueTransformer {
