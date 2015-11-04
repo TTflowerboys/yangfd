0fef0340d803be7dba00cc365b8c3645611fdf41
Felix Yan <felixonmars@archlinux.org>
2015-11-04 16:21:25 +0800

    [server] Mod: fix ticket_add_log_params plugin

--- source/server/f_currant.py
+++ source/server/f_currant.py
@@ -2069,7 +2069,7 @@ class f_currant_plugins(f_app.plugin_base):
         return kwargs
 
     def ticket_add_log_params(self, params, ticket_id, ticket, user):
-        if "type" in params:
+        if "type" in ticket:
             params["ticket_type"] = ticket["type"]
         return params
 
