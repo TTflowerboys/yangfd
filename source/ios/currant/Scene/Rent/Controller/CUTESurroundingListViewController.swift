c4b0beac9072deb6445bcfa35fb701e993087c2f
Foster Yin <fyin@bbtechgroup.com>
2015-11-09 18:56:50 +0800

    [ios] Add: Task #7558 Traffic time picker

--- source/ios/currant/Scene/Rent/Controller/CUTESurroundingListViewController.swift
+++ source/ios/currant/Scene/Rent/Controller/CUTESurroundingListViewController.swift
@@ -18,7 +18,6 @@ class CUTESurroundingListViewController: UITableViewController, UISearchBarDeleg
     internal var searchController:UISearchDisplayController?
 
 
-
     init(surroundings:[CUTESurrounding]) {
         self.surroundings = surroundings
         super.init(style: UITableViewStyle.Plain)
@@ -129,8 +128,10 @@ class CUTESurroundingListViewController: UITableViewController, UISearchBarDeleg
 
 
         if let trafficTime = surrounding.trafficTimes?[0] {
-            surroundingCell.typeButton.setTitle(trafficTime.type?.value, forState: UIControlState.Normal)
-            surroundingCell.durationButton.setTitle("\(trafficTime.time?.value) " + (trafficTime.time?.unitForDisplay)!, forState: UIControlState.Normal)
+            if trafficTime.time != nil {
+                surroundingCell.typeButton.setTitle(trafficTime.type!.value, forState: UIControlState.Normal)
+                surroundingCell.durationButton.setTitle("\(trafficTime.time!.value) " + (trafficTime.time!.unitForDisplay)!, forState: UIControlState.Normal)
+            }
             surroundingCell.setNeedsLayout()
         }
 
@@ -163,52 +164,6 @@ class CUTESurroundingListViewController: UITableViewController, UISearchBarDeleg
         }
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
 
     }
@@ -237,8 +192,7 @@ class CUTESurroundingListViewController: UITableViewController, UISearchBarDeleg
     }
 
     func onTypeButtonPressed(sender:UIButton) {
-        let index = sender.tag
-
+        self.showTrafficModePicker(sender);
     }
 
     func onDurationButtonPressed(sender:UIButton) {
@@ -254,8 +208,31 @@ class CUTESurroundingListViewController: UITableViewController, UISearchBarDeleg
         self.surroundings = surrs
         self.tableView.reloadData()
         self.delegate?.onDidRemoveSurrouding(surrounding, atIndex: index)
+    }
 
+    func showTrafficModePicker(sender:UIButton) {
+        let surr = self.surroundings[sender.tag]
+        if (surr.trafficTimes != nil) {
 
+            let modes = surr.trafficTimes!.map({ (time:CUTETrafficTime) -> String in
+                return time.type!.value + " \(time.time!.value) " + time.time!.unitForDisplay
+            })
+
+            var defaultTimeIndex = 0
+
+            for (index, time) in surr.trafficTimes!.enumerate() {
+                if time.isDefault {
+                    defaultTimeIndex = index
+                    break
+                }
+            }
+
+            ActionSheetStringPicker.showPickerWithTitle("", rows: modes, initialSelection: defaultTimeIndex, doneBlock: { (picker:ActionSheetStringPicker!, selecctedIndex:Int, selectedValue:AnyObject!) -> Void in
+                }, cancelBlock: { (picker:ActionSheetStringPicker!) -> Void in
+
+                }, origin: sender)
+
+        }
     }
 
 }
--- source/ios/currant/currant-Bridging-Header.h
+++ source/ios/currant/currant-Bridging-Header.h
@@ -24,6 +24,7 @@
 #import <RNDecryptor.h>
 #import <Sequencer.h>
 #import <INTULocationManager.h>
+#import <ActionSheetPicker.h>
 //#import <Masonry.h>
 
 
