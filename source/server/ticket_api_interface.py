014842b7332c9d4d7c0f736a1c52baaefc464267
Felix Yan <felixonmars@archlinux.org>
2015-11-14 16:18:48 +0800

    [server] Add: sort_time for sorting

--- source/server/ticket_api_interface.py
+++ source/server/ticket_api_interface.py
@@ -1144,6 +1144,7 @@ def rent_ticket_add(user, params):
 
     params.setdefault("type", "rent")
     params.setdefault("rent_available_time", datetime.utcnow())
+    params.setdefault("sort_time", datetime.utcnow())
 
     _find_or_register(params, allow_draft=True)
 
@@ -1159,7 +1160,7 @@ def rent_ticket_refresh(ticket_id, user):
     if ticket.get("creator_user_id") or ticket.get("user_id"):
         if not user or user["id"] not in (ticket.get("user_id"), ticket.get("creator_user_id")) and not (set(user["role"]) & set(["admin", "jr_admin", "support"])):
             abort(40399, logger.warning("Permission denied", exc_info=False))
-    result = f_app.ticket.update_set(ticket_id, {})
+    result = f_app.ticket.update_set(ticket_id, {"sort_time": datetime.utcnow()})
     log_params = f_app.plugin_invoke("ticket.add.log_params", {}, ticket_id, ticket, user)
     f_app.log.add("ticket_refresh", ticket_id=ticket_id, **log_params)
     return result
@@ -1288,8 +1289,9 @@ def rent_ticket_contact_info(user, ticket_id):
     status=(list, ["to rent"], str),
     per_page=int,
     last_modified_time=datetime,
+    sort_time=datetime,
     time=datetime,
-    sort=(list, ["last_modified_time", 'desc'], str),
+    sort=(list, ["sort_time", 'desc'], str),
     rent_type="enum:rent_type",
     user_id=ObjectId,
     rent_budget_min="i18n:currency",
