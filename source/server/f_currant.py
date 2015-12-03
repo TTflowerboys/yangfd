048bdce39a8c9f019b9a53b482946a7414fa9f1a
simon Artisan <endlessbytes@gmail.com>
2015-12-03 14:33:41 +0800

    [server] Mod: try to fix has draft value problem

--- source/server/f_currant.py
+++ source/server/f_currant.py
@@ -1315,7 +1315,7 @@ class f_currant_plugins(f_app.plugin_base):
                     tag="rent_ticket_publish_success",
                 )
 
-            f_app.user.update_set(ticket.get('user_id', None), {'debug_message': this_ticket.get('property', None)})
+            # f_app.user.update_set(ticket.get('user_id', None), {'debug_message': this_ticket.get('property', None)})
             if 'property' in this_ticket and this_ticket["property"].get("user_generated") is True:
                 f_app.property.update_set(this_ticket["property"]["id"], {"status": "selling"})
 
@@ -1325,7 +1325,7 @@ class f_currant_plugins(f_app.plugin_base):
                 ticket_id=ticket_id,
             ))
 
-        # f_app.user.update_set(ticket.get('user_id', None), {'debug_message': [ticket.get('status', None), ticket.get('type', None)]})
+        f_app.user.update_set(ticket.get('user_id', None), {'debug_message': [ticket.get('status', None), ticket.get('type', None)]})
         if ticket.get('type', None) == "rent":
             f_app.user.analyze_data_update(ticket.get('user_id', None), {'analyze_rent_has_draft': True})
 
