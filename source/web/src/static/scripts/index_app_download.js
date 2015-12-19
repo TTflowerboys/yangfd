baa5754254442dccb5601cebd4d17785bdfcb35c
levy.li <levy.li@bbtechgroup.com>
2015-12-19 13:46:40 +0800

    [Web]Mod: Issue #7708 首页订阅按钮

--- source/web/src/static/scripts/index_app_download.js
+++ source/web/src/static/scripts/index_app_download.js
@@ -21,4 +21,26 @@ window.setupDownload = function (Swiper) {
             window.wechatShareSDK.showGuideLine(i18n('点击后在弹出的菜单中选择 [在Safari中打开]'))
         }
     })
+
+    $('#subscribeBtn').bind('click', function (e) {
+        var email = $('[name=email]').val()
+        if(!window.project.emailReg.test(email)) {
+            window.alert(i18n('邮件格式不正确，请重新填写'))
+            return false
+        }
+        if($(this).data('disabled') === true) {
+            return false
+        }
+        $('.subscribeAndroid').find('button').text(i18n('提交中...')).data('disabled', true)
+        $.betterPost('/api/1/subscription/add', {
+            'tag': JSON.stringify(['subscribe_android_app']),
+            'email': email
+        }).done(function (val) {
+            $('.subscribeAndroid').hide().siblings('.info').show()
+        }).fail(function (ret) {
+            window.alert(window.getErrorMessageFromErrorCode(ret))
+        }).always(function () {
+            $('.subscribeAndroid').find('button').text(i18n('订阅')).data('disabled', false)
+        })
+    })
 }
