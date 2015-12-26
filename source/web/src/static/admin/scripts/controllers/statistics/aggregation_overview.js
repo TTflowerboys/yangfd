db3320f04bad5c3b5d0f0573cfc566b12f94a96e
simon Artisan <endlessbytes@gmail.com>
2015-12-26 10:37:25 +0800

    [web] Mod: modi js

--- source/web/src/static/admin/scripts/controllers/statistics/aggregation_overview.js
+++ source/web/src/static/admin/scripts/controllers/statistics/aggregation_overview.js
@@ -4,9 +4,9 @@
 
         $scope.get_aggregate_data = function () {
             if ($scope.selected.date_from && $scope.selected.date_to) {
-                api.get_general(date_from=$scope.selected.date_from, date_to=$scope.selected.date_to).success(on_refresh_general)
-                api.get_rent_request(date_from=$scope.selected.date_from, date_to=$scope.selected.date_to).success(on_refresh_rent_request)
-                api.get_rent_ticket(date_from=$scope.selected.date_from, date_to=$scope.selected.date_to).success(on_refresh_rent_ticket)
+                api.get_general($scope.selected.date_from, $scope.selected.date_to).success(on_refresh_general)
+                api.get_rent_request($scope.selected.date_from, $scope.selected.date_to).success(on_refresh_rent_request)
+                api.get_rent_ticket($scope.selected.date_from, $scope.selected.date_to).success(on_refresh_rent_ticket)
             }
         }
 
