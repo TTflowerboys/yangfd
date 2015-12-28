2ea7a8870bec2ab3c8a4928cd08fca3aa8b49cc8
simon Artisan <endlessbytes@gmail.com>
2015-12-28 16:30:31 +0800

    [web] Mod: del template files.

--- source/web/src/static/admin/scripts/controllers/statistics/route2.js
+++
@@ -1,9 +0,0 @@
-(function () {
-
-    function ctrlStatisticsRoute2($scope, fctModal, api, $stateParams) {
-        $scope.bar = [1,2,3,4,5]
-
-    }
-    angular.module('app').controller('ctrlStatisticsRoute2', ctrlStatisticsRoute2)
-
-})()
