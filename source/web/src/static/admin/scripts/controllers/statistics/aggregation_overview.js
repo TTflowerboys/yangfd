2bb21ba35a264fc342de4a6ee6d555908c382256
simon Artisan <endlessbytes@gmail.com>
2016-01-07 16:53:43 +0800

    [web] Mod: add pie graph

--- source/web/src/static/admin/scripts/controllers/statistics/aggregation_overview.js
+++ source/web/src/static/admin/scripts/controllers/statistics/aggregation_overview.js
@@ -67,42 +67,58 @@
                   $(tt).hide();
                 }
               }
+              // $('#aggregation_user_type')
+              $('#aggregation_register_user_total').css({
+                'height': '200px'
+              })
               var myChart = new xChart('line-dotted', graph_data, '#aggregation_register_user_total', opts)
             })
 
           }
-          else {
-            api.get_general($scope.selected.date_from, $scope.selected.date_to).success(on_refresh_general)
-            api.get_rent_request($scope.selected.date_from, $scope.selected.date_to).success(on_refresh_rent_request)
-            api.get_rent_ticket($scope.selected.date_from, $scope.selected.date_to).success(on_refresh_rent_ticket)
-          }
+          $q.all(
+            [
+              api.get_general($scope.selected.date_from, $scope.selected.date_to),
+              api.get_rent_request($scope.selected.date_from, $scope.selected.date_to),
+              api.get_rent_ticket($scope.selected.date_from, $scope.selected.date_to)
+            ]
+          )
+          .then(function (value) {
+            return angular.extend(value[0].data.val, value[1].data.val, value[2].data.val)
+          })
+          .then(function(value) {
+            $scope.place_holder = '无'
+            $scope.register_user_total = value.aggregation_register_user_total
+            $scope.user_type_list = value.aggregation_user_type
+            $scope.rent_request_total = value.aggregation_rent_request_total_count
+            $scope.rent_ticket_total = value.aggregation_rent_ticket_total
+            $scope.rent_ticket_type = value.aggregation_rent_ticket_type
+            return value
+          })
+          .then(function(value) {
+            var graph_user_type = []
+            _.map(value.aggregation_user_type, function(item) {
+              graph_user_type.push({
+                'label': item.type,
+                'data': item.total
+              })
+            })
+            $('#aggregation_user_type').css({'height':'200px'})
+            $.plot($("#aggregation_user_type"), graph_user_type,
+            {
+            		series: {
+            				pie: {
+            						innerRadius: 0.5,
+            						show: true
+            				}
+            		},
+            		legend: {
+            			show: false
+            		},
+            		colors: ["#FA5833", "#2FABE9", "#FABB3D", "#78CD51"]
+            });
+          })
         }
       }
-
-      // $scope.get_aggregate_data = function () {
-      //   if ($scope.selected.date_from && $scope.selected.date_to) {
-      //       api.get_general($scope.selected.date_from, $scope.selected.date_to).success(on_refresh_general)
-      //       api.get_rent_request($scope.selected.date_from, $scope.selected.date_to).success(on_refresh_rent_request)
-      //       api.get_rent_ticket($scope.selected.date_from, $scope.selected.date_to).success(on_refresh_rent_ticket)
-      //   }
-      // }
-      //
-      // function on_refresh_general(data) {
-      //   $scope.register_user_total = data.val.aggregation_register_user_total
-      //   $scope.user_type_list = data.val.aggregation_user_type
-      //   $scope.place_holder = '无'
-      // }
-      //
-      // function on_refresh_rent_request(data) {
-      //   $scope.rent_request_total = data.val.aggregation_rent_request_total_count
-      //   $scope.place_holder = '无'
-      // }
-      //
-      // function on_refresh_rent_ticket(data) {
-      //   $scope.rent_ticket_total = data.val.aggregation_rent_ticket_total
-      //   $scope.rent_ticket_type = data.val.aggregation_rent_ticket_type
-      //   $scope.place_holder = '无'
-      // }
     }
 
     angular.module('app').controller('ctrlStatisticsOverview', ctrlStatisticsOverview)
