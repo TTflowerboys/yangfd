e5f8a179febb634ec2776ce104477f231165f85f
levy.li <levy.li@bbtechgroup.com>
2016-01-11 14:37:57 +0800

    [Web]Mod: Issue #7736 添加动态发送中状态

--- source/web/src/static/admin/scripts/directives/add_dynamic.js
+++ source/web/src/static/admin/scripts/directives/add_dynamic.js
@@ -11,6 +11,9 @@ angular.module('app')
             templateUrl: '/static/admin/templates/add_dynamic.tpl.html',
             controller: function ($scope, $element, $rootScope, $compile) {
                 $scope.submit = function (content) {
+                    if(content === '') {
+                        return
+                    }
                     var dynamicData = {
                         id: misc.generateUUID(),
                         user: {
@@ -22,19 +25,25 @@ angular.module('app')
                         status: $scope.status
                     }
                     $scope.content = ''
-                    var dynamic = _.find($scope.item.custom_fields || [], {key: 'dynamic'}) || {key: 'dynamic', value: '[]'}
+                    var dynamic = _.clone(_.find($scope.item.custom_fields || [], {key: 'dynamic'}) || {key: 'dynamic', value: '[]'})
+                    var dynamicTemp = _.clone(dynamic)
                     dynamic.value = JSON.stringify(JSON.parse(dynamic.value).concat([dynamicData]))
+                    dynamicTemp.value = JSON.stringify(JSON.parse(dynamicTemp.value).concat([_.extend(_.clone(dynamicData), {sending: true})]))
+                    function generateCustomFieldsByDynamic(dynamic){
+                        return _.reject($scope.item.custom_fields || [], function (field) {
+                            return field.key === 'dynamic'
+                        }).concat([dynamic])
+                    }
+                    $scope.item.custom_fields = generateCustomFieldsByDynamic(dynamicTemp)
                     $scope.update({
                         data: {
                             id: $scope.item.id,
-                            custom_fields: _.reject($scope.item.custom_fields || [], function (field) {
-                                return field.key === 'dynamic'
-                            }).concat([dynamic])
+                            custom_fields: generateCustomFieldsByDynamic(dynamic)
                         }
                     })
                         .then(function (data) {
                             growl.addSuccessMessage(window.i18n('添加成功'), {enableHtml: true})
-                            $scope.item = data.data.val
+                            angular.extend($scope.item, data.data.val)
                         }, function () {
                             growl.addErrorMessage(window.i18n('添加失败'), {enableHtml: true})
                         })
