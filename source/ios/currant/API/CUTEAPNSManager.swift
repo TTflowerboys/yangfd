63d6f2058e62b74636bf4d4d4c02c83be87eb3db
Foster Yin <fyin@bbtechgroup.com>
2016-01-12 15:25:17 +0800

    [ios] Add: Task #7739 Generate UUID and persist in NSUserDefault

--- source/ios/currant/API/CUTEAPNSManager.swift
+++ source/ios/currant/API/CUTEAPNSManager.swift
@@ -12,34 +12,44 @@ import Foundation
 @objc(CUTEAPNSManager)
 class CUTEAPNSManager : NSObject {
     static let sharedInstance = CUTEAPNSManager()
+    var binded:Bool?
+    var deviceToken:NSData?
+    var uuid:String!
+
+    private let keyPrefix = "com.bbtechgroup.apns."
 
     override init() {
         super.init()
-        //TODO load binded value
+        deviceToken = NSUserDefaults.standardUserDefaults().dataForKey(keyPrefix + "deviceToken")
+        uuid = NSUserDefaults.standardUserDefaults().stringForKey(keyPrefix + "uuid")
+        if uuid == nil {
+            uuid = NSUUID().UUIDString
+            NSUserDefaults.standardUserDefaults().setObject(uuid, forKey: keyPrefix + "uuid")
+            NSUserDefaults.standardUserDefaults().synchronize()
+        }
     }
-
-    static func udid() -> String! {
-        //TODO choose a UDID generator
-        return "";
-    }
-
-    var binded:Bool?
-    var deviceToken:NSData?
 
     func saveDeviceToken(deviceToken:NSData) {
         self.deviceToken = deviceToken
+        NSUserDefaults.standardUserDefaults().setObject(deviceToken, forKey: keyPrefix + "deviceToken")
+        NSUserDefaults.standardUserDefaults().synchronize()
     }
 
-    ///Call after login
-    func bind(deviceToken:NSData) -> BFTask {
-        let tokenString = deviceToken.description.stringByReplacingOccurrencesOfString(" ", withString: "").stringByReplacingOccurrencesOfString("<", withString: "").stringByReplacingOccurrencesOfString(">", withString: "")
-        let udid = CUTEAPNSManager.udid()
-        return CUTEAPIManager.sharedInstance().POST("/api/1/user/apns/" + udid + "/register" + tokenString, parameters: nil, resultClass: nil)
+    ///Call after login, and must check deviceToken
+    func bind(deviceToken:NSData?) -> BFTask {
+        if deviceToken != nil {
+            let tokenString = deviceToken!.description.stringByReplacingOccurrencesOfString(" ", withString: "").stringByReplacingOccurrencesOfString("<", withString: "").stringByReplacingOccurrencesOfString(">", withString: "")
+
+            return CUTEAPIManager.sharedInstance().POST("/api/1/user/apns/" + uuid + "/register" + tokenString, parameters: nil, resultClass: nil)
+        }
+        else {
+            return BFTask(error: NSError(domain: "com.bbtechgroup.apns", code: -1, userInfo: [NSLocalizedDescriptionKey: "Device token should exist"]));
+        }
+
     }
 
     ///Call before logout
     func unbind() -> BFTask {
-        let udid = CUTEAPNSManager.udid()
-        return CUTEAPIManager.sharedInstance().POST("/api/1/user/apns/" + udid + "/unregister", parameters: nil, resultClass: nil)
+        return CUTEAPIManager.sharedInstance().POST("/api/1/user/apns/" + uuid + "/unregister", parameters: nil, resultClass: nil)
     }
 }
