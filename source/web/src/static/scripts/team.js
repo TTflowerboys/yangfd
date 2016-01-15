4de92fe713f581d4e299098f977c5776355076f9
levy.li <levy.li@bbtechgroup.com>
2016-01-15 18:46:29 +0800

    Revert "[Web]Mod: Issue #7472 统一搜索框（修复热门城市图片大小写问题）"
    This reverts commit c316c6fc80a8219256f4e178d1df0593446a1d6f.

--- source/web/src/static/scripts/team.js
+++ source/web/src/static/scripts/team.js
@@ -60,17 +60,10 @@
             var re = new RegExp('([?&])' + name + '=.*?(&|$)', 'i');
             var separator = urlWithoutHash.indexOf('?') !== -1 ? '&' : '?';
             if (urlWithoutHash.match(re)) {
-                if (value === '' || value === undefined) {
-                    return urlWithoutHash.replace(re, function (match, p1, p2) {
-                            return p2 === '' ? '' : (p1 === '?' ? '?' : p2)
-                        }) + hash
-                } else {
-                    return urlWithoutHash.replace(re, '$1' + name + '=' + encodeURIComponent(value) + '$2') + hash
-                }
-            } else if(value !== '' && value !== undefined) {
+                return urlWithoutHash.replace(re, '$1' + name + '=' + encodeURIComponent(value) + '$2') + hash
+            }
+            else {
                 return urlWithoutHash + separator + name + '=' + encodeURIComponent(value) + hash
-            } else {
-                return urlWithoutHash + hash
             }
         },
         getHash: function (n) {
@@ -537,13 +530,6 @@
                 return index + 1
             })
         },
-        openLink: function (url) {
-            if(window.team.isCurrantClient() && window.bridge) {
-                window.bridge.callHandler('openURLInNewController', url)
-                return
-            }
-            window.open(url)
-        }
     }
 })
 ();
