1537ea4cde3393e0e339da9d0157c3620ae56ad0
simon Artisan <endlessbytes@gmail.com>
2016-02-04 11:35:25 +0800

    [web] Mod: single and whole

--- source/web/src/static/admin/scripts/controllers/statistics/aggregation_rent_request.js
+++ source/web/src/static/admin/scripts/controllers/statistics/aggregation_rent_request.js
@@ -10,6 +10,22 @@
       function on_refresh(data) {
         $scope.value = data.val
         $scope.place_holder = '无'
+        $scope.value.aggregation_rent_request_period_count_result = []
+        for (var index = 0; index < $scope.value.aggregation_rent_request_period_count.length; index++) {
+          for(var detail_index = 0; detail_index < $scope.value.aggregation_rent_request_period_count[index].detail.length; detail_index++) {
+            var single_data = {}
+            single_data = {
+              'type': $scope.value.aggregation_rent_request_period_count[index].detail[detail_index].type,
+              'count': $scope.value.aggregation_rent_request_period_count[index].detail[detail_index].count
+            }
+            if (detail_index === 0) {
+              single_data.period = $scope.value.aggregation_rent_request_period_count[index].period
+              single_data.total = $scope.value.aggregation_rent_request_period_count[index].detail.length
+            }
+            $scope.value.aggregation_rent_request_period_count_result = $scope.value.aggregation_rent_request_period_count_result.concat(single_data)
+          }
+        }
+        window.console.log($scope.value.aggregation_rent_request_period_count_result)
       }
 
     }
