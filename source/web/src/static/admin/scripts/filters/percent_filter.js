c5031e11fa81226c3d6f26cbdde5ad9134e5322a
simon Artisan <endlessbytes@gmail.com>
2016-02-05 16:18:40 +0800

    [web] Mod: add different city rent data to dashboard

--- source/web/src/static/admin/scripts/filters/percent_filter.js
+++ source/web/src/static/admin/scripts/filters/percent_filter.js
@@ -2,5 +2,5 @@ angular.module('app')
   .filter('percentage', function ($filter) {
     return function (input, decimals) {
       return input ? $filter('number')(input * 100, decimals) + '%' : ''
-    };
-  });
+    }
+  })
