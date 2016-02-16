7808b8364be14e8779bb7d46ab697fc95909e83e
simon Artisan <endlessbytes@gmail.com>
2016-02-16 11:41:26 +0800

    [web] Mod: add more percentage format

--- source/web/src/static/admin/scripts/filters/percent_filter.js
+++ source/web/src/static/admin/scripts/filters/percent_filter.js
@@ -1,6 +1,9 @@
 angular.module('app')
   .filter('percentage', function ($filter) {
     return function (input, decimals) {
+      if(input === 0) {
+        return '0%'
+      }
       return input ? $filter('number')(input * 100, decimals) + '%' : ''
     }
   })
