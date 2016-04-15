d70249f7f04c8586477ce57622d7ccce17ba29b3
Foster Yin <fyin@bbtechgroup.com>
2016-04-15 17:32:16 +0800

    [web] Add: Update communication SMS templates

--- source/web/src/static/admin/scripts/values/rent_intention_ticket_status.js
+++ source/web/src/static/admin/scripts/values/rent_intention_ticket_status.js
@@ -6,7 +6,7 @@ angular.module('app')
         { name: i18n('沟通中'), value: 'in_progress' },
         { name: i18n('已拒绝'), value: 'rejected' },
         { name: i18n('已确认视频看房'), value: 'confirmed_video' },
-        { name: i18n('预订确认，等待支付'), value: 'booked' },
+        { name: i18n('预订请求，等待支付'), value: 'booked' },
         { name: i18n('定金已支付，确认入住'), value: 'holding_deposit_paid' },
         { name: i18n('预订取消'), value: 'canceled' },
         { name: i18n('租客已入住'), value: 'checked_in' }
