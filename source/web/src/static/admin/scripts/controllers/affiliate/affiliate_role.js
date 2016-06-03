5bee337e4a11776fdc5580ea9ffb2607ba38cca5
Foster Yin <fyin@bbtechgroup.com>
2016-06-03 20:41:09 +0800

    [web] Add: Progress bar for affiliate member load http://codepen.io/joe-watkins/pen/cvxuJ

--- source/web/src/static/admin/scripts/controllers/affiliate/affiliate_role.js
+++ source/web/src/static/admin/scripts/controllers/affiliate/affiliate_role.js
@@ -13,73 +13,71 @@
 
         $scope.api = api
         $scope.fetched = false
+        
+        $scope.progress = {percentage: 10}
 
-        $scope.refreshList = function () {
+        $timeout(function () {            
             api.getAll({
                 params: {
                     referral: $scope.user.id,
                     per_page: -1
                 }
-            }).success(onGetList)
-        }
-
-        function onGetList(data) {
-            $scope.fetched = true
-            $scope.list = _.map(
-                data.val,
-                function (item, index) {
-                    rentRequestIntentionApi.getAll({
-                        params: {
-                            user_id: item.id,
-                            status: JSON.stringify(['requested','assigned','in_progress','rejected','confirmed_video','booked','holding_deposit_paid','canceled','checked_in'])
-                        }
-                    }).success(
-                        function (data) {
-                            $scope.list[index].requested_tickets_count = data.val.length
-                            var success_count = 0
-                            data.val.forEach(function(item) {
-                                if (item.status === 'checked_in') {
-                                    success_count += 1
-                                }
-                            })
-                            $scope.list[index].success_rent_tickets_count = success_count
-                        }
-                    )
-                    /*couponApi.search({
-                        user_id: item.id
-                    })
-                        .success(function (data) {
-                            //todo 等接口修复后再填充优惠券数据
-                        })*/
-                    return item
+            }).then(function (response) {
+                if (response.status !== 200) {
+                    return
                 }
-            )
-        }
-
-
-        $timeout(function () {
-            $scope.refreshList()
-
-            statisticsApi.getNewAffiliateUserBehavior({
-                user_id: $scope.user.id
-            }).success(function (data) {
+                var data = response.data
+                $scope.fetched = true
+                $scope.list = _.map(
+                    data.val,
+                    function (item, index) {
+                        rentRequestIntentionApi.getAll({
+                            params: {
+                                user_id: item.id,
+                                status: JSON.stringify(['requested', 'assigned', 'in_progress', 'rejected', 'confirmed_video', 'booked', 'holding_deposit_paid', 'canceled', 'checked_in'])
+                            }
+                        }).success(
+                            function (data) {
+                                $scope.list[index].requested_tickets_count = data.val.length
+                                var success_count = 0
+                                data.val.forEach(function (item) {
+                                    if (item.status === 'checked_in') {
+                                        success_count += 1
+                                    }
+                                })
+                                $scope.list[index].success_rent_tickets_count = success_count
+                            }
+                            )
+                        return item
+                    }
+                )
+                $scope.progress.percentage = 50
+                                                
+                return  statisticsApi.getNewAffiliateUserBehavior({user_id: $scope.user.id})
+            }).then(function (response) {
+                if (response.status !== 200) {
+                    return
+                }
+                var data = response.data
                 if(data.val && data.val.length) {
                     $scope.data.userTotal = data.val[0].affiliate_new_user_total //总注册会员数
                     $scope.showAggregationDataUserTotal = true
                 }
-            })
-
-            statisticsApi.getAllAffiliateUserBehavior({
-                user_id: $scope.user.id
-            }).success(
-                function (data) {
-                    if(data.val && data.val.length) {
-                        $scope.data.request_count = data.val[0].affiliate_all_user_request_count
-                        $scope.data.success_rent = data.val[0].affiliate_all_user_success_rent
-                        $scope.showAggregationDataMemberTicketData = true
-                    }
+                
+                $scope.progress.percentage = 75
+                return statisticsApi.getAllAffiliateUserBehavior({user_id: $scope.user.id})
+            }).then(function (response) {    
+                if (response.status !== 200) {
+                    return
                 }
-            )
+                var data = response.data           
+                if (data.val && data.val.length) {
+                    $scope.data.request_count = data.val[0].affiliate_all_user_request_count
+                    $scope.data.success_rent = data.val[0].affiliate_all_user_success_rent
+                    $scope.showAggregationDataMemberTicketData = true
+                }
+                $scope.progress.percentage = 100
+            })                        
         })
     }
 
