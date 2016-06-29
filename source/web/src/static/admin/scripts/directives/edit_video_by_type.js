5665ec6c7857d933eebaa4c3d8e741e5fc7332b7
Foster Yin <fyin@bbtechgroup.com>
2016-06-29 11:01:59 +0800

    [web] Add: Fix js error

--- source/web/src/static/admin/scripts/directives/edit_video_by_type.js
+++ source/web/src/static/admin/scripts/directives/edit_video_by_type.js
@@ -20,6 +20,7 @@ angular.module('app')
                 scope.onFileSelected = function ($files) {
                     var file = $files[0]
                     if (file) {
+                        var url = undefined
                         if (scope.host === 'aws_s3') {
                             url = '/api/1/upload_file'
                         } else {
