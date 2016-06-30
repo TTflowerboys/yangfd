ec0663dba7c54b5b9c6331109735183e3f0954e0
Foster Yin <fyin@bbtechgroup.com>
2016-06-30 15:23:26 +0800

    [web] Add: Fix city name from new i18n result again

--- source/web/src/static/admin/scripts/filters/property_address.js
+++ source/web/src/static/admin/scripts/filters/property_address.js
@@ -1,7 +1,7 @@
 angular.module('app')
     .filter('propertyAddress', function ($rootScope) {
-        function getValue(item, key){
-            key = key || $rootScope.userLanguage.value
+        function getValue(item){
+            var key = $rootScope.userLanguage.value
             if (!_.isObject(item)) {
                 return item
             } else {
@@ -12,6 +12,6 @@ angular.module('app')
             if (!property) {
                 return ''
             }
-            return (_.compact([(_.compact([getValue(property.floor), getValue(property.house_name)])).join('-')].concat([getValue(property.community), getValue(property.street), getValue(property.city, 'name'), window.team.countryMap[getValue(property.country, 'code')]]))).join(',')
+            return (_.compact([(_.compact([getValue(property.floor), getValue(property.house_name)])).join('-')].concat([getValue(property.community), getValue(property.street), getValue(property.city.name), window.team.countryMap[getValue(property.country.code)]]))).join(',')
         }
     })
