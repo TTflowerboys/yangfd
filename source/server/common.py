f3f46a1b176a0ac8a6c268f1927df2a24568f440
Felix Yan <felixonmars@archlinux.org>
2016-07-28 12:47:07 +0800

    [server] Mod: use production sinch key

--- source/server/common.py
+++ source/server/common.py
@@ -137,8 +137,8 @@ class common(f_common):
     nexmo_api_secret = "9c4d83a4"
     nexmo_default_sender = "13605895103"
     nexmo_number_mapping_dimensions = ["ticket_id"]
-    sinch_api_key = "6fd22ac3-d8c5-484c-b304-4c285d33735b"
-    sinch_api_secret = "hsnNr7EaAkWzFFXcuBnWkw=="
+    sinch_api_key = "6608a0fb-719c-4167-8e5e-2bfe44c88346"
+    sinch_api_secret = "NWobeaXMlESbOtEVlS3tBA=="
 
     sms_verification_default_method = "sinch"
     user_sms_verification_msg = "%s 为您的洋房东手机验证码，1小时内有效。感谢您使用洋房东的服务。"
