0553d55874217d79839162dcc204fffc4ca0bf52
Foster Yin <fyin@bbtechgroup.com>
2016-08-29 15:03:26 +0800

    [web] Add: $http disabling caching https://github.com/angular/angular.js/issues/1586

--- source/web/src/static/admin/scripts/factories/statistics_api.js
+++ source/web/src/static/admin/scripts/factories/statistics_api.js
@@ -5,28 +5,28 @@
 
         return {
             get_general: function (date_from, date_to) {
-              return $http.get('/api/1/aggregation-general' + '?date_from=' + date_from + '&date_to=' + date_to)
+              return $http.get('/api/1/aggregation-general' + '?date_from=' + date_from + '&date_to=' + date_to , { headers: { 'Cache-Control' : 'no-cache' } })
             },
             get_rent_ticket: function (date_from, date_to) {
-              return $http.get('/api/1/aggregation-rent-ticket' + '?date_from=' + date_from + '&date_to=' + date_to)
+              return $http.get('/api/1/aggregation-rent-ticket' + '?date_from=' + date_from + '&date_to=' + date_to , { headers: { 'Cache-Control' : 'no-cache' } })
             },
             get_rent_intention_ticket: function (date_from, date_to) {
-              return $http.get('/api/1/aggregation-rent-intention-ticket' + '?date_from=' + date_from + '&date_to=' + date_to)
+              return $http.get('/api/1/aggregation-rent-intention-ticket' + '?date_from=' + date_from + '&date_to=' + date_to , { headers: { 'Cache-Control' : 'no-cache' } })
             },
             get_property_view: function (date_from, date_to) {
-              return $http.get('/api/1/aggregation-property-view' + '?date_from=' + date_from + '&date_to=' + date_to)
+              return $http.get('/api/1/aggregation-property-view' + '?date_from=' + date_from + '&date_to=' + date_to , { headers: { 'Cache-Control' : 'no-cache' } })
             },
             get_email_detail: function (date_from, date_to) {
-              return $http.get('/api/1/aggregation-email-detail' + '?date_from=' + date_from + '&date_to=' + date_to)
+              return $http.get('/api/1/aggregation-email-detail' + '?date_from=' + date_from + '&date_to=' + date_to , { headers: { 'Cache-Control' : 'no-cache' } })
             },
             get_favorite: function (date_from, date_to) {
-              return $http.get('/api/1/aggregation-favorite' + '?date_from=' + date_from + '&date_to=' + date_to)
+              return $http.get('/api/1/aggregation-favorite' + '?date_from=' + date_from + '&date_to=' + date_to , { headers: { 'Cache-Control' : 'no-cache' } })
             },
             get_rent_request: function (date_from, date_to) {
-              return $http.get('/api/1/aggregation-rent-request' + '?date_from=' + date_from + '&date_to=' + date_to)
+              return $http.get('/api/1/aggregation-rent-request' + '?date_from=' + date_from + '&date_to=' + date_to , { headers: { 'Cache-Control' : 'no-cache' } })
             },
             get_view_contact: function (date_from, date_to) {
-              return $http.get('/api/1/aggregation-view-contact' + '?date_from=' + date_from + '&date_to=' + date_to)
+              return $http.get('/api/1/aggregation-view-contact' + '?date_from=' + date_from + '&date_to=' + date_to , { headers: { 'Cache-Control' : 'no-cache' } })
             },
             getNewAffiliateUserBehavior: function (params) {
                 return $http.post('/api/1/affiliate-get-new-user-behavior', params)
