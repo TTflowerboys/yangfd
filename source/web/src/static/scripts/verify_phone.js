d154f12d40c06b687b5f69f01d69338f9db1445b
Foster Yin <fyin@bbtechgroup.com>
2016-09-05 19:08:27 +0800

    [web] Add: Refresh verify phone status

--- source/web/src/static/scripts/verify_phone.js
+++ source/web/src/static/scripts/verify_phone.js
@@ -3,69 +3,114 @@
     var $errorMsg = $('.errorMessage')
     var $getCodeBtn = $('.getCode')
     var $verifyBtn = $('.verifyBtn')
+    var $goToNextBtn = $('.goToNextBtn')
     var $hint = $('.formsHint')
     var isPhoneValid = true
     $('.phoneRow').bind('click', function () {
         $hint.show()
         $(this).find('.phoneReadonly').hide().next('.phoneEdit').show()
     })
-    requestSmsCode() //页面载入即自动发送验证码
+    
+    //requestSmsCode() //页面载入即自动发送验证码
     $getCodeBtn.bind('click', function () {
         if(!$getCodeBtn.attr('disabled') && isPhoneValid) {
             requestSmsCode()
         }
     })
-    $verifyBtn.bind('click', function () {
-        var code = $('[name=code]').val()
-        var params = {
-            code: code
-        }
+
+    function sendVoiceVerification() {
         $errorMsg.hide()
-        if (/^(\d){6}$/.test(code)) {
-            $.betterPost('/api/1/user/' + window.user.id + '/sms_verification/verify', params)
-                .done(function (data) {
-                    window.user.phone_verified = true
 
-                    if(window.team.getQuery('from').indexOf('intention') >= 0){
-                        if(window.bridge !== undefined && window.user && window.user.user_type && window.user.user_type.length) {
-                            window.bridge.callHandler('login', window.user);
-                        }
-                    } else {
-                        if(window.bridge !== undefined && window.user){
-                            window.bridge.callHandler('updateUser', window.user);
-                        }
-                    }
-                    $errorMsg.text(window.i18n('验证成功'))
-                    $errorMsg.show()
-                    
-                    if(window.user && window.user.user_type && window.user.user_type.length){
-                        var affiliateType = _.find(window.user.user_type, function (oneType) {
-                            return oneType.slug === 'affiliate'
-                        })
-                        if (affiliateType) {
-                            location.href = '/user-invite'
-                        }
-                        else {
-                            location.href = '/'
-                        }
-                    }
-                    else {                        
-                        window.project.goBackFromURL()
-                    }
-                })
-                .fail(function (ret) {
-                    window.dhtmlx.message({type:'error', text: window.i18n('验证失败')})
-                    $errorMsg.text(window.i18n('验证失败'))
-                    $errorMsg.show()
-                })
-                .always(function () {
-
-                })
-        } else {
-            window.dhtmlx.message({type:'error', text: window.i18n('验证码为6位数字，请填写正确后再验证')})
-            $errorMsg.text(window.i18n('验证码为6位数字，请填写正确后再验证')).show()
+        var params = {
+            phone: '+' + $('[name=country_code]').val() + $('[name=phone]').val(),
+            verify_method: 'call'
         }
-    })
+
+        $.betterPost('/api/1/user/sms_verification/send', params)
+            .done(function (val) {
+                $errorMsg.text(window.i18n('发送成功'))
+                $errorMsg.show()
+                $verifyBtn.hide()
+                $goToNextBtn.hide()
+                startVerificationTimer()
+                startCheckVoiceVerfication()
+            })
+            .fail(function (ret) {
+                window.dhtmlx.message({type:'error', text: window.i18n('发送成功')})
+                $errorMsg.text(window.i18n('发送成功'))
+                $errorMsg.show()
+            })
+            .always(function () {
+            })
+    }
+    
+    $verifyBtn.bind('click', sendVoiceVerification)
+
+    function goToNext() {
+        window.user.phone_verified = true
+
+        if(window.team.getQuery('from').indexOf('intention') >= 0){
+            if(window.bridge !== undefined && window.user && window.user.user_type && window.user.user_type.length) {
+                window.bridge.callHandler('login', window.user);
+            }
+        } else {
+            if(window.bridge !== undefined && window.user){
+                window.bridge.callHandler('updateUser', window.user);
+            }
+        }
+        
+        if(window.user && window.user.user_type && window.user.user_type.length){
+            var affiliateType = _.find(window.user.user_type, function (oneType) {
+                return oneType.slug === 'affiliate'
+            })
+            if (affiliateType) {
+                location.href = '/user-invite'
+            }
+            else {
+                location.href = '/'
+            }
+        }
+        else {
+            window.project.goBackFromURL()
+        }
+    }
+
+    $goToNextBtn.bind('click', goToNext())
+
+    function startCheckVoiceVerfication() {
+        var params = {
+            phone: '+' + $('[name=country_code]').val() + $('[name=phone]').val()
+        }
+        $.betterGet('/api/1/user/sms_verfication/sinch_call_check', params)
+            .done(function (val) {
+                $errorMsg.text(window.i18n('验证成功'))
+                $errorMsg.show()
+                $verifyBtn.hide()
+                $goToNextBtn.show()
+            })
+            .fail(function (ret) {
+                startCheckVoiceVerfication()
+            })
+    }
+
+    function startVerificationTimer() {
+        var $form = $('.formWrap')
+        $form.find('#voiceVerification').hide()
+        $form.find('#voiceHint').show()
+        var sec = 60
+        var timer = setInterval(function(){
+            if(sec === 0){
+                $form.find('#voiceHint').html(window.i18n('请按照语音提示操作（默认按手机键盘上数字1即可验证成功），如果没有接到联系电话，请') +  window.i18n('<a id="voiceVerification" class="voiceVerification">重试</a>'))
+                $form.find('#voiceHint #voiceVerification').click(sendVoiceVerification)
+                clearInterval(timer)
+            }
+            else {
+                $form.find('#voiceHint').text(window.i18n('请按照语音提示操作（默认按手机键盘上数字1即可验证成功），如果没有接到联系电话，请') + sec + window.i18n('s 后重试'))
+                sec--
+            }
+        },1000)
+    }
+
     function requestSmsCode () {
         $getCodeBtn.prop('disabled', true)
         var originParams
