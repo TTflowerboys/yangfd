088796e78289d79a004ebf058211414cdbadec38
Foster Yin <fyin@bbtechgroup.com>
2016-10-19 16:40:21 +0800

    [web] Add: Restore old translation msg from po file comments

---
+++ source/po/restore_old_msg.py
@@ -0,0 +1,35 @@
+#!/bin/env python
+
+# Usage: python restore_old_msg.py web/en_GB.po > web/en_GB.restore.po
+# mv web/en_GB.restore.po web/en_GB.po
+
+import sys
+
+
+file_name = sys.argv[1]
+f = open(file_name, 'r')
+
+
+old_msg_dic = {}
+key = ''
+value = ''
+
+for line in open(file_name):
+    if line.startswith('#~ msgid'):
+        key = line[3:]
+        # print key
+    elif line.startswith('#~ msgstr'):
+        value = line[3:]
+        # print value
+        if len(key) & len(value):
+            old_msg_dic[key] = value
+
+
+previous_line = ''
+for line in open(file_name):
+    if len(previous_line) and previous_line.startswith('msgid ') and line.startswith('msgstr ""') and previous_line in old_msg_dic:
+        print old_msg_dic[previous_line],
+    else:
+        print line,
+
+    previous_line = line
