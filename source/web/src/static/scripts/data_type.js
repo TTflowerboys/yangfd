1bae17c3cf69dbac9a47ff5e4ad92fc9ade64d9f
Tom.lei <tom.lei@bbtechgroup.com>
2016-11-11 20:15:07 +0800

    [Web] Add:'formatted_currency_mouth'.

--- source/web/src/static/scripts/data_type.js
+++ source/web/src/static/scripts/data_type.js
@@ -42,6 +42,20 @@
                     )
                 )
             }
+        }else if (type === 'formatted_currency_mouth') {
+            if (dom.tagName.toLowerCase() === 'input') {
+                $dom.val(
+                    team.formatCurrency(
+                        $dom.val()
+                    )
+                )
+            } else {
+                $dom.html(
+                    team.formatCurrency(
+                        $dom.attr('data-price')
+                    )
+                )
+            }
         }
     }
 
