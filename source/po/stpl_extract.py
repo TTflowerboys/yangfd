b6bc355402f2d8487d698cdec04ea83ab675b2aa
Felix Yan <felixonmars@archlinux.org>
2016-11-24 14:27:51 +0800

    [po] Mod: some fixes for python 3.x

--- source/po/stpl_extract.py
+++ source/po/stpl_extract.py
@@ -1,9 +1,12 @@
 import re
+import six
 
 
 def babel_extract(fileobj, keywords, comment_tags, options):
     keyword_regex = re.compile("(" + "|".join(keywords) + r")\(['\"](.*?[^\\])['\"]\)")
 
     for lineno, line in enumerate(fileobj):
+        if isinstance(line, six.binary_type):
+            line = line.decode("utf-8")
         for match in keyword_regex.findall(line):
-            yield lineno + 1, None, match[1].decode("utf-8"), []
+            yield lineno + 1, None, match[1], []
