b5d1a90ea16e3edcd6cbebbad898452738ee78de
threetowns <tom.lei@bbtechgroup.com>
2016-12-09 20:01:32 +0800

    [Web] Add: mobile chat.

--- source/web/src/static/scripts/user_chat.js
+++ source/web/src/static/scripts/user_chat.js
@@ -1,7 +1,10 @@
 var chat = {
   init : function(){
-    $('#btn_send').on('click',function(){
+    $('#btn_send,#btn_send_phone').on('click',function(){
       if (chat.isSendMsg()) { chat.sendTextMessage() }
+    })
+    $('#btn_send_phone').on('click',function(){
+      if (chat.isMobileSendMsg()) { chat.sendMobileTextMessage() }
     })
     $('#edit_area').on('keydown', function(e) {
       if(e.keyCode === team.keyCode.KEYCODE_ENTER){
@@ -13,11 +16,14 @@ var chat = {
   isSendMsg : function(){
     return $('#edit_area').val().trim().length ? true : false;
   },
+  isMobileSendMsg : function(){
+    return $('#chat_edit_area').val().trim().length ? true : false;
+  },
   meMsgTpl : function(picUrl,plain){
-      return '<div class="message me"><img src="'+picUrl+'" alt="" class="avatar"><div class="content"><div class="bubble bubble_primary right"><div class="bubble_cont"><div class="plain"><pre>'+plain+'</pre></div></div></div></div></div>'
+      return '<div class="message me"><img src="'+picUrl+'" alt="" class="avatar"><div class="content"><div class="bubble bubble_primary right"><div class="bubble_cont"><div class="plain">'+plain+'</div></div></div></div></div>'
   },
   defMsgTpl : function(picUrl,plain){
-    return '<div class="message"><img src="'+picUrl+'" alt="" class="avatar"><div class="content"><div class="bubble bubble_default left"><div class="bubble_cont"><div class="plain"><pre>'+plain+'</pre></div></div></div></div></div>'
+    return '<div class="message"><img src="'+picUrl+'" alt="" class="avatar"><div class="content"><div class="bubble bubble_default left"><div class="bubble_cont"><div class="plain">'+plain+'</div></div></div></div></div>'
   },
   meMsgImgTpl : function(picUrl,img){
     return '<div class="message me"><img src="'+picUrl+'" alt="" class="avatar"><div class="content"><div class="bubble bubble_primary right"><div class="bubble_cont"><div class="plain"><img src="'+img+'"></div></div></div></div></div>'
@@ -39,8 +45,25 @@ var chat = {
       }
     });
   },
+  sendMobileTextMessage : function(){
+    $.ajax({
+      url: '/api/1/message',
+      type: 'post',
+      data:null,
+      dataType: 'json',
+      timeout: 20000,
+      cache: false,
+      error: function(){//出错
+          window.alert('服务端出错！');
+      },
+      success: function(res){//成功
+        $('#chatContent').prepend(chat.meMsgTpl('/static/images/chat/hostHeader.jpg',$('#chat_edit_area').val()));
+        chat.clearEditArea()
+      }
+    });
+  },
   clearEditArea : function(){
-    $('#edit_area').val('');
+    $('#edit_area,#chat_edit_area').val('');
   }
 }
 chat.init();
