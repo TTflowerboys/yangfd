4738c4650be82317513799c774df818b48039f77
threetowns <tom.lei@bbtechgroup.com>
2016-12-19 18:14:52 +0800

    [Web] Fix:chat tab.

--- source/web/src/static/scripts/chat_list.js
+++ source/web/src/static/scripts/chat_list.js
@@ -1,145 +1,163 @@
+$(function(){
+    var $headerButtons = $('.contentHeader .buttons')
+    var $headerTabs = $('.tabs')
+    var $list = $('#chatListContent')
+    var isLoading = false
+    var xhr
+    var placeholder = $('.emptyPlaceHolder')
+    var chatListHeader = $('.chatListHeader')
 
-function chatListBind(ko, module) {
-    ko.components.register('kot-chat-list', {
-        viewModel: function (params) {
-            var self = this
-            this.pageIndex = 0
-            this.pagePreStatus = true
-            this.pageNexStatus = true
-            this.pageLengthSingle = 10
-            this.pagePreText = i18n('上一页')
-            this.pageNexText = i18n('下一页')
-            this.showAllText = i18n('全部')
-            this.showNewText = i18n('未读')
-            this.showReadText = i18n('已读')
-            this.messageSourceData = categoryMessage(JSON.parse($('.messageData').text()))
-            this.messageData = filterMessage(this.messageSourceData, 'all')
-            this.switchTab = function (data) {
-                self.tabActive(data)
-                self.messageData = filterMessage(self.messageSourceData, data)
-                self.messageData.forEach(function (data) {
-                    data.expand(false)
-                    data.expandStatus(i18n('展开'))
-                })
-                self.messageList(self.messageData.slice(0, self.pageLengthSingle))
-                self.pageIndex = 0
-                self.pageShowIndex(1)
-                self.pagePreDisabled('disabled')
-                self.pagePreImgUrl('/static/images/user/pre-page-disabled.png')
-                if (self.messageList().length >= self.messageData.length) {
-                    self.pageNexDisabled('disabled')
-                    self.pageNexImgUrl('/static/images/user/next-page-disabled.png')
-                }
-                else {
-                    self.pageNexDisabled(null)
-                    self.pageNexImgUrl('/static/images/user/next-page.png')
-                }
-            }
 
-            this.pageShowIndex = ko.observable(1)
-            this.tabActive = ko.observable('all')
-            this.messageList = ko.observableArray(this.messageData.slice(0, this.pageLengthSingle))
-            this.pagePreImgUrl = ko.observable('/static/images/user/pre-page-disabled.png')
-            this.pageNexImgUrl = ko.observable('/static/images/user/next-page.png')
-            if (this.pageLengthSingle > this.messageData) {
-                this.pageNexImgUrl('/static/images/user/next-page-disabled.png')
-            }
-            this.pagePreDisabled = ko.observable('disabled')
-            this.pageNexDisabled = ko.observable(null)
-            this.pageNexTrigger = function () {
-                var messageShowIndexBegin = self.pageIndex * self.pageLengthSingle
-                var messageShowIndexEnd = messageShowIndexBegin + self.pageLengthSingle
-                var messageShowNextPageIndexEnd = messageShowIndexEnd + self.pageLengthSingle
-                if (messageShowIndexEnd < self.messageData.length) {
-                    self.pageIndex += 1
-                    self.pageShowIndex(self.pageIndex + 1)
-                }
-                if (messageShowNextPageIndexEnd >= self.messageData.length) {
-                    self.pageNexDisabled('disabled')
-                    self.pageNexImgUrl('/static/images/user/next-page-disabled.png')
-                }
-                messageShowIndexBegin = self.pageIndex * self.pageLengthSingle
-                messageShowIndexEnd = messageShowIndexBegin + self.pageLengthSingle
-                if (messageShowIndexBegin) {
-                    self.pagePreDisabled(null)
-                    self.pagePreImgUrl('/static/images/user/pre-page.png')
-                }
-                self.messageList(self.messageData.slice(messageShowIndexBegin, messageShowIndexEnd))
-            }
-            this.pagePreTrigger = function () {
-                var messageShowIndexBegin = self.pageIndex * self.pageLengthSingle
-                var messageShowIndexEnd = messageShowIndexBegin + self.pageLengthSingle
-                if (self.pageIndex) {
-                    self.pageIndex -= 1
-                    self.pageShowIndex(self.pageIndex + 1)
-                }
-                if (!self.pageIndex) {
-                    self.pagePreDisabled('disabled')
-                    self.pagePreImgUrl('/static/images/user/pre-page-disabled.png')
-                }
-                messageShowIndexBegin = self.pageIndex * self.pageLengthSingle
-                messageShowIndexEnd = messageShowIndexBegin + self.pageLengthSingle
-                if (messageShowIndexEnd < self.messageData.length) {
-                    self.pageNexDisabled(null)
-                    self.pageNexImgUrl('/static/images/user/next-page.png')
-                }
-                self.messageList(self.messageData.slice(messageShowIndexBegin, messageShowIndexEnd))
-            }
-            function MessageListElement(data) {
-                var self = this
-                this.title = data.type_presentation.value + ' - ' + data.title
-                this.content = data.text
-                this.status = data.status
-                this.messageId = data.id
-                var formatter = window.lang === 'en_GB'? 'DD-MM-YYYY': 'YYYY-MM-DD'
-                this.time = window.moment(data.time * 1000).format(formatter)
-                this.expand = ko.observable(false)
-                this.expandStatus = ko.observable(i18n('展开'))
-                this.hasRead = ko.observable((data.status === 'new' || data.status === 'sent') ? false : true)
-                this.toggleExpand = function () {
-                    function markMessageRead() {
-                        self.status = 'read'
-                        self.hasRead(true)
-                        $.betterPost('/api/1/message/' + self.messageId + '/mark/' + 'read')
-                            .done(function (data) {
-                            })
-                            .fail(function (ret) {
-                            })
-                            .always(function () {
+    //Init page with rent
+    if (team.isPhone()) {
+        if (window.lang === 'zh_Hans_CN') {
+            $headerTabs.show()
+        }        
+    } else {
+        if (window.lang === 'zh_Hans_CN') {
+            $headerButtons.show()
+        }
+    }
 
-                            })
-                    }
+    loadChatCore()
 
-                    self.expand(self.expand() ? false : true)
+    // 我的咨询
+    function loadChatCore() {
+        var defer = $.Deferred()
+        
+        if (xhr && xhr.readyState !== 4) {
+            xhr.abort()
+        }
+        placeholder.hide()
+        $list.empty()
+        $('.loadIndicator').show()
 
-                    self.expandStatus(self.expand() ? i18n('收起') : i18n('展开'))
-
-                    if (!self.hasRead()) {
-                        markMessageRead()
-                    }
+        var params = {
+            'status': 'requested',
+            'user_id': window.user.id,
+            'per_page': -1
+        }
+        xhr = $.post('/api/1/rent_intention_ticket/search', params)
+            .success(function (data) {
+                var val = data.val
+                var array = val
+                if (array && array.length > 0) {
+                    var Tpl = '';
+                    chatListHeader.show()
+                    $(array).each(function (i, va){
+                        Tpl += '<div class="chatListItmes"><div class="title">';
+                        Tpl += '<a href="/property-to-rent/'+va.interested_rent_tickets[0].id+'" target="_blank">'+va.interested_rent_tickets[0].title+'</a></div>';
+                        Tpl += '<div class="info"><div class="name">'+va.interested_rent_tickets[0].user.nickname+'</div>';
+                        Tpl += '<div class="massage"><div class="text">'+(va.interested_rent_tickets[0].description === undefined? '': va.interested_rent_tickets[0].description)+'</div>';
+                        Tpl += '<div class="time"  data-utc-time="">'+team.parsePublishDate(parseInt(va.interested_rent_tickets[0].time))+'</div>';
+                        Tpl += '<a href="/user-chat/'+va.id+'/details" class="reply" target="_blank">'+i18n('回复')+'</a>';
+                        Tpl += '</div></div></div>';
+                    })
+                    $('#chatListContent').html(Tpl);
+                } else {
+                    chatListHeader.hide()
+                    $('#intentionPlaceHolder').show()
                 }
-            }
-            function categoryMessage(message) {
-                var resultList = []
-                message.forEach(function (data, index) {
-                    if (typeof (data) === 'object' && data) {
-                        resultList.push(new MessageListElement(data))
-                    }
-                })
-                return resultList
-            }
-            function filterMessage(message, tab) {
-                var resultList = []
-                message.forEach(function (data, index) {
-                    if (data.status === tab || tab === 'all') {
-                        resultList.push(data)
-                    }
-                })
-                return resultList
-            }
+                defer.resolve()
+            }).fail(function () {
+                $('#intentionPlaceHolder').show()
+                chatListHeader.hide()
+                defer.reject()
+            }).complete(function () {
+                $('.loadIndicator').hide()
+                isLoading = false
+            })
+        return defer.promise()
+    }
 
-        },
-        template: { element: 'kotChatListTemplate' }
+    // 咨询我的
+    function loadChatFrom(){
+        var defer = $.Deferred()
+        
+        if (xhr && xhr.readyState !== 4) {
+            xhr.abort()
+        }
+        placeholder.hide()
+        $list.empty()
+        $('.loadIndicator').show()
+
+        var params = {
+            'status': 'requested',
+            'user_id': window.user.id,
+            'per_page': -1
+        }
+        xhr = $.post('/api/1/rent_ticket/search', params)
+            .success(function (data) {
+                var val = data.val
+                var array = val
+                if (array && array.length > 0) {
+                    var Tpl = '';
+                    chatListHeader.show()
+                    $(array).each(function (i, va){
+                        Tpl += '<div class="chatListItmes"><div class="title">';
+                        Tpl += '<a href="/property-to-rent/'+va.interested_rent_tickets[0].id+'" target="_blank">'+va.interested_rent_tickets[0].title+'</a></div>';
+                        Tpl += '<div class="info"><div class="name">'+va.user.nickname+'</div>';
+                        Tpl += '<div class="massage"><div class="text">'+(va.description === undefined ? '': va.description)+'</div>';
+                        Tpl += '<div class="time"  data-utc-time="">'+team.parsePublishDate(parseInt(va.time))+'</div>';
+                        Tpl += '<a href="/user-chat/'+va.id+'/details" class="reply" target="_blank">'+i18n('回复')+'</a>';
+                        Tpl += '</div></div></div>';
+                    })
+                    $('#chatListContent').html(Tpl);
+                } else {
+                    chatListHeader.hide()
+                    $('#rentPlaceHolder').show()
+                }
+                defer.resolve()
+            }).fail(function () {
+                $('#rentPlaceHolder').show()
+                chatListHeader.hide()
+                defer.reject()
+            }).complete(function () {
+                $('.loadIndicator').hide()
+                isLoading = false
+            })
+        return defer.promise()
+    }
+
+
+    function switchTypeTab(state) {
+        var originHash = location.hash.slice(1)
+        var param = originHash.split('?')[1]
+        $('.ui-tabs-nav li').removeClass('ui-tabs-selected')
+        $('.ui-tabs-nav .' + state.replace('Only', '')).addClass('ui-tabs-selected')
+        $('.buttons .button').removeClass('button').addClass('ghostButton')
+        $('.buttons .' + state.replace('Only', '')).removeClass('ghostButton').addClass('button')
+        location.hash = state + (param ? '?' + param : '')
+    }
+
+    $(window).on('hashchange', function () {
+        var hash = location.hash.slice(1)
+        var state = hash.split('?')[0]
+        var extraParam = hash.split('?')[1]
+        var rentStatus
+        if(extraParam) {
+            rentStatus = decodeURIComponent(extraParam.match(/status=(.+)/)[1]).split(',')
+        }
+        switch(state) {
+            case 'intention':
+                switchTypeTab(state)
+                loadChatCore(rentStatus)
+                break
+            case 'rent': //出租申请单
+                switchTypeTab(state)
+                loadChatFrom()
+                break
+        }
+
     })
-}
-chatListBind(window.ko, window.currantModule = window.currantModule || {})
+    $(window).trigger('hashchange')
+
+    _.each(['Rent', 'Intention'], function (val) {
+        $('button#show' + val + 'Btn').click(function () {
+            switchTypeTab(val.toLowerCase())
+        })
+        $('#show' + val + 'Tab').click(function () {
+            switchTypeTab(val.toLowerCase())
+        })
+    })
+})
