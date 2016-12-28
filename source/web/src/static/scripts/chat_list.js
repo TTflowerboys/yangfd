f3f37eaa56210de0b342961f5b0cafad890de0e6
threetowns <tom.lei@bbtechgroup.com>
2016-12-28 10:18:02 +0800

    [Web] Fix:websocket.

--- source/web/src/static/scripts/chat_list.js
+++ source/web/src/static/scripts/chat_list.js
@@ -188,29 +188,30 @@ $(function(){
     var listener = {};
     listener.onreceivemessage = function(socketVal) {
 
-            $('.chatListItmes .info').on('socketChatMsg',function(){
-                var $this = $(this),val = $this.data('id'),target_user_id = $this.data('user_id');
-                if (socketVal.ticket_id === val && socketVal.from_user.id === target_user_id) {
-                    $.betterPost('/api/1/rent_intention_ticket/'+val+'/chat/history', {target_user_id: target_user_id})
-                        .done(function (data) {
-                            if (data && data.val !== null && data.val.length > 0) {
-                                var lastChatTpl  = '<div class="name">'+data.val[0].from_user.nickname.substring(1,-1)+'**</div>';
-                                    lastChatTpl += '<div class="massage"><div class="text">'+data.val[0].message+'</div>';
-                                    lastChatTpl += '<div class="time">'+team.parsePublishDate(parseInt(data.val[0].time))+'</div></div>';
-                                $this.html(lastChatTpl);
-                            }else{
-                                $this.html('<div class="loading">'+i18n('没有最新留言')+'</div>');
-                            }
-                        })
-                        .fail(function (ret) {
-                            $this.html('<div class="loading">'+window.getErrorMessageFromErrorCode(ret)+'...</div>');
-                            //window.dhtmlx.message({ type: 'error', text: window.getErrorMessageFromErrorCode(ret) })
-                        })
-                }else{
-                    window.console.log('no')
-                }
-            })
-            $('.chatListItmes .info').trigger('socketChatMsg')
+        $('.chatListItmes .info').on('socketChatMsg',function(){
+            var $this = $(this),val = $this.data('id'),target_user_id = $this.data('user_id');
+            if (socketVal.ticket_id === val && socketVal.from_user.id === target_user_id) {
+                $.betterPost('/api/1/rent_intention_ticket/'+val+'/chat/history', {target_user_id: target_user_id})
+                    .done(function (data) {
+                        if (data && data.val !== null && data.val.length > 0) {
+                            var lastChatTpl  = '<div class="name">'+data.val[0].from_user.nickname.substring(1,-1)+'**</div>';
+                                lastChatTpl += '<div class="massage"><div class="text">'+data.val[0].message+'</div>';
+                                lastChatTpl += '<div class="time">'+team.parsePublishDate(parseInt(data.val[0].time))+'</div></div>';
+                            $this.html(lastChatTpl);
+                        }else{
+                            $this.html('<div class="loading">'+i18n('没有最新留言')+'</div>');
+                        }
+                    })
+                    .fail(function (ret) {
+                        $this.html('<div class="loading">'+window.getErrorMessageFromErrorCode(ret)+'...</div>');
+                        //window.dhtmlx.message({ type: 'error', text: window.getErrorMessageFromErrorCode(ret) })
+                    })
+            }else{
+                window.console.log('no')
+            }
+        })
+        $('.chatListItmes .info').trigger('socketChatMsg')
+        
     }
     window.wsListeners.push(listener)
 
