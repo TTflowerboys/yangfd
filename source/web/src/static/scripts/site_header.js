2f4bda8de48fd5ebe851c0fdc0adda33653b54e3
Foster Yin <fyin@bbtechgroup.com>
2016-12-29 20:40:46 +0800

    [web] Add: Task #8116 Show the unread message in header

--- source/web/src/static/scripts/site_header.js
+++ source/web/src/static/scripts/site_header.js
@@ -1,4 +1,59 @@
 ;(function () {
+    if (window.user) {
+
+        var markMessageAsRead =  function(messageId) {
+            $.betterPost('/api/1/message/' + messageId + '/mark/' + 'read')
+                .done(function (data) {
+                })
+                .fail(function (ret) {
+                })
+                .always(function () {
+
+                })
+        }
+
+        $.betterPost('/api/1/message/search', {status: 'new', type: 'chat', per_page: 5})
+            .done(function (data) {
+                if (data.length > 0) {
+                    document.getElementById('icon-message').style.display = 'none'
+                    document.getElementById('icon-message-notif').style.display = 'inline'
+                    _.each(data, function(message) {
+                        var result = _.template($('#headerMessageList_template').html())({message: message})
+                        $('.supHeader .messageList ul').append(result)
+                    })
+
+
+                    $('.supHeader .messageList ul li').mouseover(function() {
+                         $(this).find('.time').hide();
+                         $(this).find('.close').show();
+
+                    }).mouseout(function() {
+                        $(this).find('.close').hide();
+                        $(this).find('.time').show();
+                    })
+
+                    $('.supHeader .messageList ul li .close').click(function() {
+                        var $item = $(this).parents('li')
+                        var messageId = $item.attr('data-id')
+                        $item.find('img').hide()
+                        $item.animate({
+                            opacity: 0.25,
+                            height: 0
+                        }, 200, function() {
+                            $item.remove()
+                        });
+
+                        markMessageAsRead(messageId);
+                    })
+                }
+            })
+            .fail(function (ret) {
+            })
+            .always(function () {
+
+            })
+    }
+
     window.wsListeners = []
     //onreceivemessage
     if (window.user) {
