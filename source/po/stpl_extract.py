baf58e6dad7eab0a231d4afe8d414dfb552bbce9
Foster Yin <fyin@bbtechgroup.com>
2017-01-07 17:48:49 +0800

    [Server] Add: Move i18n js extract into stpl_extract.py

--- source/po/stpl_extract.py
+++ source/po/stpl_extract.py
@@ -4,9 +4,12 @@ import six
 
 def babel_extract(fileobj, keywords, comment_tags, options):
     keyword_regex = re.compile("(" + "|".join(keywords) + r")\(u?['\"](.*?[^\\])['\"]\)")
+    js_regex = re.compile(r"i18n\(['\"](.+?)['\"](,|\))")
 
     for lineno, line in enumerate(fileobj):
         if isinstance(line, six.binary_type):
             line = line.decode("utf-8")
         for match in keyword_regex.findall(line):
             yield lineno + 1, None, match[1], []
+        for match in js_regex.findall(line):
+            yield lineno + 1, None, match[0], []
