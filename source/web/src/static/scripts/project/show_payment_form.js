20efb13ad56838dcbabf118e617d47d5d51b0f61
tom <tom.lei@bbtechgroup.com>
2017-02-06 19:08:34 +0800

    [Web] Add:show payment form.

---
+++ source/web/src/static/scripts/project/show_payment_form.js
@@ -0,0 +1,9 @@
+(function (ko) {
+	ko.components.register('show-payment-form', {
+        viewModel: function(params) {
+        	
+
+        },
+        template: { element: 'show-payment-form-tpl' }
+    })
+})(window.ko);
