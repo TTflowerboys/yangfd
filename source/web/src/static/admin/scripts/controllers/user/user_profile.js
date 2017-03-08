f36c2e4aa302b9ed310e9c1bfec6d05302acd991
tom <tom.lei@bbtechgroup.com>
2017-03-08 21:14:09 +0800

    [Web] Add:admin can change user password

--- source/web/src/static/admin/scripts/controllers/user/user_profile.js
+++ source/web/src/static/admin/scripts/controllers/user/user_profile.js
@@ -3,7 +3,7 @@
  */
 (function () {
 
-    function ctrlUserProfile($scope, $state, api, misc) {
+    function ctrlUserProfile($scope, $state, $http, growl, fctModal, api, misc) {
         $scope.api = api
 
         if ($state.current.url === '/:id') {
@@ -21,6 +21,33 @@
                 .success(function (data) {
                     $scope.item = data.val
                 })
+        }
+
+
+        $scope.onRandomString = function (length) {
+            var chars = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
+            var pass = '';
+            for (var x = 0; x < length; x++) {
+                var i = Math.floor(Math.random() * chars.length);
+                pass += chars.charAt(i);
+            }
+            $('#randomString').val(pass)
+        }
+
+        $scope.onChangePassword = function ($event, form) {
+            var password = $('#randomString').val()
+            var id = $('#id').val()
+            if (password.length < 0) {
+                growl.addWarnMessage('密码不能为空')
+            }else{
+                fctModal.show('确定重置密码?', undefined, function () {
+                    var params = {
+                        'user_id': id,
+                        'password': Base64.encode(password)
+                    }
+                    return $http.post('/api/1/user/edit', params, {successMessage: i18n('密码修改成功'), errorMessage: true})
+                })
+            }            
         }
 
     }
